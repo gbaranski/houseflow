19867a0effe448a132a7211a2f36a2dcac1526d2
gbaranski <gbaranski19@gmail.com>
2020-07-13 19:31:23 +0200

    cleaned code

--- src/auth.ts
+++ src/auth.ts
@@ -1,4 +1,6 @@
 import express from 'express';
+import jwt from 'jsonwebtoken';
+import http from 'http';
 
 function validateCredentials(
   username: string | undefined,
@@ -31,3 +33,46 @@ export function authenticateDevice(device: string, token: string): void {
     throw new Error('Invalid token or device name');
   }
 }
+
+interface VerifyClientInfo {
+  origin: string;
+  secure: boolean;
+  req: http.IncomingMessage;
+}
+
+interface VerifyClientCallback {
+  (
+    res: boolean,
+    code?: number,
+    message?: string,
+    headers?: http.OutgoingHttpHeaders,
+  ): void;
+}
+
+export const verifyClient = (
+  info: VerifyClientInfo,
+  callback: VerifyClientCallback,
+): void => {
+  if (!process.env.JWT_KEY) throw new Error('Missing process.env.JWT_KEY');
+  console.log(info.req.headers.token || undefined);
+  const token = info.req.headers.token || '';
+  if (!token) {
+    console.log('client doesnt have token');
+    callback(false, 401, 'Unauthorized');
+  } else {
+    jwt.verify(token as string, process.env.JWT_KEY, (err, decoded) => {
+      if (!decoded) {
+        console.log('Missing decoded username at JWT Token');
+        callback(false, 400, 'Missing decoded username');
+        return;
+      }
+      if (err) {
+        callback(false, 401, 'Unauthorized');
+        console.log('client has invalid token');
+      } else {
+        info.req.headers.device = decoded.device;
+        callback(true);
+      }
+    });
+  }
+};
