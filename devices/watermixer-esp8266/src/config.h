2be69499fcf18c4cda550b9295de1f80cff744d9
gbaranski <gbaranski19@gmail.com>
2020-08-01 20:35:05 +0200

    update to use SSL on websocket at watermixer

--- devices/watermixer-esp8266/src/config.h
+++ devices/watermixer-esp8266/src/config.h
@@ -2,12 +2,3 @@
 
 // GPIO
 const int RELAYPIN = 4;
-
-// WIFI
-const char *ssid = "Nigeria";
-const char *password = "hondamsx125";
-
-// PROD
-const char *TOKEN_SERVER_URL = "http://api.gbaranski.com/api/getToken";
-const char *websockets_server = "api.gbaranski.com"; //server adress and port
-const uint16_t websockets_port = 6436;
--- devices/watermixer-esp8266/src/secret.example.h
+++ devices/watermixer-esp8266/src/secret.example.h
@@ -1,3 +1,12 @@
 #include <Arduino.h>
 
 const String WATERMIXER_TOKEN = "TOKEN";
+
+// WIFI
+const char *ssid = "WIFI_SSID";
+const char *password = "WIFI_PASS";
+
+// PROD
+const char *TOKEN_SERVER_URL = "http://example.com/api/getToken";
+const uint16_t websockets_port = 443;
+const char *websockets_server = "example.com"; //server adress and port
