1cc359b50de8489c4dd5e47f93f4af05263ffeba
gbaranski <gbaranski19@gmail.com>
2020-08-05 19:01:59 +0200

    Add currentuser type

--- types/src/client.ts
+++ types/src/client.ts
@@ -1,6 +1,12 @@
 import { DeviceType } from './other';
 import { RequestAlarmclock, RequestWatermixer } from '.';
 
+export interface CurrentUser {
+  fullAccess: string[];
+  name: string;
+  uid: string;
+}
+
 export interface ClientRequest<T extends DeviceType> {
   device?: T;
   request: T extends DeviceType.ALARMCLOCK
