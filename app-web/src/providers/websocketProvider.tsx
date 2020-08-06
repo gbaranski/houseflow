da091ff4096a347c71b09c18d37f84ba87959663
gbaranski <gbaranski19@gmail.com>
2020-08-06 18:25:02 +0200

    Add deviceDataProvider, improve webSocket

--- app-web/src/providers/websocketProvider.tsx
+++ app-web/src/providers/websocketProvider.tsx
@@ -10,11 +10,11 @@ export const WebsocketContext = React.createContext<IWebsocketContext>({
   setWebsocket: undefined,
 });
 
-interface UserProviderProps {
+interface WebsocketProviderProps {
   children: React.ReactNode;
 }
 
-export const WebsocketProvider = ({ children }: UserProviderProps) => {
+export const WebsocketProvider = ({ children }: WebsocketProviderProps) => {
   const [websocket, setWebsocket] = React.useState<WebSocket | undefined>();
   return (
     <WebsocketContext.Provider
