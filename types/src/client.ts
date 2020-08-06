7b5a5383bff2b7773aef4435ff29ebbbd1ea6031
gbaranski <gbaranski19@gmail.com>
2020-08-06 19:54:44 +0200

    Extended client types

--- types/src/client.ts
+++ types/src/client.ts
@@ -1,12 +1,23 @@
-import { RequestAlarmclock, RequestWatermixer, DeviceType } from '.';
+import { AlarmclockData, DateTime, WatermixerData, State } from '.';
 
-export interface ClientRequest<T extends DeviceType> {
-  device?: T;
-  request: T extends DeviceType.ALARMCLOCK
-    ? RequestAlarmclock
-    : T extends DeviceType.WATERMIXER
-    ? RequestWatermixer
-    : undefined;
+export interface RequestClient {
+  deviceUid?: string;
+  type: ClientRequests;
+  data?: DateTime | State;
+}
+export enum ClientRequests {
+  GET_DATA = 'GET_DATA',
+  GET_DEVICES = 'GET_DEVICES',
+  GET_DEVICES_STATUS = 'GET_DEVICES_STATUS',
+}
+
+export interface ResponseClient<
+  T extends WatermixerData | AlarmclockData | undefined
+> {
+  ok: boolean;
+  deviceUid: string;
+  responseFor: ClientRequests;
+  data: T;
 }
 
 // const Example: ClientRequest<DeviceType.ALARMCLOCK> = {
