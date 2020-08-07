3b091f39581a4542a66804e18e8c92a160f28106
gbaranski <gbaranski19@gmail.com>
2020-08-07 23:34:46 +0200

    Updated server for newest types

--- server/src/devices/watermixer/index.ts
+++ server/src/devices/watermixer/index.ts
@@ -1,28 +1,20 @@
 import WebSocket from 'ws';
-import { v4 as uuidv4 } from 'uuid';
-import {
-  watermixerSample,
-  WatermixerData,
-  ResponseDevice,
-  RequestTypes,
-  CurrentDevice,
-  DeviceType,
-} from '@gbaranski/types';
 import Device from '..';
+import { Watermixer, Device as DeviceType } from '@gbaranski/types';
 import { validateDeviceMessage } from '@/helpers';
 
-export class WatermixerDevice extends Device<WatermixerData> {
-  constructor(ws: WebSocket, device: CurrentDevice) {
-    super(ws, watermixerSample, DeviceType.WATERMIXER, device.uid);
+export class WatermixerDevice extends Device<Watermixer.Data> {
+  constructor(ws: WebSocket, device: DeviceType.FirebaseDevice) {
+    super(ws, Watermixer.SAMPLE, 'WATERMIXER', device.uid, device.secret);
   }
 
   handleMessage(message: WebSocket.Data): void {
     validateDeviceMessage(message);
-    const parsedResponse = JSON.parse(message as string) as ResponseDevice<
-      undefined
-    >;
-    if (parsedResponse.responseFor === RequestTypes.GET_DATA) {
-      this.deviceData = (parsedResponse.data as unknown) as WatermixerData;
+    const parsedResponse = JSON.parse(
+      message as string,
+    ) as DeviceType.ResponseDevice<undefined>;
+    if (parsedResponse.responseFor === 'GET_DATA') {
+      this.deviceData = (parsedResponse.data as unknown) as Watermixer.Data;
     }
   }
 }
