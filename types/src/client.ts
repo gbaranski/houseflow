be21053383d6da1baac52b37887df132d7f1f24b
Grzegorz Baranski <gbaranski19@gmail.com>
2020-08-12 01:51:28 +0200

    Revert "moved few types from app-web"
    This reverts commit e925381e8c26a6ec4e7c653ea008d06e3f5c28da.

--- types/src/client.ts
+++ types/src/client.ts
@@ -1,20 +1,12 @@
-import {
-  AlarmclockData,
-  DateTime,
-  WatermixerData,
-  State,
-  DeviceType,
-  CurrentDevice,
-  AnyDeviceData,
-} from '.';
+import { AlarmclockData, DateTime, WatermixerData, State } from '.';
+import { DeviceType, CurrentDevice } from './device';
 
 export interface RequestClient {
   deviceUid?: string;
   type: ClientRequests;
   deviceType?: DeviceType;
-  data?: DateTime | State | ClientCurrentDevice<DeviceType>[];
+  data?: DateTime | State;
 }
-
 export enum ClientRequests {
   GET_DATA = 'GET_DATA',
   GET_DEVICES = 'GET_DEVICES',
@@ -30,27 +22,6 @@ export interface ResponseClient<
   responseFor: ClientRequests;
   data: T;
 }
-
-export interface ClientCurrentDevice<T extends DeviceType> {
-  type: T;
-  secret: string;
-  uid: string;
-  data?: TDeviceData<T>;
-  status?: State;
-}
-
-export type TDeviceDataArgs<T extends AnyDeviceData | undefined> = {
-  devicesData: T;
-  setDevicesData: ((data: T) => any) | undefined;
-};
-
-type TDeviceData<
-  T extends DeviceType | undefined
-> = T extends DeviceType.ALARMCLOCK
-  ? TDeviceDataArgs<AlarmclockData>
-  : T extends DeviceType.WATERMIXER
-  ? TDeviceDataArgs<WatermixerData>
-  : undefined;
 
 // const Example: ClientRequest<DeviceType.ALARMCLOCK> = {
 //   request: (type: RequestTypes, data?: DateTime | boolean) =>
