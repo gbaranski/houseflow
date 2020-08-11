dda405f8d5f370bce406bcdfff6c6983ebabcbe2
gbaranski <gbaranski19@gmail.com>
2020-08-12 01:50:56 +0200

    updated welcome page

--- app-web/src/pages/welcome/index.tsx
+++ app-web/src/pages/welcome/index.tsx
@@ -8,6 +8,7 @@ import Grid from '@material-ui/core/Grid';
 import Copyright from '../../components/copyright';
 import { MuiPickersUtilsProvider } from '@material-ui/pickers';
 import DateFnsUtils from '@date-io/date-fns';
+import { Paper, Typography } from '@material-ui/core';
 
 const useStyles = makeStyles((theme) => ({
   title: {
@@ -30,12 +31,13 @@ const useStyles = makeStyles((theme) => ({
     flexDirection: 'column',
   },
   fixedHeight: {
-    height: 240,
+    height: 100,
   },
 }));
 
 function Welcome() {
   const classes = useStyles();
+  const fixedHeightPaper = clsx(classes.paper, classes.fixedHeight);
 
   return (
     <>
@@ -44,7 +46,24 @@ function Welcome() {
         <div className={classes.appBarSpacer} />
         <Container maxWidth="lg" className={classes.container}>
           <Grid container spacing={3}>
-            Welcome
+            <Grid item xs={12} md={8} lg={12}>
+              <Paper className={fixedHeightPaper}>
+                <Typography
+                  variant="h5"
+                  style={{
+                    fontWeight: 600,
+                    textAlign: 'center',
+                  }}
+                >
+                  🚧 Control Home - Alpha 🚧
+                </Typography>
+                <Typography style={{ textAlign: 'left', fontWeight: 300 }}>
+                  This webapp is currently under heavy development 🏗 . You might
+                  find a lot of bugs or even dragons 🐉...Please use this app on
+                  your own risk 👷.
+                </Typography>
+              </Paper>
+            </Grid>
           </Grid>
           <Box pt={4}>
             <Copyright />
