66e18f664e3c6d25574f7ff5c9e88eec26a7a05a
Grzegorz Baranski <gbaranski19@gmail.com>
2020-08-12 01:51:28 +0200

    Revert "Fix types"
    This reverts commit 5a6f8bc87d19c60e90426a0efb785b94c1ff8c83.

--- types/src/client.ts
+++ types/src/client.ts
@@ -2,7 +2,7 @@ import { DateTime, State, AnyDeviceData } from '.';
 import { Device } from './device';
 
 export namespace Client {
-  export type RequestType = 'TEST' & Device.RequestType;
+  export type RequestType = 'TEST';
 
   export interface Request {
     requestType: RequestType;
@@ -15,7 +15,7 @@ export namespace Client {
 
   export interface Response {
     requestType: ResponseType;
-    data?: ActiveDevice;
+    data?: ResponseType;
   }
 
   export interface FirebaseDevice {
