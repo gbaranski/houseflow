66baafe112ffd56a26df56ee857deb079764f7c5
gbaranski <gbaranski19@gmail.com>
2020-08-13 00:36:15 +0200

    Add device page, update access admin page, patchRoutes problem

---
+++ app-web-antd/config/routes.ts
@@ -0,0 +1,64 @@
+import { IRoute } from 'umi';
+
+export const routes: IRoute[] = [
+  {
+    path: '/user',
+    layout: false,
+    routes: [
+      {
+        name: 'login',
+        path: '/user/login',
+        component: './user/login',
+      },
+    ],
+  },
+  {
+    path: '/device',
+    name: 'Device',
+    icon: 'smile',
+    exact: false,
+    component: './device/',
+  },
+  {
+    path: '/welcome',
+    name: 'welcome',
+    icon: 'smile',
+    component: './Welcome',
+  },
+  {
+    path: '/admin',
+    name: 'admin',
+    icon: 'crown',
+    access: 'canAdmin',
+    component: './Admin',
+    routes: [
+      {
+        path: '/admin/sub-page',
+        name: 'sub-page',
+        icon: 'smile',
+        component: './Welcome',
+      },
+    ],
+  },
+  {
+    name: 'list.table-list',
+    icon: 'table',
+    path: '/list',
+    component: './ListTableList',
+  },
+  {
+    path: '/',
+    redirect: '/welcome',
+  },
+  {
+    component: './404',
+  },
+];
+
+export function patchRoutes() {
+  routes.unshift({
+    path: '/foo',
+    exact: true,
+    icon: 'smile',
+  });
+}
