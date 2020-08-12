31bb83be68b03469aec8b559811828ea2891b50e
gbaranski <gbaranski19@gmail.com>
2020-08-12 15:08:19 +0200

    init commit

---
+++ app-web-antd/config/defaultSettings.ts
@@ -0,0 +1,26 @@
+import { Settings as ProSettings } from '@ant-design/pro-layout';
+
+type DefaultSettings = ProSettings & {
+  pwa: boolean;
+};
+
+const proSettings: DefaultSettings = {
+  navTheme: 'dark',
+  // 拂晓蓝
+  primaryColor: '#1890ff',
+  layout: 'side',
+  contentWidth: 'Fluid',
+  fixedHeader: false,
+  fixSiderbar: true,
+  colorWeak: false,
+  menu: {
+    locale: true,
+  },
+  title: 'Ant Design Pro',
+  pwa: false,
+  iconfontUrl: '',
+};
+
+export type { DefaultSettings };
+
+export default proSettings;
---
+++ app-web-antd/src/pages/account/center/index.tsx
@@ -0,0 +1,266 @@
+import { PlusOutlined, HomeOutlined, ContactsOutlined, ClusterOutlined } from '@ant-design/icons';
+import { Avatar, Card, Col, Divider, Input, Row, Tag } from 'antd';
+import React, { Component, useState, useRef } from 'react';
+import { GridContent } from '@ant-design/pro-layout';
+import { Link, connect, Dispatch } from 'umi';
+import { RouteChildrenProps } from 'react-router';
+import { ModalState } from './model';
+import Projects from './components/Projects';
+import Articles from './components/Articles';
+import Applications from './components/Applications';
+import { CurrentUser, TagType } from './data.d';
+import styles from './Center.less';
+
+const operationTabList = [
+  {
+    key: 'articles',
+    tab: (
+      <span>
+        文章 <span style={{ fontSize: 14 }}>(8)</span>
+      </span>
+    ),
+  },
+  {
+    key: 'applications',
+    tab: (
+      <span>
+        应用 <span style={{ fontSize: 14 }}>(8)</span>
+      </span>
+    ),
+  },
+  {
+    key: 'projects',
+    tab: (
+      <span>
+        项目 <span style={{ fontSize: 14 }}>(8)</span>
+      </span>
+    ),
+  },
+];
+
+interface CenterProps extends RouteChildrenProps {
+  dispatch: Dispatch;
+  currentUser: Partial<CurrentUser>;
+  currentUserLoading: boolean;
+}
+interface CenterState {
+  tabKey?: 'articles' | 'applications' | 'projects';
+}
+
+const TagList: React.FC<{ tags: CurrentUser['tags'] }> = ({ tags }) => {
+  const ref = useRef<Input | null>(null);
+  const [newTags, setNewTags] = useState<TagType[]>([]);
+  const [inputVisible, setInputVisible] = useState<boolean>(false);
+  const [inputValue, setInputValue] = useState<string>('');
+
+  const showInput = () => {
+    setInputVisible(true);
+    if (ref.current) {
+      // eslint-disable-next-line no-unused-expressions
+      ref.current?.focus();
+    }
+  };
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(e.target.value);
+  };
+
+  const handleInputConfirm = () => {
+    let tempsTags = [...newTags];
+    if (inputValue && tempsTags.filter((tag) => tag.label === inputValue).length === 0) {
+      tempsTags = [...tempsTags, { key: `new-${tempsTags.length}`, label: inputValue }];
+    }
+    setNewTags(tempsTags);
+    setInputVisible(false);
+    setInputValue('');
+  };
+
+  return (
+    <div className={styles.tags}>
+      <div className={styles.tagsTitle}>标签</div>
+      {(tags || []).concat(newTags).map((item) => (
+        <Tag key={item.key}>{item.label}</Tag>
+      ))}
+      {inputVisible && (
+        <Input
+          ref={ref}
+          type="text"
+          size="small"
+          style={{ width: 78 }}
+          value={inputValue}
+          onChange={handleInputChange}
+          onBlur={handleInputConfirm}
+          onPressEnter={handleInputConfirm}
+        />
+      )}
+      {!inputVisible && (
+        <Tag onClick={showInput} style={{ borderStyle: 'dashed' }}>
+          <PlusOutlined />
+        </Tag>
+      )}
+    </div>
+  );
+};
+
+class Center extends Component<CenterProps, CenterState> {
+  // static getDerivedStateFromProps(
+  //   props: accountAndcenterProps,
+  //   state: accountAndcenterState,
+  // ) {
+  //   const { match, location } = props;
+  //   const { tabKey } = state;
+  //   const path = match && match.path;
+
+  //   const urlTabKey = location.pathname.replace(`${path}/`, '');
+  //   if (urlTabKey && urlTabKey !== '/' && tabKey !== urlTabKey) {
+  //     return {
+  //       tabKey: urlTabKey,
+  //     };
+  //   }
+
+  //   return null;
+  // }
+
+  state: CenterState = {
+    tabKey: 'articles',
+  };
+
+  public input: Input | null | undefined = undefined;
+
+  componentDidMount() {
+    const { dispatch } = this.props;
+    dispatch({
+      type: 'accountAndcenter/fetchCurrent',
+    });
+    dispatch({
+      type: 'accountAndcenter/fetch',
+    });
+  }
+
+  onTabChange = (key: string) => {
+    // If you need to sync state to url
+    // const { match } = this.props;
+    // router.push(`${match.url}/${key}`);
+    this.setState({
+      tabKey: key as CenterState['tabKey'],
+    });
+  };
+
+  renderChildrenByTabKey = (tabKey: CenterState['tabKey']) => {
+    if (tabKey === 'projects') {
+      return <Projects />;
+    }
+    if (tabKey === 'applications') {
+      return <Applications />;
+    }
+    if (tabKey === 'articles') {
+      return <Articles />;
+    }
+    return null;
+  };
+
+  renderUserInfo = (currentUser: Partial<CurrentUser>) => (
+    <div className={styles.detail}>
+      <p>
+        <ContactsOutlined
+          style={{
+            marginRight: 8,
+          }}
+        />
+        {currentUser.title}
+      </p>
+      <p>
+        <ClusterOutlined
+          style={{
+            marginRight: 8,
+          }}
+        />
+        {currentUser.group}
+      </p>
+      <p>
+        <HomeOutlined
+          style={{
+            marginRight: 8,
+          }}
+        />
+        {(currentUser.geographic || { province: { label: '' } }).province.label}
+        {
+          (
+            currentUser.geographic || {
+              city: {
+                label: '',
+              },
+            }
+          ).city.label
+        }
+      </p>
+    </div>
+  );
+
+  render() {
+    const { tabKey } = this.state;
+    const { currentUser = {}, currentUserLoading } = this.props;
+    const dataLoading = currentUserLoading || !(currentUser && Object.keys(currentUser).length);
+    return (
+      <GridContent>
+        <Row gutter={24}>
+          <Col lg={7} md={24}>
+            <Card bordered={false} style={{ marginBottom: 24 }} loading={dataLoading}>
+              {!dataLoading && (
+                <div>
+                  <div className={styles.avatarHolder}>
+                    <img alt="" src={currentUser.avatar} />
+                    <div className={styles.name}>{currentUser.name}</div>
+                    <div>{currentUser.signature}</div>
+                  </div>
+                  {this.renderUserInfo(currentUser)}
+                  <Divider dashed />
+                  <TagList tags={currentUser.tags || []} />
+                  <Divider style={{ marginTop: 16 }} dashed />
+                  <div className={styles.team}>
+                    <div className={styles.teamTitle}>团队</div>
+                    <Row gutter={36}>
+                      {currentUser.notice &&
+                        currentUser.notice.map((item) => (
+                          <Col key={item.id} lg={24} xl={12}>
+                            <Link to={item.href}>
+                              <Avatar size="small" src={item.logo} />
+                              {item.member}
+                            </Link>
+                          </Col>
+                        ))}
+                    </Row>
+                  </div>
+                </div>
+              )}
+            </Card>
+          </Col>
+          <Col lg={17} md={24}>
+            <Card
+              className={styles.tabsCard}
+              bordered={false}
+              tabList={operationTabList}
+              activeTabKey={tabKey}
+              onTabChange={this.onTabChange}
+            >
+              {this.renderChildrenByTabKey(tabKey)}
+            </Card>
+          </Col>
+        </Row>
+      </GridContent>
+    );
+  }
+}
+
+export default connect(
+  ({
+    loading,
+    accountAndcenter,
+  }: {
+    loading: { effects: { [key: string]: boolean } };
+    accountAndcenter: ModalState;
+  }) => ({
+    currentUser: accountAndcenter.currentUser,
+    currentUserLoading: loading.effects['accountAndcenter/fetchCurrent'],
+  }),
+)(Center);
