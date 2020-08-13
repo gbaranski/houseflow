c62cd250519249a3ff2370e46e2316762f351ae6
gbaranski <gbaranski19@gmail.com>
2020-08-13 04:59:33 +0200

    Removed old app-web and moved into his place app-web-antd

--- app-web/src/pages/login/success/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Typography } from '@material-ui/core';
-import LoadingPage from '../../loading';
-
-const LoginSuccess = () => {
-  return <LoadingPage title="Experimental" />;
-};
-export default LoginSuccess;
--- app-web/src/services/websocket.ts
+++ app-web/src/services/websocket.ts
@@ -1,76 +1,23 @@
-import { WSS_URL } from '../config';
-import { Device, Client, AnyDeviceData } from '@gbaranski/types';
-import { toast } from 'react-toastify';
+const WSS_URL = 'ws://localhost:8001';
 
-export const beginWebsocketConnection = async (
-  token: string,
-): Promise<WebSocket> => {
-  console.log('Starting websocket service');
-  return new WebSocket(WSS_URL, token);
+let websocket: WebSocket | undefined;
+
+export const connectWebsocket = (token: string) => {
+  websocket = new WebSocket(WSS_URL, token);
 };
 
-export const setupWebsocketHandlers = async (
-  ws: WebSocket,
-  devices: Device.ActiveDevice<AnyDeviceData>[],
-  setDevices: (devices: Device.ActiveDevice<AnyDeviceData>[]) => any,
-  firstDataArrived: boolean,
-  setFirstDataArrived: (state: boolean) => any,
-  onConnectionClose: () => any,
-) => {
-  ws.addEventListener('message', (wsResponse) => {
-    handleMessage(
-      wsResponse.data,
-      devices,
-      setDevices,
-      firstDataArrived,
-      setFirstDataArrived,
-    );
-  });
-};
-
-const handleMessage = (
-  message: string,
-  devices: Device.ActiveDevice<AnyDeviceData>[],
-  setDevices: (devices: Device.ActiveDevice<AnyDeviceData>[]) => any,
-  firstDataArrived: boolean,
-  setFirstDataArrived: (state: boolean) => any,
-) => {
-  const response = JSON.parse(message) as Client.Response;
-  if (!response) throw new Error('Websocket response is not okay!');
-  if (response.requestType === 'DATA') {
-    console.log('Received new data', response.data);
-    setDevices(response.data as Device.ActiveDevice<AnyDeviceData>[]);
-    if (!firstDataArrived) {
-      setFirstDataArrived(true);
-    }
-  } else if (response.requestType === 'DEVICES') {
-    console.log('Recieved devices', response.data);
+export const setupOnOpenListeners = (onMessage: (message: MessageEvent) => any) => {
+  if (!websocket) throw new Error('Websocket is not defined');
+  if (websocket.OPEN) {
+    websocket.onmessage = onMessage;
+    return;
   }
+  websocket.onopen = () => {
+    if (!websocket) throw new Error('Websocket is not defined');
+    websocket.onmessage = onMessage;
+  };
 };
 
-export const preWebsocketMessage = (ws: WebSocket) => {
-  try {
-    if (!ws) throw new Error('Websocket is not defined');
-    if (!ws.OPEN) throw new Error('Websocket connection is not opened');
-  } catch (e) {
-    toast.error('Failed! ❌', {
-      autoClose: 1000,
-      progressStyle: {
-        background: 'red',
-      },
-      bodyStyle: {
-        color: 'gray',
-      },
-    });
-    throw e;
-  }
-  toast('Sending request! ✅', {
-    autoClose: 1000,
-    progressStyle: {
-      background: 'green',
-    },
-    bodyStyle: {
-      color: 'gray',
-    },
-  });
+export const getWebsocket = (): WebSocket | undefined => {
+  return websocket;
 };
