fe818f4734ed69d7027ace4e57f9ea56cf3304af
gbaranski <gbaranski19@gmail.com>
2020-08-14 17:48:05 +0200

    change ws url depending on nod_env

--- app-web/src/services/websocket.ts
+++ app-web/src/services/websocket.ts
@@ -1,5 +1,7 @@
-const WSS_URL = 'ws://localhost:8001';
+const WSS_URL =
+  process.env.NODE_ENV === 'development' ? 'ws://localhost:8001' : 'wss://ws.gbaranski.com/8001';
 
+console.log({ processenv: process.env.NODE_ENV });
 let websocket: WebSocket | undefined;
 
 export const connectWebsocket = (token: string) => {
