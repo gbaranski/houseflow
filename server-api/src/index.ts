ccc345c94bdf1588ba622c4c8afce9319a482393
gbaranski <gbaranski19@gmail.com>
2020-08-19 14:27:40 +0200

    server-api works with docker

--- server-api/src/index.ts
+++ server-api/src/index.ts
@@ -2,16 +2,16 @@ import http from 'http';
 import app from './app';
 import chalk from 'chalk';
 
-if (!process.env.PORT) throw new Error('Port not defined in .env');
+const PORT = process.env.PORT_API;
+
+if (!PORT) throw new Error('Port not defined in .env');
 
 const httpServer = http.createServer(app);
 
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
-httpServer.listen(process.env.PORT, '0.0.0.0', () => {
+httpServer.listen(PORT, '0.0.0.0', () => {
   console.log(
-    chalk.yellowBright(
-      `Listening for http requests on port ${process.env.PORT}`,
-    ),
+    chalk.yellowBright(`Listening for http requests on port ${PORT}`),
   );
 });
