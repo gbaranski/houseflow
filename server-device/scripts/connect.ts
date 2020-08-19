45798f601e9ddbaff95accc9bdd074b059a83378
gbaranski <gbaranski19@gmail.com>
2020-08-19 16:19:56 +0200

    update connect script

--- server-device/scripts/connect.ts
+++ server-device/scripts/connect.ts
@@ -2,20 +2,24 @@ import WebSocket from 'ws';
 import fetch from 'node-fetch';
 import { Device, Alarmclock } from '@gbaranski/types';
 
+const deviceSecret = process.env.TEST_SECRET;
+const deviceUid = process.env.TEST_UID;
+const deviceType = process.env.TEST_TYPE;
+
 (async () => {
   const URL = 'ws://localhost:8002';
   const req = await fetch('http://localhost:8000/api/getDeviceToken', {
     headers: {
-      uid: '93ce1298-0782-42b7-916e-9a7d01e1ea8d',
-      deviceType: 'ALARMCLOCK',
-      secret: '873e5133-c7d1-4a7a-bd22-b98793417917',
+      uid: deviceUid,
+      deviceType: deviceType,
+      secret: deviceSecret,
     },
   });
   const ws = new WebSocket(URL, {
     headers: {
-      uid: '93ce1298-0782-42b7-916e-9a7d01e1ea8d',
-      deviceType: 'ALARMCLOCK',
-      secret: '873e5133-c7d1-4a7a-bd22-b98793417917',
+      uid: deviceUid,
+      deviceType: deviceType,
+      secret: deviceSecret,
       token: await req.text(),
     },
   });
