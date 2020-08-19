640924ca5de9142ff461c590a3f904a3b6fa9906
gbaranski <gbaranski19@gmail.com>
2020-08-19 14:43:39 +0200

    updated packages and docker configs

--- server-device/src/index.ts
+++ server-device/src/index.ts
@@ -5,6 +5,9 @@ import { verifyDevice, onConnection } from '@/services/websocket';
 import '@/services/redis_pub';
 import '@/services/redis_sub';
 
+const PORT = process.env.PORT_DEVICE;
+if (!PORT) throw new Error('Port is not defined in .env');
+
 const requestListener: http.RequestListener = (req, res) => {
   res.writeHead(200);
   res.end('Hello from device zone');
@@ -22,10 +25,8 @@ wss.on('connection', onConnection);
 
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
-httpServer.listen(process.env.PORT, '0.0.0.0', () =>
+httpServer.listen(PORT, '0.0.0.0', () =>
   console.log(
-    chalk.yellow(
-      `Listening for websocket_devices connection at port ${process.env.PORT}`,
-    ),
+    chalk.yellow(`Listening for websocket_devices connection at port ${PORT}`),
   ),
 );
