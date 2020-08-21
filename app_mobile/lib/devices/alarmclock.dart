d2e1180b42593334a38a75198e3d492a1a399039
gbaranski <gbaranski19@gmail.com>
2020-08-21 14:25:26 +0200

    Add cards and devices

---
+++ app_mobile/lib/devices/alarmclock.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class _AlarmclockState extends State<Alarmclock> {
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Text("Hello"),
+    );
+  }
+
+}
+
+class Alarmclock extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    return _AlarmclockState();
+  }
+
+}
---
+++ app_mobile/lib/devices/watermixer.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class _WatermixerState extends State<Watermixer> {
+  @override
+  Widget build(BuildContext context) {
+    return Text("Watermixer");
+  }
+
+}
+
+class Watermixer extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    return _WatermixerState();
+  }
+
+}
