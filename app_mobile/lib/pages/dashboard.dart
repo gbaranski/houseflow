da47cfaa3794d241e94883454d3d784dc304c4a7
gbaranski <gbaranski19@gmail.com>
2020-08-22 12:19:33 +0200

    fix dashboard container

--- app_mobile/lib/pages/dashboard.dart
+++ app_mobile/lib/pages/dashboard.dart
@@ -4,9 +4,12 @@ import 'package:app_mobile/devices/watermixer.dart';
 
 class _DashboardState extends State<Dashboard> {
   final List<Widget> _devices = [
-    Alarmclock(),
-    Divider(),
-    Watermixer(),
+    Container(
+      margin: const EdgeInsets.only(left: 10.0, right: 10.0, top: 20),
+      child: Column(children: [
+        Alarmclock(),
+      ]),
+    )
   ];
 
   @override
