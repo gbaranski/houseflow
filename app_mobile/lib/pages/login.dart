ffb8b303cbbd4d75477a3cae759141a41caeede2
gbaranski <gbaranski19@gmail.com>
2020-08-23 20:39:23 +0200

    update package-name, prepare for firebase integration

---
+++ app_mobile/lib/pages/login.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class LoginWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(child: Text("Login Page"));
+  }
+}
