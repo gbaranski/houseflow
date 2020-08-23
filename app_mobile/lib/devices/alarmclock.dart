04ef28b1b9aae8ec382e8d8a561346aed986486e
gbaranski <gbaranski19@gmail.com>
2020-08-23 19:03:35 +0200

    update layout of cards

--- app_mobile/lib/devices/alarmclock.dart
+++ app_mobile/lib/devices/alarmclock.dart
@@ -4,87 +4,99 @@ class _AlarmclockState extends State<Alarmclock> {
   @override
   Widget build(BuildContext context) {
     return Card(
-      child: Container(
-        margin: const EdgeInsets.only(top: 10),
-        child: Column(children: [
-          Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
-            Column(children: [
-              Text(
-                "Temperature",
-                style: TextStyle(
-                    fontWeight: FontWeight.w300,
-                    fontSize: 14,
-                    color: Colors.black.withOpacity(0.6)),
-              ),
-              Row(
-                children: [
-                  Icon(Icons.ac_unit),
-                  Text("28,6°C",
+      child: InkWell(
+          splashColor: Colors.blue.withAlpha(30),
+          onTap: () {
+            print('Card tapped.');
+          },
+          child: Container(
+            margin: const EdgeInsets.only(top: 10),
+            child: Column(children: [
+              Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+                Column(children: [
+                  Text(
+                    "Temperature",
+                    style: TextStyle(
+                        fontWeight: FontWeight.w300,
+                        fontSize: 14,
+                        color: Colors.black.withOpacity(0.6)),
+                  ),
+                  Row(
+                    children: [
+                      Icon(Icons.ac_unit),
+                      Text("28,6°C",
+                          style: TextStyle(
+                              fontSize: 26, fontWeight: FontWeight.w300)),
+                    ],
+                  ),
+                  Text(
+                    "Alarm Time",
+                    style: TextStyle(
+                        fontWeight: FontWeight.w300,
+                        fontSize: 14,
+                        color: Colors.black.withOpacity(0.6)),
+                  ),
+                  Row(
+                    children: [
+                      Text("07:45",
+                          style: TextStyle(
+                              fontSize: 26, fontWeight: FontWeight.w300)),
+                      Text("/OFF")
+                    ],
+                  )
+                ]),
+                Column(children: [
+                  Text(
+                    "Humidity",
+                    style: TextStyle(
+                        fontWeight: FontWeight.w300,
+                        fontSize: 14,
+                        color: Colors.black.withOpacity(0.6)),
+                  ),
+                  Row(
+                    children: [
+                      Icon(Icons.opacity),
+                      Text("49%",
+                          style: TextStyle(
+                              fontSize: 26, fontWeight: FontWeight.w300)),
+                    ],
+                  ),
+                  Text(
+                    "Remaining Time",
+                    style: TextStyle(
+                        fontWeight: FontWeight.w300,
+                        fontSize: 14,
+                        color: Colors.black.withOpacity(0.6)),
+                  ),
+                  Text("09:54:02",
                       style:
                           TextStyle(fontSize: 26, fontWeight: FontWeight.w300)),
-                ],
-              ),
-              Text(
-                "Alarm Time",
-                style: TextStyle(
-                    fontWeight: FontWeight.w300,
-                    fontSize: 14,
-                    color: Colors.black.withOpacity(0.6)),
-              ),
-              Text("07:45/OFF",
-                  style: TextStyle(fontSize: 26, fontWeight: FontWeight.w300)),
-            ]),
-            Column(children: [
-              Text(
-                "Humidity",
-                style: TextStyle(
-                    fontWeight: FontWeight.w300,
-                    fontSize: 14,
-                    color: Colors.black.withOpacity(0.6)),
-              ),
+                ]),
+              ]),
               Row(
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Icon(Icons.opacity),
-                  Text("49%",
-                      style:
-                          TextStyle(fontSize: 26, fontWeight: FontWeight.w300)),
+                  ButtonBar(
+                    alignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      FlatButton(
+                        child: const Text('TEST ALARM'),
+                        onPressed: () {/* ... */},
+                      ),
+                      FlatButton(
+                        child: const Text('SET TIME'),
+                        onPressed: () {/* ... */},
+                      ),
+                      FlatButton(
+                        child: const Text('SWITCH STATE'),
+                        onPressed: () {/* ... */},
+                      ),
+                    ],
+                  ),
                 ],
-              ),
-              Text(
-                "Remaining Time",
-                style: TextStyle(
-                    fontWeight: FontWeight.w300,
-                    fontSize: 14,
-                    color: Colors.black.withOpacity(0.6)),
-              ),
-              Text("09:54:02",
-                  style: TextStyle(fontSize: 26, fontWeight: FontWeight.w300)),
+              )
             ]),
-          ]),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              ButtonBar(
-                alignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  FlatButton(
-                    child: const Text('TEST ALARM'),
-                    onPressed: () {/* ... */},
-                  ),
-                  FlatButton(
-                    child: const Text('SET TIME'),
-                    onPressed: () {/* ... */},
-                  ),
-                  FlatButton(
-                    child: const Text('SWITCH STATE'),
-                    onPressed: () {/* ... */},
-                  ),
-                ],
-              ),
-            ],
-          )
-        ]),
-      ),
+          )),
     );
   }
 }
--- app_mobile/lib/devices/watermixer.dart
+++ app_mobile/lib/devices/watermixer.dart
@@ -3,15 +3,67 @@ import 'package:flutter/material.dart';
 class _WatermixerState extends State<Watermixer> {
   @override
   Widget build(BuildContext context) {
-    return Text("Watermixer");
+    return Card(
+        child: InkWell(
+      splashColor: Colors.blue.withAlpha(30),
+      onTap: () {
+        print('Card tapped.');
+      },
+      child: Container(
+        margin: const EdgeInsets.only(top: 10),
+        child: Column(children: [
+          Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+            Column(children: [
+              Text(
+                "Mixing state",
+                style: TextStyle(
+                    fontWeight: FontWeight.w300,
+                    fontSize: 14,
+                    color: Colors.black.withOpacity(0.6)),
+              ),
+              Row(
+                children: [
+                  Text("Mixing!",
+                      style:
+                          TextStyle(fontSize: 26, fontWeight: FontWeight.w300)),
+                ],
+              ),
+            ]),
+            Column(children: [
+              Text(
+                "Remaining Time",
+                style: TextStyle(
+                    fontWeight: FontWeight.w300,
+                    fontSize: 14,
+                    color: Colors.black.withOpacity(0.6)),
+              ),
+              Text("9m 12s",
+                  style: TextStyle(fontSize: 26, fontWeight: FontWeight.w300)),
+            ]),
+          ]),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ButtonBar(
+                alignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  FlatButton(
+                    child: const Text('START MIXING'),
+                    onPressed: () {/* ... */},
+                  ),
+                ],
+              ),
+            ],
+          )
+        ]),
+      ),
+    ));
   }
-
 }
 
-class Watermixer extends StatefulWidget{
+class Watermixer extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
     return _WatermixerState();
   }
-
 }
