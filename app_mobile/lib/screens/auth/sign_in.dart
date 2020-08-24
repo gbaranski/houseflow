042592dc986e92519efb3a0da9375e56750b9d61
gbaranski <gbaranski19@gmail.com>
2020-08-25 01:59:00 +0200

    Updated style of login page, need to handle redirects

--- app_mobile/lib/screens/auth/sign_in.dart
+++ app_mobile/lib/screens/auth/sign_in.dart
@@ -26,88 +26,100 @@ class _SignInState extends State<SignIn> {
   Widget build(BuildContext context) {
     return Scaffold(body: LayoutBuilder(
         builder: (BuildContext context, BoxConstraints viewportConstraints) {
-      return Container(
-          decoration: BoxDecoration(
-              gradient: LinearGradient(
-                  begin: Alignment.topCenter,
-                  end: Alignment.bottomCenter,
-                  colors: _gradientColors,
-                  stops: [0.0, 0.9])),
-          padding: EdgeInsets.symmetric(
-            horizontal: 50,
+      return SingleChildScrollView(
+        child: ConstrainedBox(
+          constraints: BoxConstraints(
+            minHeight: viewportConstraints.maxHeight,
+            minWidth: viewportConstraints.minWidth,
           ),
-          child: SingleChildScrollView(
-            child: ConstrainedBox(
-              constraints: BoxConstraints(
-                minHeight: viewportConstraints.maxHeight,
-                minWidth: viewportConstraints.minWidth,
+          child: IntrinsicHeight(
+            child: Container(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 20,
               ),
-              child: IntrinsicHeight(
-                child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
-                    children: [
-                      SizedBox(
-                        height: 50,
-                      ),
-                      // TODO move to constants
-                      SvgPicture.asset(
-                        'assets/images/logo.svg',
-                        semanticsLabel: "Logo",
-                        height: 200,
-                      ),
-                      SizedBox(height: 30),
-                      RaisedButton(
-                        child: Text("Sign in anonymously"),
-                        onPressed: () async {
-                          dynamic result = await _authService.signInAnon();
-                          if (result == null) {
-                            print('Error signing in');
-                          } else {
-                            print('Signed in');
-                            print(result.uid);
-                          }
-                        },
-                      ),
-                      Form(
-                          key: _formKey,
-                          child: Column(children: <Widget>[
-                            LoginForm(
-                              onSubmit: _authService.signInWithEmailAndPassword,
-                              submitMessage: 'Log in',
-                              formKey: _formKey,
-                            ),
-                          ])),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Column(
-                            children: [
-                              Text("Forgot password?"),
-                            ],
+              child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    SizedBox(
+                      height: 50,
+                    ),
+                    // TODO move to constants
+                    SvgPicture.asset(
+                      'assets/images/logo.svg',
+                      semanticsLabel: "Logo",
+                      height: 200,
+                    ),
+                    SizedBox(height: 30),
+//                      RaisedButton(
+//                        child: Text("Sign in anonymously"),
+//                        onPressed: () async {
+//                          dynamic result = await _authService.signInAnon();
+//                          if (result == null) {
+//                            print('Error signing in');
+//                          } else {
+//                            print('Signed in');
+//                            print(result.uid);
+//                          }
+//                        },
+//                      ),
+                    Form(
+                        key: _formKey,
+                        child: Column(children: <Widget>[
+                          LoginForm(
+                            onSubmit: _authService.signInWithEmailAndPassword,
+                            submitMessage: 'LOG IN',
+                            formKey: _formKey,
                           ),
-                          Column(
-                            children: [
-                              Row(children: [
-                                Text("New user? "),
+                        ])),
+                    SizedBox(
+                      height: 15,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Column(
+                          children: [
+                            Row(
+                              children: [
                                 Text(
-                                  "SIGN UP",
+                                  "Reset",
                                   style: TextStyle(
+                                    color: Colors.blueAccent[200],
                                     fontWeight: FontWeight.w700,
                                   ),
                                 ),
-                              ])
-                            ],
-                          )
-                        ],
-                      ),
+                                Text(
+                                  " password!",
+                                ),
+                              ],
+                            )
+                          ],
+                        ),
+                        Column(
+                          children: [
+                            Row(children: [
+                              Text("New user? "),
+                              Text(
+                                "SIGN UP",
+                                style: TextStyle(
+                                  color: Colors.blueAccent[200],
+                                  fontWeight: FontWeight.w700,
+                                ),
+                              ),
+                            ])
+                          ],
+                        )
+                      ],
+                    ),
 
-                      SizedBox(
-                        height: 50,
-                      ),
-                    ]),
-              ),
+                    SizedBox(
+                      height: 50,
+                    ),
+                  ]),
             ),
-          ));
+          ),
+        ),
+      );
     }));
   }
 }
