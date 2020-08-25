7624aa653cf98ff126b4806b3068916aaf4ddb9f
gbaranski <gbaranski19@gmail.com>
2020-08-25 05:43:21 +0200

    Add splashscreen, fix flashing login page when logged

--- app_mobile/lib/services/auth.dart
+++ app_mobile/lib/services/auth.dart
@@ -1,10 +1,13 @@
 import 'package:app_mobile/models/user.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart' as auth;
+import 'package:flutter/cupertino.dart';
 
-class AuthService {
+class AuthService extends ChangeNotifier {
   final auth.FirebaseAuth _auth = auth.FirebaseAuth.instance;
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  bool isInitialized = false;
 
   Future<FirebaseUser> _convertToFirebaseUser(auth.User user) async {
     if (user == null) return null;
@@ -23,6 +26,11 @@ class AuthService {
 
   // auth change user stream
   Stream<auth.User> get user {
+    _auth.authStateChanges().listen((event) {
+      isInitialized = true;
+      print("Initialized");
+      notifyListeners();
+    });
     return _auth.authStateChanges();
   }
 
