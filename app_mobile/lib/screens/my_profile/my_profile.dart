1f9551f266cbd74c367b55ab0b592f7b9d057776
gbaranski <gbaranski19@gmail.com>
2020-08-25 17:08:02 +0200

    Add currentuser in auth provider, done my_profile

--- app_mobile/lib/screens/my_profile/my_profile.dart
+++ app_mobile/lib/screens/my_profile/my_profile.dart
@@ -1,17 +1,60 @@
 import 'package:app_mobile/services/auth.dart';
 import 'package:app_mobile/shared/constants.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 
 class MyProfile extends StatelessWidget {
+  Widget noProfileImage(BuildContext context) {
+    return SizedBox(
+      height: 200,
+      width: 200,
+      child: DecoratedBox(
+        decoration: const BoxDecoration(
+            color: Colors.black26,
+            borderRadius: BorderRadius.all(Radius.circular(100))),
+        child: Icon(
+          Icons.person_outline,
+          color: Colors.white,
+          size: 150,
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Consumer<AuthService>(
-      builder: (context, model, child) {
+      builder: (_context, model, child) {
         return Container(
+          padding: const EdgeInsets.only(top: 20),
+          alignment: Alignment.topCenter,
           child: Column(
             children: [
-              Text("UID: ${model.firebaseUser.uid}"),
+              model.currentUser.photoURL == null
+                  ? noProfileImage(context)
+                  : Image.network(model.currentUser.photoURL),
+              SizedBox(
+                height: 10,
+              ),
+              GestureDetector(
+                onLongPress: () {
+                  Clipboard.setData(ClipboardData(text: model.currentUser.uid))
+                      .then((_) {
+                    HapticFeedback.vibrate();
+                    final snackBar = SnackBar(
+                      content: Text("UID copied to clipboard"),
+                    );
+                    Scaffold.of(context).showSnackBar(snackBar);
+                  });
+                },
+                child: Text(
+                  model.currentUser.displayName,
+                  style: const TextStyle(
+                    fontSize: 26,
+                  ),
+                ),
+              ),
               RaisedButton(
                 color: LayoutBlueColor1,
                 textColor: Colors.white,
--- app_mobile/lib/services/auth.dart
+++ app_mobile/lib/services/auth.dart
@@ -1,11 +1,12 @@
+import 'package:flutter/material.dart';
 import 'package:app_mobile/models/user.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart' as auth;
-import 'package:flutter/cupertino.dart';
 
 class AuthService extends ChangeNotifier {
   final auth.FirebaseAuth _auth = auth.FirebaseAuth.instance;
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  auth.User currentUser;
   FirebaseUser firebaseUser;
 
   AuthStatus authStatus = AuthStatus.NOT_DETERMINED;
@@ -32,6 +33,7 @@ class AuthService extends ChangeNotifier {
         authStatus = AuthStatus.NOT_LOGGED_IN;
       } else {
         firebaseUser = await _convertToFirebaseUser(event);
+        currentUser = event;
         authStatus = AuthStatus.LOGGED_IN;
       }
       print("Initialized");
@@ -67,6 +69,7 @@ class AuthService extends ChangeNotifier {
 
   Future signOut() async {
     try {
+      firebaseUser = null;
       return await _auth.signOut();
     } catch (e) {
       print(e.toString());
