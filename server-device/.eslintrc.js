7d341a2529689960e777b228097655eeb2d303d0
gbaranski <gbaranski19@gmail.com>
2020-08-29 16:02:56 +0200

    Initialized eslint configs

---
+++ server-device/.eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  env: {
+    es2020: true,
+    node: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: [
+    '@typescript-eslint',
+  ],
+  rules: {
+  },
+};
