33357117ef6bb1b361c4ff045c20feccc99c39f0
gbaranski <gbaranski19@gmail.com>
2020-08-30 21:16:45 +0200

    basic mqtt communication works

--- devices/watermixer-esp8266/src/mqtt.h
+++ devices/watermixer-esp8266/src/mqtt.h
@@ -7,9 +7,71 @@
 #ifndef MQTT_H
 #pragma once
 
-void initializeMqtt(WiFiClient espClient) {
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+long lastMsg = 0;
+#define MSG_BUFFER_SIZE (50)
+char msg[MSG_BUFFER_SIZE];
+int value = 0;
+
+void callback(char* topic, byte* message, unsigned int length) {
+  Serial.println("message arrived on topic:");
+  Serial.print(topic);
+  Serial.print(". Message: ");
+  String messageTemp;
+
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)message[i]);
+    messageTemp += (char)message[i];
+  }
+  Serial.println();
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Create a random client ID
+    String clientId = "ESP8266Client-";
+    clientId += String(random(0xffff), HEX);
+    // Attempt to connect
+    if (client.connect(clientId.c_str())) {
+      Serial.println("connected");
+      // Once connected, publish an announcement...
+      client.publish("outTopic", "hello world");
+      // ... and resubscribe
+      client.subscribe("inTopic");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
+void initializeMqtt() {
   Serial.println("Initializing MQTT");
-  PubSubClient client(espClient);
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
+}
+
+void mqttLoop() {
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+  unsigned long now = millis();
+  if (now - lastMsg > 2000) {
+    lastMsg = now;
+    ++value;
+    snprintf(msg, MSG_BUFFER_SIZE, "hello world #%ld", value);
+    Serial.print("Publish message: ");
+    Serial.println(msg);
+    client.publish("outTopic", msg);
+  }
 }
 
 #endif
