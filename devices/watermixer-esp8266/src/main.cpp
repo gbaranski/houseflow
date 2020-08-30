adbffb9a8746f553615d4731e297d89b76ed78dc
gbaranski <gbaranski19@gmail.com>
2020-08-30 21:16:45 +0200

    basic mqtt communication works

--- devices/watermixer-esp8266/src/main.cpp
+++ devices/watermixer-esp8266/src/main.cpp
@@ -9,8 +9,6 @@
 #include "config.h"
 #include "mqtt.h"
 
-WiFiClient espClient;
-
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
@@ -19,9 +17,10 @@ void setup() {
   wifiManager.autoConnect();
 
   Serial.println("Connected to WiFi");
-  initializeMqtt(espClient);
+  initializeMqtt();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  mqttLoop();
 }
