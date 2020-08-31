167e4e08db9d26e5a74ac83959189f6b073fd18a
gbaranski <gbaranski19@gmail.com>
2020-09-01 01:52:33 +0200

    add OTA updates, downloading binary doesn't work yet

---
+++ devices/watermixer-esp8266/src/ota.h
@@ -0,0 +1,49 @@
+#include <Arduino.h>
+#include <ESP8266httpUpdate.h>
+
+#include "config.h"
+
+void update_started() {
+  Serial.println("CALLBACK:  HTTP update process started");
+}
+
+void update_finished() {
+  Serial.println("CALLBACK:  HTTP update process finished");
+}
+
+void update_progress(int cur, int total) {
+  Serial.printf("CALLBACK:  HTTP update process at %d of %d bytes...\n", cur,
+                total);
+}
+
+void update_error(int err) {
+  Serial.printf("CALLBACK:  HTTP update fatal error code %d\n", err);
+}
+
+void checkUpdates() {
+  t_httpUpdate_return ret;
+
+  ESPhttpUpdate.rebootOnUpdate(false);
+  ESPhttpUpdate.onStart(update_started);
+  ESPhttpUpdate.onEnd(update_finished);
+  ESPhttpUpdate.onProgress(update_progress);
+  ESPhttpUpdate.onError(update_error);
+
+  ret = ESPhttpUpdate.update(UPDATE_IP, UPDATE_PORT, UPDATE_PATH, VERSION);
+
+  switch (ret) {
+    case HTTP_UPDATE_FAILED:
+      Serial.printf("HTTP_UPDATE_FAILD Error (%d): %s\n",
+                    ESPhttpUpdate.getLastError(),
+                    ESPhttpUpdate.getLastErrorString().c_str());
+      break;
+
+    case HTTP_UPDATE_NO_UPDATES:
+      Serial.println("HTTP_UPDATE_NO_UPDATES");
+      break;
+
+    case HTTP_UPDATE_OK:
+      Serial.println("HTTP_UPDATE_OK");
+      break;
+  }
+}
