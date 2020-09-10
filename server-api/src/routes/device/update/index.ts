72251d2d9f98dfe30e675ef350c3196072839f87
gbaranski <gbaranski19@gmail.com>
2020-09-10 02:18:52 +0200

    working OTA updates, checking md5 doesnt work well

--- server-api/src/routes/device/update/index.ts
+++ server-api/src/routes/device/update/index.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response, NextFunction } from 'express';
-import esp8266Router from './esp8266'
+import fs, { readSync } from 'fs'
+import { findBinaryFile } from './misc';
 
 const router = express.Router();
 
@@ -8,6 +9,25 @@ router.use((err: Error, req: Request, res: Response, next: NextFunction) => {
     res.sendStatus(401);
 })
 
-router.use('/esp8266', esp8266Router);
+router.use('/esp8266', async (req, res): Promise<any> => {
+
+    const staMac = req.get('x-esp8266-sta-mac');
+    const sdkVersion = req.get("x-esp8266-sdk-version");
+    const sketchMd5 = req.get('x-esp8266-sketch-md5');
+    if (!sdkVersion) throw new Error("SDK Version not defined");
+    if (!staMac) throw new Error("STA Mac not defined");
+    if (!sketchMd5) throw new Error("Sketch MD5 not defined");
+
+    try {
+        const file = await findBinaryFile(staMac);
+        console.log({ devMd5: sketchMd5, fMd5: file.md5 });
+        if (file.md5 === sketchMd5) return res.sendStatus(304);
+
+        res.download(file.path, (err) => err !== undefined ?? console.log(err));
+    } catch (e) {
+        console.log(e.message);
+        res.sendStatus(500);
+    }
+});
 
 export default router;
