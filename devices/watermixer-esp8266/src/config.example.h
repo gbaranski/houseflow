940eca8cb6cd0758c66c22804cfb1bc5738bc439
gbaranski <gbaranski19@gmail.com>
2020-09-13 21:27:52 +0200

    update example config

--- devices/watermixer-esp8266/src/config.example.h
+++ devices/watermixer-esp8266/src/config.example.h
@@ -8,9 +8,12 @@
 #define DEVICE_UID "UID"
 #define DEVICE_SECRET "SECRET"
 #define ON_CONNECT_TOPIC "on/connected"
+#define UPDATE_URL "http://IP:PORT/update/esp8266"
 
 const String START_MIX_TOPIC = DEVICE_UID "/event/todevice/startmix";
 const char* ON_CONNECT_JSON =
     "{\"uid\": \"" DEVICE_UID "\",\"secret\":\"" DEVICE_SECRET "\"}";
+
+#define VERSION ON_CONNECT_JSON
 
 #endif
