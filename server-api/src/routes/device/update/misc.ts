c04cc43546404586d39d156d21dd2782095aaad8
gbaranski <gbaranski19@gmail.com>
2020-09-13 20:45:15 +0200

    Optimized code, OTA is fully working

--- server-api/src/routes/device/update/misc.ts
+++ server-api/src/routes/device/update/misc.ts
@@ -1,21 +1,40 @@
 import fs from 'fs-extra';
 import md5File from 'md5-file';
+import { Response } from 'express';
+import path from 'path';
 
-const binPath = `${process.env.PWD}/bin`
+const binPath = `${process.env.PWD}/bin`;
 
-export async function findBinaryFile(type: string): Promise<{ path: string; md5: string; size: number }> {
-    const binFilePath = `${binPath}/${type.toLowerCase()}.bin`;
+export interface File {
+  path: string;
+  md5: string;
+  size: number;
+}
 
-    const binary = await fs.pathExists(binFilePath);
-    if (!binary) throw new Error(`Binary not found ${binFilePath}`);
-    const stats = await fs.stat(binFilePath);
+export async function findBinaryFile(type: string): Promise<File> {
+  const binFilePath = `${binPath}/${type.toLowerCase()}.bin`;
 
-    const size = stats['size'];
-    const md5 = md5File(binFilePath);
+  const binary = await fs.pathExists(binFilePath);
+  if (!binary) throw new Error(`Binary not found ${binFilePath}`);
+  const stats = await fs.stat(binFilePath);
 
-    return {
-        md5: await md5,
-        path: binFilePath,
-        size,
-    };
+  const size = stats['size'];
+  const md5 = md5File(binFilePath);
+
+  return {
+    md5: await md5,
+    path: binFilePath,
+    size,
+  };
+}
+
+export async function sendBinaryFile(res: Response, file: File) {
+  res.set('Content-Type', 'application/octet-stream');
+  res.set(
+    'Content-Disposition',
+    `attachment;filename=${path.basename(file.path)}`,
+  );
+  res.set('Content-Length', String(file.size));
+  res.set('x-MD5', file.md5);
+  res.sendFile(file.path);
 }
