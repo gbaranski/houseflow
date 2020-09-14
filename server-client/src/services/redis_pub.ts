8aa53d4547d7511d4d7dfa6136e712c11a6351f3
gbaranski <gbaranski19@gmail.com>
2020-09-14 22:51:04 +0200

    add support for sending requests

--- server-client/src/services/redis_pub.ts
+++
@@ -1,9 +0,0 @@
-import redis from 'redis';
-import { Client, Device } from '@gbaranski/types';
-
-const redisPublisher = redis.createClient('redis://redis:6379');
-redisPublisher.on('connect', () => console.log('Initialized redis publisher'));
-
-export const publishRequest = (request: Device.RequestDevice) => {
-  redisPublisher.publish('request', JSON.stringify(request));
-};
