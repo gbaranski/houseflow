0f81b3c50d9d975160ea45c436d16c0670f4c01e
gbaranski <gbaranski19@gmail.com>
2020-09-14 23:53:33 +0200

    add refreshing initialState

--- app-web/src/pages/user/login/components/loginGoogle/index.tsx
+++ app-web/src/pages/user/login/components/loginGoogle/index.tsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import { Button, message } from 'antd';
-import { history } from 'umi';
+import { history, useModel } from 'umi';
 import { GoogleOutlined } from '@ant-design/icons';
 import { signToGoogleWithPopup } from '@/services/firebase';
 
 export default function LoginGoogle() {
+  const initialState = useModel('@@initialState');
   const handleSubmit = async () => {
     try {
       const credentials = await signToGoogleWithPopup();
       if (!credentials.user) throw new Error('Current user not defined');
       message.info('User logged in');
       history.replace('/welcome');
+      initialState.refresh();
     } catch (e) {
       message.error(e.message);
     }
