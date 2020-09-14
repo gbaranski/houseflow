ea4b72f3256cac6980471e7ae8789d6928ecc7a1
gbaranski <gbaranski19@gmail.com>
2020-09-14 21:57:45 +0200

    update displaying watermixer data

--- app-web/src/components/Watermixer/card.tsx
+++ app-web/src/components/Watermixer/card.tsx
@@ -3,13 +3,14 @@ import { Card, Statistic, Row, Col, Tooltip } from 'antd';
 import { CoffeeOutlined } from '@ant-design/icons';
 import { useModel } from 'umi';
 import { Device, Watermixer } from '@gbaranski/types';
-import { parseSeconds, parseWaterBoolean } from '@/utils/utils';
 
 interface WatermixerCardProps {
   device: Device.ActiveDevice<Watermixer.Data>;
 }
 const WatermixerCard: React.FC<WatermixerCardProps> = ({ device }) => {
   const { mixWater } = useModel('watermixer');
+
+  const hasElapsed = (timestamp: number) => Date.now() > timestamp;
 
   return (
     <Card
@@ -26,11 +27,11 @@ const WatermixerCard: React.FC<WatermixerCardProps> = ({ device }) => {
         <Col span={10} style={{ textAlign: 'left' }}>
           <Statistic
             title="Mixing state"
-            value={parseWaterBoolean(device.data.finishMixTimestamp)}
+            value={hasElapsed(device.data.finishMixTimestamp) ? 'Idle' : 'Mixing!'}
           />
         </Col>
         <Col span={10} style={{ textAlign: 'right' }}>
-          <Statistic title="Time left" value={parseSeconds(device.data.remainingSeconds)} />
+          <Statistic title="Time left" value={device.data.finishMixTimestamp} />
         </Col>
       </Row>
     </Card>
