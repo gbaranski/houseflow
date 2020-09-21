d6c991a507f626e5ab06094e440eb73a786c2389
gbaranski <gbaranski19@gmail.com>
2020-09-21 13:26:58 +0200

    Update types

--- types/src/client.ts
+++ types/src/client.ts
@@ -1,23 +1,9 @@
-import { Device } from '.';
-import { DocumentReference } from '@firebase/firestore-types';
-import { CurrentConnections } from './misc';
-
 export namespace Client {
-  export type RequestType = 'CONNECTIONS';
-
-  export type ResponseType = 'DATA' | 'CONNECTIONS';
   export type UserRole = 'admin' | 'moderator' | 'user';
 
-  export interface Response {
-    requestType: ResponseType;
-    data?: Device.ActiveDevice[] | Device.FirebaseDevice[] | CurrentConnections;
-  }
   export interface FirebaseUser {
-    devices: DocumentReference[];
+    devices: { notification: boolean; uid: boolean }[];
     role: UserRole;
     uid: string;
-  }
-  export interface ActiveUser extends FirebaseUser {
-    ip: string;
   }
 }
