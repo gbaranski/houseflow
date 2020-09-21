25874ab11a2586f8db0fcc1233ea8a1b65073ca8
gbaranski <gbaranski19@gmail.com>
2020-09-21 13:37:37 +0200

    Add type for firebaseuserdevice

--- types/src/client.ts
+++ types/src/client.ts
@@ -1,8 +1,13 @@
 export namespace Client {
   export type UserRole = 'admin' | 'moderator' | 'user';
 
+  export interface FirebaseUserDevice {
+    notification: boolean;
+    uid: boolean;
+  }
+
   export interface FirebaseUser {
-    devices: { notification: boolean; uid: boolean }[];
+    devices: FirebaseUserDevice[];
     role: UserRole;
     uid: string;
   }
