44680d0f0a67ae9b006aa5a9ffbd1442af9265d5
gbaranski <gbaranski19@gmail.com>
2020-09-22 02:35:15 +0200

    update changing state of devices

--- app-web/src/models/deviceData.ts
+++ app-web/src/models/deviceData.ts
@@ -1,16 +1,32 @@
-import { useState } from 'react';
+import { useCallback, useReducer } from 'react';
 import { Client, Device } from '@gbaranski/types';
 import { subscribeAllowedDevices } from '@/services/firebase';
 
-export default () => {
-  const [firebaseDevices, setFirebaseDevices] = useState<Device.FirebaseDevice[]>([]);
+type FirebaseDevicesAction = { type: 'UPDATE' | 'DELETE'; firebaseDevice: Device.FirebaseDevice };
 
-  const onFirebaseDeviceUpdate = (newDevice: Device.FirebaseDevice) => {
-    const newFirebaseDevices = firebaseDevices
-      .filter((device) => device.uid !== newDevice.uid)
-      .concat(newDevice);
-    setFirebaseDevices(newFirebaseDevices);
-  };
+const firebaseDevicesReducer = (
+  firebaseDevices: Device.FirebaseDevice[],
+  action: FirebaseDevicesAction,
+) => {
+  switch (action.type) {
+    case 'UPDATE':
+      return firebaseDevices
+        .filter((_firebaseDevice) => _firebaseDevice.uid !== action.firebaseDevice.uid)
+        .concat(action.firebaseDevice);
+    case 'DELETE':
+      return firebaseDevices.filter(
+        (_firebaseDevice) => _firebaseDevice.uid !== action.firebaseDevice.uid,
+      );
+    default:
+      throw new Error('Wrong action type');
+  }
+};
+export default () => {
+  const [firebaseDevices, dispatchFirebaseDevices] = useReducer(firebaseDevicesReducer, []);
+
+  const onFirebaseDeviceUpdate = useCallback((newDevice: Device.FirebaseDevice) => {
+    dispatchFirebaseDevices({ type: 'UPDATE', firebaseDevice: newDevice });
+  }, []);
 
   const initializeFirebaseDevices = (firebaseUser: Client.FirebaseUser) => {
     subscribeAllowedDevices(firebaseUser, onFirebaseDeviceUpdate);
@@ -18,7 +34,7 @@ export default () => {
 
   return {
     firebaseDevices,
-    setFirebaseDevices,
+    dispatchFirebaseDevices,
     initializeFirebaseDevices,
   };
 };
