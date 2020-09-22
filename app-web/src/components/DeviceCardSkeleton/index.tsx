feb60a2c0f127d0ac9d58ae0b2d92e4a8fcbc017
gbaranski <gbaranski19@gmail.com>
2020-09-22 02:38:37 +0200

    fix capitalization of inactive device

--- app-web/src/components/DeviceCardSkeleton/index.tsx
+++ app-web/src/components/DeviceCardSkeleton/index.tsx
@@ -2,11 +2,12 @@ import React from 'react';
 import { Card, Empty } from 'antd';
 import { Link } from 'umi';
 import { EnterOutlined } from '@ant-design/icons';
+import { capitalizeFirst } from '@/utils/utils';
 
 export default (props: { name: string }) => {
   return (
     <Card
-      title={props.name}
+      title={capitalizeFirst(props.name.toLowerCase())}
       style={{ width: 300 }}
       bodyStyle={{ minHeight: 180 }}
       actions={[
