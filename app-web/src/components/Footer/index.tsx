e3ba5efe107928aa70f518a292951711c9e50a0d
gbaranski <gbaranski19@gmail.com>
2020-09-22 02:51:00 +0200

    update footer

--- app-web/src/components/Footer/index.tsx
+++ app-web/src/components/Footer/index.tsx
@@ -4,24 +4,18 @@ import { DefaultFooter } from '@ant-design/pro-layout';
 
 export default () => (
   <DefaultFooter
-    copyright="2019 蚂蚁金服体验技术部出品"
+    copyright={new Date().getFullYear().toString()}
     links={[
       {
-        key: 'Ant Design Pro',
-        title: 'Ant Design Pro',
-        href: 'https://pro.ant.design',
+        key: 'website',
+        title: 'Control Home',
+        href: 'https://github.com/gbaranski/Control-Home',
         blankTarget: true,
       },
       {
         key: 'github',
         title: <GithubOutlined />,
-        href: 'https://github.com/ant-design/ant-design-pro',
-        blankTarget: true,
-      },
-      {
-        key: 'Ant Design',
-        title: 'Ant Design',
-        href: 'https://ant.design',
+        href: 'https://github.com/gbaranski/Control-Home',
         blankTarget: true,
       },
     ]}
