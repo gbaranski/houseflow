21edc48362eb820c0710e8c6a189906ccc47c4f1
gbaranski <gbaranski19@gmail.com>
2020-09-23 00:23:12 +0200

    Remove few types, add new requesty type, moved retreiving topics to types

--- types/src/device.ts
+++ types/src/device.ts
@@ -6,30 +6,8 @@ export type AnyDeviceData = Alarmclock.Data | Watermixer.Data;
 export namespace Device {
   export type DeviceType = 'ALARMCLOCK' | 'WATERMIXER' | 'GATE' | 'GARAGE';
 
-  export type RequestType =
-    | 'GET_DATA'
-    | 'START_MIXING'
-    | 'SET_TIME'
-    | 'SET_STATE'
-    | 'TEST_SIREN'
-    | 'REBOOT'
-    | 'UNKNOWN';
-
-  export type RequestTopic = 'startmix';
-
-  export interface RequestDevice {
-    topic: {
-      name: RequestTopic;
-      uid: Uid;
-    };
-    data?: DateTime | State;
-  }
-
-  export interface ResponseDevice<
-    T extends Alarmclock.Data | Watermixer.Data | undefined
-  > {
-    responseFor: RequestType;
-    data: T;
+  export interface Request {
+    correlationData: string;
   }
 
   export interface FirebaseDevice<
--- types/src/watermixer.ts
+++ types/src/watermixer.ts
@@ -1,3 +1,5 @@
+import { Topic } from '.';
+
 export namespace Watermixer {
   export interface Data {
     finishMixTimestamp: number;
@@ -6,4 +8,9 @@ export namespace Watermixer {
   export const SAMPLE: Data = {
     finishMixTimestamp: 0,
   };
+
+  export const getStartMixingTopic = (uid: string): Topic => ({
+    request: `${uid}/event/startmix/request`,
+    response: `${uid}/event/startmix/response`,
+  });
 }
