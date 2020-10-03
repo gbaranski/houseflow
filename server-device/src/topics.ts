04ebf0486162292f21fa6f7dc36e1ee37bb65611
gbaranski <gbaranski19@gmail.com>
2020-10-03 12:45:10 +0200

    Removed server-device and server-client

--- server-device/src/topics.ts
+++
@@ -1,4 +0,0 @@
-import { Device } from "@gbaranski/types";
-
-export const getEventTopic = (req: Device.RequestDevice) => `${req.topic.uid}/event/todevice/${req.topic.name}`;
-export const ON_CONNECTED_TOPIC = 'on/connected';
