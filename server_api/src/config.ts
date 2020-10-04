8f0e088fa4bf9f36e14b4054300959ff85898ae3
gbaranski <gbaranski19@gmail.com>
2020-10-04 11:23:51 +0200

    remove server_api

--- server_api/src/config.ts
+++
@@ -1,4 +0,0 @@
-export const CORS_WHITELIST = [
-  'https://control.gbaranski.com',
-  'http://localhost:3000',
-];
--- server_api/src/routes/mqtt/index.ts
+++
@@ -1,7 +0,0 @@
-import express from 'express';
-import mqttAuthRouter from './auth';
-
-const router = express.Router();
-
-router.use('/auth', mqttAuthRouter);
-export default router;
