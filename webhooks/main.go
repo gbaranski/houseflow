265526484c24578f461f3fe37ee1e49367ab783d
gbaranski <gbaranski19@gmail.com>
2020-10-06 23:09:45 +0200

    webhooks now using port env variable

--- webhooks/main.go
+++ webhooks/main.go
@@ -4,17 +4,21 @@ import (
 	"context"
 	"log"
 	"net/http"
+	"os"
 
 	server "github.com/gbaranski/homeflow/webhooks/server"
 	services "github.com/gbaranski/homeflow/webhooks/services"
 )
 
 func main() {
+	PORT := os.Getenv("PORT_WEBHOOKS")
+	if len(PORT) == 0 {
+		log.Fatalln("Port is not defined")
+	}
 	firebaseClient, error := services.InitFirebase(context.Background())
 	if error != nil {
 		log.Fatalln(error)
 	}
-	PORT := "8001"
 	log.Println("Starting webhooks")
 	http.HandleFunc("/event", func(w http.ResponseWriter, req *http.Request) {
 		server.OnEvent(w, req, firebaseClient)
