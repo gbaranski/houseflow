ec229670ff0ed1d5de35b332c7923d8447af250f
gbaranski <gbaranski19@gmail.com>
2020-10-08 00:09:35 +0200

    Fix ws url

--- app_web/src/services/mqtt.ts
+++ app_web/src/services/mqtt.ts
@@ -1,7 +1,10 @@
 import { getRandomShortUid } from '@/utils/utils';
 import { connect, MqttClient } from 'mqtt';
 
-const BROKER_URL = 'wss://localhost:8084/mqtt';
+const BROKER_URL =
+  process.env.NODE_ENV === 'development'
+    ? 'wss://localhost:8084/mqtt'
+    : 'wss://api.gbaranski.com:8084/mqtt';
 
 export const connectMqtt = (token: string, uid: string) => {
   return new Promise<MqttClient>((resolve, reject) => {
