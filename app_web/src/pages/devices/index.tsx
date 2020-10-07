929c70eef5d5e90fe7d30156e0d0d2af023bcdee
gbaranski <gbaranski19@gmail.com>
2020-10-07 22:27:26 +0200

    Fix showing empty when user has no devices

--- app_web/src/pages/devices/index.tsx
+++ app_web/src/pages/devices/index.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { PageContainer, PageLoading } from '@ant-design/pro-layout';
-import { Card, Row } from 'antd';
+import { Card, Empty, Row } from 'antd';
 import { useModel } from 'umi';
 import DeviceList from '@/components/DevicesList';
 
@@ -17,9 +17,19 @@ export default (): React.ReactNode => {
   return (
     <PageContainer>
       <Card>
-        <Row>
-          <DeviceList firebaseDevices={firebaseDevices} />
-        </Row>
+        {firebaseDevices.length > 1 ? (
+          <Row>
+            <DeviceList firebaseDevices={firebaseDevices} />
+          </Row>
+        ) : (
+          <Row justify="center">
+            <Empty
+              style={{ color: 'rgba(0,0,0,.50)' }}
+              // TODO: Add some link for contact
+              description="No devices, if you think thats an issue, contact us"
+            />
+          </Row>
+        )}
       </Card>
     </PageContainer>
   );
