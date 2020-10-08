c7d8ec882dadff3fe922563d083dbea64fbdf332
gbaranski <gbaranski19@gmail.com>
2020-10-08 20:40:16 +0200

    Update MQTT on Web

--- app_web/src/services/mqtt.ts
+++ app_web/src/services/mqtt.ts
@@ -1,18 +1,18 @@
 import { getRandomShortUid } from '@/utils/utils';
-import { connect, MqttClient } from 'mqtt';
+import mqtt from 'mqtt';
 
-const BROKER_URL =
-  process.env.NODE_ENV === 'development'
-    ? 'wss://localhost:8084/mqtt'
-    : 'wss://api.gbaranski.com:8084/mqtt';
+const BROKER_DEV_URL = 'wss://localhost:8084/mqtt';
+const BROKER_PROD_URL = 'wss://api.gbaranski.com:8084/mqtt';
+
+const BROKER_URL = process.env.NODE_ENV === 'development' ? BROKER_DEV_URL : BROKER_PROD_URL;
 
 export const connectMqtt = (token: string, uid: string) => {
-  return new Promise<MqttClient>((resolve, reject) => {
-    const client = connect(BROKER_URL, {
+  return new Promise<mqtt.MqttClient>((resolve, reject) => {
+    const client = mqtt.connect(BROKER_PROD_URL, {
       clientId: `web_ + ${getRandomShortUid()}`,
       username: uid,
+
       password: token,
-      protocolVersion: 5,
     });
     client.on('connect', () => {
       resolve(client);
