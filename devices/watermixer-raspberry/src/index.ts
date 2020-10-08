12d0d4d62cbbbc30373482f97766eb515e5bc263
gbaranski <gbaranski19@gmail.com>
2020-10-08 18:40:45 +0200

    Update watermixer

--- devices/watermixer-raspberry/src/index.ts
+++ devices/watermixer-raspberry/src/index.ts
@@ -1,5 +1,4 @@
 import mqtt from 'mqtt';
-import fs from 'fs';
 import { onConnection } from './app';
 
 const { DEVICE_UID, DEVICE_SECRET, MQTT_URL } = process.env;
@@ -8,16 +7,11 @@ if (!DEVICE_UID || !DEVICE_SECRET || !MQTT_URL) {
   throw new Error('DEVICE_UID or DEVICE_SECRET are not defined in .env');
 }
 
-const caCert = fs.readFileSync('../../certs/ca.pem', 'utf-8');
-if (!caCert) throw new Error('ca.pem is empty or not exists in certs/ca.pem');
-
 const mqttClient = mqtt.connect('wss://api.gbaranski.com:8084/mqtt', {
   clientId: 'device_' + Math.random().toString(16).substr(2, 8),
   username: DEVICE_UID,
   password: DEVICE_SECRET,
   protocolVersion: 4,
-  ca: caCert,
-  clean: true,
 });
 
 mqttClient.on('error', console.log);
