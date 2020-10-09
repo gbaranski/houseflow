e3b0b9601a557901e27f370079eb2fdea6a4a2ab
gbaranski <gbaranski19@gmail.com>
2020-10-09 13:11:02 +0200

    Update settings

--- app_mobile/lib/screens/settings/settings.dart
+++ app_mobile/lib/screens/settings/settings.dart
@@ -1,5 +1,8 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:homeflow/models/device.dart';
+import 'package:homeflow/models/user.dart';
 import 'package:homeflow/services/auth.dart';
+import 'package:homeflow/services/firebase.dart';
 import 'package:homeflow/shared/device_icon.dart';
 import 'package:homeflow/utils/misc.dart';
 import 'package:flutter/material.dart';
@@ -8,24 +11,34 @@ import 'package:provider/provider.dart';
 class Settings extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Text("Hello");
-    // return Consumer<AuthService>(builder: (context, authModel, child) {
-    //   final deviceModel = Provider.of<DeviceService>(context, listen: false);
-    //   deviceModel.getFirebaseDevices(authModel.firebaseUser);
-    //   return ListView.builder(
-    //       itemCount: deviceModel.firebaseDevices.length,
-    //       itemBuilder: (context, index) {
-    //         final FirebaseDevice firebaseDevice =
-    //             deviceModel.firebaseDevices[index];
+    return Consumer<AuthService>(builder: (context, authModel, child) {
+      return ListView.builder(
+          itemCount: authModel.firebaseUser.devices.length,
+          itemBuilder: (context, index) {
+            final FirebaseUserDevice firebaseUserDevice =
+                authModel.firebaseUser.devices[index];
 
-    //         return ExpansionTile(
-    //           leading: DeviceIcon(firebaseDevice.type),
-    //           title: Text(upperFirstCharacter(firebaseDevice.type)),
-    //           children: [
-    //             Text("UID: ${firebaseDevice.uid}"),
-    //           ],
-    //         );
-    //       });
-    // });
+            return StreamBuilder(
+                stream: FirebaseService.getFirebaseDeviceSnapshot(
+                    firebaseUserDevice.uid),
+                builder: (BuildContext context,
+                    AsyncSnapshot<DocumentSnapshot> snapshot) {
+                  if (snapshot.hasError) return Text("Error occured");
+                  if (snapshot.connectionState == ConnectionState.waiting)
+                    return CircularProgressIndicator();
+                  final Map<String, dynamic> data = snapshot.data.data();
+                  final FirebaseDevice firebaseDevice =
+                      FirebaseDevice.fromMap(data);
+
+                  return ExpansionTile(
+                    leading: DeviceIcon(firebaseDevice.type),
+                    title: Text(upperFirstCharacter(firebaseDevice.type)),
+                    children: [
+                      Text("UID: ${firebaseDevice.uid}"),
+                    ],
+                  );
+                });
+          });
+    });
   }
 }
