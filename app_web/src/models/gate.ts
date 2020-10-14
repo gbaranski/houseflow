18d94b330790e8a83353807c392948379960b0b1
gbaranski <gbaranski19@gmail.com>
2020-10-14 13:44:33 +0200

    Promisify sending MQTT request web

--- app_web/src/models/gate.ts
+++ app_web/src/models/gate.ts
@@ -2,27 +2,29 @@ import { updateDeviceData } from '@/services/firebase';
 import { sendRequest } from '@/services/mqtt';
 import { getRandomShortUid } from '@/utils/utils';
 import { Device, Gate } from '@gbaranski/types';
+import { message } from 'antd';
 import { MqttClient } from 'mqtt';
 
 export default () => {
-  const openGate = (device: Device.FirebaseDevice, mqttClient: MqttClient) => {
+  const openGate = async (device: Device.FirebaseDevice, mqttClient: MqttClient) => {
     const request: Device.Request = {
       correlationData: getRandomShortUid(),
     };
 
-    const onSuccess = () => {
-      const lastOpenTimestamp = Date.now();
-      updateDeviceData(device.uid, {
-        lastOpenTimestamp,
+    try {
+      message.info('Sending!');
+      await sendRequest({
+        request,
+        topic: Gate.getOpenGateTopic(device.uid),
+        mqttClient,
       });
-    };
-
-    sendRequest({
-      request,
-      topic: Gate.getOpenGateTopic(device.uid),
-      mqttClient,
-      onSuccess,
-    });
+      updateDeviceData(device.uid, {
+        lastOpenTimestamp: Date.now(),
+      });
+    } catch (e) {
+      console.log(`Error when sending request ${e}`);
+      message.error(e.message);
+    }
   };
 
   return {
--- app_web/src/models/watermixer.ts
+++ app_web/src/models/watermixer.ts
@@ -2,6 +2,7 @@ import { updateDeviceData } from '@/services/firebase';
 import { sendRequest } from '@/services/mqtt';
 import { getRandomShortUid } from '@/utils/utils';
 import { Watermixer, Device } from '@gbaranski/types';
+import { message } from 'antd';
 import { MqttClient } from 'mqtt';
 
 const MILLIS_IN_SECOND = 1000;
@@ -9,24 +10,26 @@ const SECOND_IN_MINUTE = 60;
 const MIX_MINUTES = 10;
 
 export default () => {
-  const mixWater = (device: Device.FirebaseDevice, mqttClient: MqttClient) => {
+  const mixWater = async (device: Device.FirebaseDevice, mqttClient: MqttClient) => {
     const request: Device.Request = {
       correlationData: getRandomShortUid(),
     };
 
-    const onSuccess = () => {
+    try {
+      message.info('Sending!');
+      await sendRequest({
+        request,
+        topic: Watermixer.getStartMixingTopic(device.uid),
+        mqttClient,
+      });
       const finishMixTimestamp = Date.now() + MILLIS_IN_SECOND * SECOND_IN_MINUTE * MIX_MINUTES;
       updateDeviceData(device.uid, {
         finishMixTimestamp,
       });
-    };
-
-    sendRequest({
-      request,
-      topic: Watermixer.getStartMixingTopic(device.uid),
-      mqttClient,
-      onSuccess,
-    });
+    } catch (e) {
+      console.log(`Error when sending request ${e}`);
+      message.error(e.message);
+    }
   };
 
   return {
