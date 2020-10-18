1fc22df61a8dca3552fb02c0b5d8812fc602915d
gbaranski <gbaranski19@gmail.com>
2020-10-18 22:34:52 +0200

    remove bottom navigation

--- app_mobile/lib/screens/home/home.dart
+++ app_mobile/lib/screens/home/home.dart
@@ -1,68 +1,11 @@
-import 'package:flutter_svg/svg.dart';
 import 'package:houseflow/screens/dashboard/dashboard.dart';
 import 'package:houseflow/screens/my_profile/my_profile.dart';
-import 'package:houseflow/screens/settings/settings.dart';
 import 'package:houseflow/services/auth.dart';
-import 'package:houseflow/shared/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:houseflow/shared/profile_image.dart';
 import 'package:provider/provider.dart';
 
-class Home extends StatefulWidget {
-  @override
-  _HomeState createState() => _HomeState();
-}
-
-class _HomeState extends State<Home> {
-  static final List<Widget> _navPages = <Widget>[
-    Dashboard(),
-    Settings(),
-  ];
-
-  static const List<BottomNavigationBarItem> _navItems = [
-    const BottomNavigationBarItem(
-        icon: const Icon(
-          Icons.home,
-          size: 28,
-        ),
-        label: 'Home'),
-    const BottomNavigationBarItem(
-        icon: const Icon(
-          Icons.settings,
-          size: 28,
-        ),
-        label: 'Settings'),
-  ];
-
-  int _currentIndex = 0;
-
-  void onNavItemTap(int index) {
-    setState(() {
-      _currentIndex = index;
-    });
-  }
-
-  Widget navigation(BuildContext context) {
-    const BorderRadius borderRadius = BorderRadius.only(
-        topLeft: Radius.circular(30), topRight: Radius.circular(30));
-    return Container(
-      decoration: const BoxDecoration(borderRadius: borderRadius),
-      child: ClipRRect(
-        borderRadius: borderRadius,
-        child: BottomNavigationBar(
-          backgroundColor: Colors.white,
-          selectedFontSize: 14,
-          type: BottomNavigationBarType.shifting,
-          items: _navItems,
-          currentIndex: _currentIndex,
-          selectedItemColor: LayoutBlueColor1,
-          unselectedItemColor: NavigationUnselectedItemColor,
-          onTap: onNavItemTap,
-        ),
-      ),
-    );
-  }
-
+class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Consumer<AuthService>(
@@ -103,8 +46,7 @@ class _HomeState extends State<Home> {
                 ),
               ),
             ),
-            bottomNavigationBar: navigation(context),
-            body: _navPages.elementAt(_currentIndex));
+            body: Dashboard());
       },
     );
   }
