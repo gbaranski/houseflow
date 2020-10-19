666f4b62e8a22a7c3e022cbb77c3a2a0a48e4f37
gbaranski <gbaranski19@gmail.com>
2020-10-19 14:08:24 +0200

    Add on message webhook

--- webhooks/server/event.go
+++ webhooks/server/event.go
@@ -20,6 +20,15 @@ func handleDisconnected(e *types.DisconnectedEvent, client *services.FirebaseCli
 	services.UpdateDeviceStatus(context.Background(), client, e.Username, false)
 }
 
+func handleMessagePublish(e *types.MessageEvent, client *services.FirebaseClient) {
+	clientData, err := services.GetClientData(e.FromClientID)
+	if err != nil {
+		log.Printf("Error occured when retreiving client data %s\n", err)
+	}
+	requestDeviceUID := e.Topic[:36]
+	log.Printf("This request was about %s", requestDeviceUID)
+}
+
 // OnEvent handles /event request
 func OnEvent(w http.ResponseWriter, req *http.Request, client *services.FirebaseClient) {
 	fmt.Fprintf(w, "Hello at /event")
@@ -35,10 +44,6 @@ func OnEvent(w http.ResponseWriter, req *http.Request, client *services.Firebase
 		return
 	}
 
-	if strings.HasPrefix(e.ClientID, "device_") == false {
-		return
-	}
-
 	switch e.Action {
 	case "client_connected":
 		e, err := utils.BytesToConnectedEvent(bytes)
@@ -46,6 +51,10 @@ func OnEvent(w http.ResponseWriter, req *http.Request, client *services.Firebase
 			log.Printf("Failed parsing bytes to ConnectedEvent %s\n", err)
 			return
 		}
+		if strings.HasPrefix(e.ClientID, "device_") == false {
+			return
+		}
+
 		handleConnected(e, client)
 	case "client_disconnected":
 		e, err := utils.BytesToDisconnectedEvent(bytes)
@@ -53,7 +62,20 @@ func OnEvent(w http.ResponseWriter, req *http.Request, client *services.Firebase
 			log.Printf("Failed parsing bytes to DisconnectedEvent %s\n", err)
 			return
 		}
+		if strings.HasPrefix(e.ClientID, "device_") == false {
+			return
+		}
 		handleDisconnected(e, client)
+	case "message_publish":
+		e, err := utils.BytesToMessageEvent(bytes)
+		if err != nil {
+			log.Printf("Failed parsing bytes to MessageEvent %s\n", err)
+			return
+		}
+		if strings.HasPrefix(e.FromClientID, "device_") == true {
+			return
+		}
+		handleMessagePublish(e, client)
 	default:
 		log.Printf("Unrecognized action %s\n", e.Action)
 	}
--- webhooks/types/event.go
+++ webhooks/types/event.go
@@ -2,14 +2,14 @@ package eventtypes
 
 // WebhookEvent when webhook triggered
 type WebhookEvent struct {
-	Username string `json:"username"`
-	Action   string `json:"action"`
-	ClientID string `json:"clientid"`
+	Action string `json:"action"`
 }
 
 // ConnectedEvent when client connects
 type ConnectedEvent struct {
 	WebhookEvent
+	Username           string `json:"username"`
+	ClientID           string `json:"clientid"`
 	ProtoVersion       int    `json:"proto_ver"`
 	KeepAlive          int    `json:"keepalive"`
 	IPAddress          string `json:"ipaddress"`
@@ -19,5 +19,73 @@ type ConnectedEvent struct {
 // DisconnectedEvent when client disconnected
 type DisconnectedEvent struct {
 	WebhookEvent
-	Reason string `json:"reason"`
+	Username string `json:"username"`
+	ClientID string `json:"clientid"`
+	Reason   string `json:"reason"`
+}
+
+// MessageEvent when client sends message
+type MessageEvent struct {
+	WebhookEvent
+	ToClientID   string `json:"clientid"`
+	FromClientID string `json:"from_client_id"`
+	FromUsername string `json:"from_username"`
+	Topic        string `json:"topic"`
+	Qos          int    `json:"qos"`
+	Retain       bool   `json:"retain"`
+	Payload      string `json:"payload"`
+	Timestamp    int    `json:"ts"`
+}
+
+// MqttClientData Clent data received from API
+type MqttClientData struct {
+	ConnectedNode          string `json:"node"`
+	ClientID               string `json:"clientid"`
+	Username               string `json:"username"`
+	ProtocolName           string `json:"proto_name"`
+	ProtocolVersion        int    `json:"proto_ver"`
+	IPAddress              string `json:"ip_address"`
+	Port                   int    `json:"port"`
+	IsBridge               bool   `json:"is_bridge"`
+	ConnectedAt            string `json:"connected_at"`
+	DisconnectedAt         string `json:"disconnected_at"`
+	IsConnected            bool   `json:"connected"`
+	Zone                   string `json:"zone"`
+	KeepAlive              int    `json:"keepalive"`
+	CleanStart             bool   `json:"clean_start"`
+	ExpiryInterval         int    `json:"expiry_interval"`
+	CreatedAt              string `json:"created_at"`
+	SubscriptionCount      int    `json:"subscriptions_cnt"`
+	MaxSubscriptions       int    `json:"max_subscriptions"`
+	InFlight               int    `json:"inflight"`
+	MaxInFlight            int    `json:"max_inflight"`
+	MessageQueueLength     int    `json:"mqueue_len"`
+	MaxMessageQueueLength  int    `json:"max_mqueue"`
+	MessageQueueDropped    int    `json:"mqueue_dropped"`
+	AwaitingRel            int    `json:"awaiting_rel"`
+	MaxAwaitingRel         int    `json:"max_awaiting_rel"`
+	BytesReceived          int    `json:"recv_oct"`
+	MqttPacketsReceived    string `json:"recv_pkt"`
+	PublishPacketsRecieved string `json:"recv_msg"`
+	BytesSent              string `json:"send_oct"`
+	TCPPacketsSent         string `json:"send_cnt"`
+	MqttPacketsSent        string `json:"send_pkt"`
+	PublishPacketsSent     string `json:"send_msg"`
+	MailboxLength          string `json:"mailbox_len"`
+	HeapSize               string `json:"heap_size"`
+	Reductions             string `json:"reductions"`
+}
+
+// MqttMeta received with API requested
+type MqttMeta struct {
+	Page  int `json:"page"`
+	Limit int `json:"limit"`
+	Count int `json:"count"`
+}
+
+// GetClientResponse response for retreiving client
+type GetClientResponse struct {
+	Code int              `json:"code"`
+	Data []MqttClientData `json:"data"`
+	Meta MqttMeta         `json:"meta"`
 }
