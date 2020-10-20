dcfe71b88fc67bc55cd05e215c307d2e09964bff
gbaranski <gbaranski19@gmail.com>
2020-10-20 11:17:18 +0200

    Add deviceType to firebase device history type on webhooks

--- webhooks/services/firebase.go
+++ webhooks/services/firebase.go
@@ -28,6 +28,14 @@ type FirebaseUser struct {
 	username string
 }
 
+// FirebaseDevice refers to device in firebase
+type FirebaseDevice struct {
+	IPAddress  string
+	Status     bool
+	DeviceType string
+	UID        string
+}
+
 // InitFirebase firebase initialization
 func InitFirebase(ctx context.Context) (*FirebaseClient, error) {
 	app, err := firebase.NewApp(ctx, nil)
@@ -68,14 +76,30 @@ func GetUserUsername(ctx context.Context, client *FirebaseClient, uid string) (s
 	return data["username"].(string), nil
 }
 
+// GetFirebaseDevice retreives firebase device from firestore
+func GetFirebaseDevice(ctx context.Context, client *FirebaseClient, uid string) (*FirebaseDevice, error) {
+	dsnap, err := client.db.Collection("users").Doc(uid).Get(ctx)
+	if err != nil {
+		return nil, err
+	}
+	if dsnap.Exists() == false {
+		return nil, errors.New("firebase: FirebaseUser snapshot does not exist")
+	}
+	data := dsnap.Data()
+	firebaseDevice := FirebaseDevice{IPAddress: data["type"].(string), Status: data["status"].(bool), DeviceType: data["type"].(string), UID: data["uid"].(string)}
+	return &firebaseDevice, nil
+
+}
+
 // AddDeviceHistory adds history for a device
-func AddDeviceHistory(ctx context.Context, client *FirebaseClient, deviceUID string, deviceRequest *types.DeviceRequest) (string, error) {
-	ref, _, err := client.db.Collection("devices").Doc(deviceUID).Collection("history").Add(ctx, map[string]interface{}{
-		"request":   deviceRequest.Request,
-		"username":  deviceRequest.Username,
-		"ipAddress": deviceRequest.IPAddress,
-		"timestamp": deviceRequest.Timestamp,
-		"deviceUid": deviceUID,
+func AddDeviceHistory(ctx context.Context, client *FirebaseClient, firebaseDevice *FirebaseDevice, deviceRequest *types.DeviceRequest) (string, error) {
+	ref, _, err := client.db.Collection("devices").Doc(firebaseDevice.UID).Collection("history").Add(ctx, map[string]interface{}{
+		"request":    deviceRequest.Request,
+		"username":   deviceRequest.Username,
+		"ipAddress":  deviceRequest.IPAddress,
+		"timestamp":  deviceRequest.Timestamp,
+		"deviceUid":  firebaseDevice.UID,
+		"deviceType": firebaseDevice.DeviceType,
 	})
 	return ref.ID, err
 }
