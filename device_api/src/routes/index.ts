4092366fe1887d02ae193d396eeec90b7e8d912d
gbaranski <gbaranski19@gmail.com>
2020-11-03 11:28:14 +0100

    Moved api_server to device_api

--- device_api/src/routes/index.ts
+++ device_api/src/routes/index.ts
@@ -1,9 +1,12 @@
 import express from 'express';
+import deviceRequest from './request';
 
 const router = express.Router();
 
 router.get('/', (req, res): void => {
-  res.send('Hello from API server');
+  res.send('Hello from Device API server');
 });
+
+router.use('/request');
 
 export default router;
