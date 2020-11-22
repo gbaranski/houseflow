2813d3d1623d506ecad6c223886de3507fea0e1a
gbaranski <gbaranski19@gmail.com>
2020-11-22 12:27:11 +0100

    feat: Add DeviceRequestByActionName and ByUID(default) to types

--- types/src/client.ts
+++ types/src/client.ts
@@ -2,11 +2,22 @@ import { GeoPoint, ReadWriteExecuteAccess } from '.';
 
 export namespace Client {
   export type UserRole = 'admin' | 'user';
-  export interface DeviceRequest {
-    user: {
-      token: string;
-      geoPoint: GeoPoint;
+
+  export interface DeviceRequestUser {
+    token: string;
+    geoPoint: GeoPoint;
+  }
+
+  export interface DeviceRequestByActionName {
+    user: DeviceRequestUser;
+    device: {
+      action: number;
+      data?: string;
     };
+  }
+
+  export interface DeviceRequestByUID {
+    user: DeviceRequestUser;
     device: {
       uid: string;
       action: number;
