3f2e4a616ee1992fba3f40488e3db70e32806dc8
gbaranski <gbaranski19@gmail.com>
2020-11-23 01:56:20 +0100

    feat: AdditionalView now accepts floatingActionBtn

--- app_mobile/lib/screens/additional_view/index.dart
+++ app_mobile/lib/screens/additional_view/index.dart
@@ -3,33 +3,36 @@ import 'package:houseflow/shared/constants.dart';
 
 class AdditionalView extends StatelessWidget {
   final Widget body;
-  AdditionalView({@required this.body});
+  final FloatingActionButton floatingActionButton;
+  AdditionalView({@required this.body, this.floatingActionButton});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: CustomScrollView(slivers: [
-        SliverAppBar(
-          backgroundColor: Colors.white,
-          elevation: 0,
-          expandedHeight: 100,
-          leading: Padding(
-            padding: const EdgeInsets.only(top: 25, left: 15),
-            child: IconButton(
-              onPressed: () => Navigator.pop(context),
-              tooltip: "Back",
-              icon: Icon(
-                Icons.arrow_back_ios,
-                color: ACTION_ICON_COLOR,
+      floatingActionButton: floatingActionButton,
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            backgroundColor: Colors.white,
+            elevation: 0,
+            expandedHeight: 100,
+            leading: Padding(
+              padding: const EdgeInsets.only(top: 25, left: 15),
+              child: IconButton(
+                onPressed: () => Navigator.pop(context),
+                tooltip: "Back",
+                icon: Icon(
+                  Icons.arrow_back_ios,
+                  color: ACTION_ICON_COLOR,
+                ),
               ),
             ),
           ),
-        ),
-        SliverToBoxAdapter(child: body)
-      ],
+          SliverToBoxAdapter(child: body)
+        ],
         physics:
-        AlwaysScrollableScrollPhysics().applyTo(BouncingScrollPhysics()),
+            AlwaysScrollableScrollPhysics().applyTo(BouncingScrollPhysics()),
       ),
     );
   }
