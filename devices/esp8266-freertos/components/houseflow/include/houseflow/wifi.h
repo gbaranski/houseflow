ad8d8a78211e1a52b10ccf40ea30ca8eb0e0dc07
gbaranski <gbaranski19@gmail.com>
2020-12-19 15:51:44 +0100

    chore: refactor, move wifi code to components

---
+++ devices/esp8266-freertos/components/houseflow/include/houseflow/wifi.h
@@ -0,0 +1,12 @@
+//
+// Created by gbaranski on 19/12/2020.
+//
+
+#ifndef ESP8266_FREERTOS_HF_WIFI_H
+#define ESP8266_FREERTOS_HF_WIFI_H
+
+static const char *WIFI_TAG = "wifi station";
+
+void wifi_init_sta(void);
+
+#endif //ESP8266_FREERTOS_HF_WIFI_H
