1463c999c8faeb937c2ffd662b9096a484ea1605
gbaranski <gbaranski19@gmail.com>
2020-12-19 15:54:30 +0100

    style: remove comments

--- devices/esp8266-freertos/main/main.c
+++ devices/esp8266-freertos/main/main.c
@@ -1,18 +1,14 @@
-/* WiFi station Example
 
-   This example code is in the Public Domain (or CC0 licensed, at your option.)
 
-   Unless required by applicable law or agreed to in writing, this
-   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-   CONDITIONS OF ANY KIND, either express or implied.
-*/
+
+#include <esp_types.h>
 #include "esp_log.h"
 #include "nvs.h"
 #include "nvs_flash.h"
 
 #include "houseflow/wifi.h"
 
-void app_main()
+__unused void app_main()
 {
     ESP_ERROR_CHECK(nvs_flash_init());
 
