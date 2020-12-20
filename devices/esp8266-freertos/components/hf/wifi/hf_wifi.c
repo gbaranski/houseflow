1bc10e39fffb89a98596457afd5c64db6821cf1b
gbaranski <gbaranski19@gmail.com>
2020-12-20 15:59:28 +0100

    fix: remove init event_loop from wifi

--- devices/esp8266-freertos/components/hf/wifi/hf_wifi.c
+++ devices/esp8266-freertos/components/hf/wifi/hf_wifi.c
@@ -53,8 +53,6 @@ void wifi_init_sta(void) {
 
     tcpip_adapter_init();
 
-    ESP_ERROR_CHECK(esp_event_loop_create_default());
-
     wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
     ESP_ERROR_CHECK(esp_wifi_init(&cfg));
 
