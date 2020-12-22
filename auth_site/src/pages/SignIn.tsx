a8d943bc57be0cc713e8aefd331eaaa992f142b2
gbaranski <gbaranski19@gmail.com>
2020-12-22 22:15:00 +0100

    fix(auth_site): remove firebase, material-ui, add formik

--- auth_site/src/pages/SignIn.tsx
+++ auth_site/src/pages/SignIn.tsx
@@ -1,112 +1,56 @@
 import React from 'react';
-import {
-  Avatar,
-  Box,
-  Button,
-  Checkbox,
-  Container,
-  CssBaseline,
-  FormControlLabel,
-  Grid,
-  Link,
-  ThemeProvider,
-  makeStyles,
-  TextField,
-  Typography,
-} from '@material-ui/core';
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
-import { Copyright } from '../components/Copyright';
-import Coffee from 'mdi-material-ui/Coffee';
+import { Field, Form, Formik } from 'formik';
+import * as Yup from 'yup';
 
-const useStyles = makeStyles((theme) => ({
-  paper: {
-    marginTop: theme.spacing(8),
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-  },
-  avatar: {
-    margin: theme.spacing(1),
-    backgroundColor: theme.palette.secondary.main,
-  },
-  form: {
-    width: '100%', // Fix IE 11 issue.
-    marginTop: theme.spacing(1),
-  },
-  submit: {
-    margin: theme.spacing(3, 0, 2),
-  },
-}));
-
-export function SignIn() {
-  const classes = useStyles();
-  return (
-    <Container component="main" maxWidth="xs">
-      <CssBaseline />
-      <div className={classes.paper}>
-        <Avatar className={classes.avatar}>
-          <LockOutlinedIcon />
-        </Avatar>
-        <Typography component="h1" variant="h5">
-          Sign in
-        </Typography>
-
-        <Button variant="contained" color="secondary" startIcon={<Coffee />}>
-          Delete
-        </Button>
-        <form className={classes.form} noValidate>
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            id="email"
-            label="Email Address"
-            name="email"
-            autoComplete="email"
-            autoFocus
-          />
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            name="password"
-            label="Password"
-            type="password"
-            id="password"
-            autoComplete="current-password"
-          />
-          <FormControlLabel
-            control={<Checkbox value="remember" color="primary" />}
-            label="Remember me"
-          />
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            color="primary"
-            className={classes.submit}
-          >
-            Sign In
-          </Button>
-          <Grid container>
-            <Grid item xs>
-              <Link href="#" variant="body2">
-                Forgot password?
-              </Link>
-            </Grid>
-            <Grid item>
-              <Link href="/signup" variant="body2">
-                {"Don't have an account? Sign Up"}
-              </Link>
-            </Grid>
-          </Grid>
-        </form>
-      </div>
-      <Box mt={8}>
-        <Copyright />
-      </Box>
-    </Container>
-  );
+interface SignUpValues {
+  email: string;
+  password: string;
 }
+
+const SignInSchema = Yup.object().shape({
+  email: Yup.string().email('Invalid email').required('Required'),
+  password: Yup.string()
+    .min(8, 'Password too short! Min 8 characters')
+    .max(64, 'Password too long! Max 64 characters')
+    .required('Required'),
+});
+
+const SignIn = () => {
+  const initialValues: SignUpValues = {
+    email: '',
+    password: '',
+  };
+
+  return (
+    <div>
+      <h1>Sign in page</h1>
+      <Formik
+        initialValues={initialValues}
+        validationSchema={SignInSchema}
+        onSubmit={async (values) => {
+          // same shape as initial values
+          console.log({
+            email: values.email,
+            password: values.password,
+          });
+        }}
+      >
+        {({ errors, touched }) => (
+          <Form>
+            <Field name="email" type="email" />
+            {errors.email && touched.email ? <div>{errors.email}</div> : null}
+            <br />
+            <Field name="password" />
+            {errors.password && touched.password ? (
+              <div>{errors.password}</div>
+            ) : null}
+            <br />
+            <button type="submit">Submit</button>
+          </Form>
+        )}
+      </Formik>
+    </div>
+  );
+};
+
+export default SignIn;
