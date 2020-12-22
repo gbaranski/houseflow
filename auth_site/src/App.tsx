fd3bdda8d49b48752fb66c4fd345df031e3408cd
gbaranski <gbaranski19@gmail.com>
2020-12-22 22:15:00 +0100

    fix(auth_site): remove firebase, material-ui, add formik

--- auth_site/src/App.tsx
+++ auth_site/src/App.tsx
@@ -1,36 +1,24 @@
 import React from 'react';
-import { ThemeProvider } from '@material-ui/core';
-import { BrowserRouter, Switch, Route, Link, Redirect } from 'react-router-dom';
-import { createMuiTheme } from '@material-ui/core/styles';
-import { SignIn } from './pages/SignIn';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
-
-const theme = createMuiTheme({
-  palette: {
-    type: 'dark',
-  },
-});
 
 function App() {
   return (
     <BrowserRouter>
-      <ThemeProvider theme={theme}>
-        <Switch>
-          <Route path="/signin">
-            <SignIn />
-          </Route>
-          <Route path="/signup">
-            <SignUp />
-          </Route>
-          <Route
-            path="/"
-            component={() => {
-              window.location.href = 'https://github.com/gbaranski/houseflow';
-              return null;
-            }}
-          />
-        </Switch>
-      </ThemeProvider>
+      <Switch>
+        <Route path="/signin">
+          <SignIn />
+        </Route>
+        <Route path="/signup">{/* <SignUp /> */}</Route>
+        <Route
+          path="/"
+          component={() => {
+            window.location.href = 'https://github.com/gbaranski/houseflow';
+            return null;
+          }}
+        />
+      </Switch>
     </BrowserRouter>
   );
 }
