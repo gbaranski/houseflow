eeba1598b219fd798ed4136f521dc0ded9b43acb
gbaranski <gbaranski19@gmail.com>
2020-12-23 16:32:02 +0100

    feat: add creating account auth site

---
+++ auth_site/src/config.ts
@@ -0,0 +1,1 @@
+export const AUTH_URL = 'http://localhost:80';
--- auth_site/src/pages/SignIn.tsx
+++ auth_site/src/pages/SignIn.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { Field, Form, Formik } from 'formik';
 import * as Yup from 'yup';
+import { Link } from 'react-router-dom';
 
-interface SignUpValues {
+interface SignInValues {
   email: string;
   password: string;
 }
@@ -15,11 +16,47 @@ const SignInSchema = Yup.object().shape({
     .required('Required'),
 });
 
+type MayNull<T> = {
+  [P in keyof T]: T[P] | null;
+};
+
+interface GoogleParams {
+  // The Google client ID you registered with Google.
+  client_id: string | null;
+  // The URL to which you send the response to this request.
+  redirect_uri: string | null;
+  // A bookkeeping value that is passed back to Google unchanged in the redirect URI.
+  state: string | null;
+  // Optional: A space-delimited set of scope strings that specify the data Google is requesting authorization for.
+  scope: string | null;
+  // The type of value to return in the response. For the OAuth 2.0 authorization code flow, the response type is always code.
+  response_type: string | null;
+  // The Google Account language setting in RFC5646 format, used to localize your content in the user's preferred language.
+  user_locale: string | null;
+}
+
+const getGoogleParams = (): GoogleParams => {
+  const params = new URL(document.location.href).searchParams;
+
+  return {
+    client_id: params.get('client_id'),
+    redirect_uri: params.get('redirect_uri'),
+    state: params.get('state'),
+    scope: params.get('scope'),
+    response_type: params.get('response_type'),
+    user_locale: params.get('user_locale'),
+  };
+};
+
 const SignIn = () => {
-  const initialValues: SignUpValues = {
+  const initialValues: SignInValues = {
     email: '',
     password: '',
   };
+
+  const googleParams = getGoogleParams();
+
+  const onSubmit = (values: SignInValues) => {};
 
   return (
     <div>
@@ -27,25 +64,21 @@ const SignIn = () => {
       <Formik
         initialValues={initialValues}
         validationSchema={SignInSchema}
-        onSubmit={async (values) => {
-          // same shape as initial values
-          console.log({
-            email: values.email,
-            password: values.password,
-          });
-        }}
+        onSubmit={onSubmit}
       >
         {({ errors, touched }) => (
           <Form>
-            <Field name="email" type="email" />
+            <Field name="email" type="email" placeholder="Email" />
             {errors.email && touched.email ? <div>{errors.email}</div> : null}
             <br />
-            <Field name="password" />
+            <Field name="password" placeholder="Password" type="password" />
             {errors.password && touched.password ? (
               <div>{errors.password}</div>
             ) : null}
             <br />
             <button type="submit">Submit</button>
+            <br />
+            <Link to="/signup">Sign up</Link>
           </Form>
         )}
       </Formik>
