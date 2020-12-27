cbd6baf8e59e49bf729a891a5ea517c97eb6a513
gbaranski <gbaranski19@gmail.com>
2020-12-27 22:07:40 +0100

    fix: error handling if its not invalid grant issue

--- auth/server/token.go
+++ auth/server/token.go
@@ -11,26 +11,46 @@ import (
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
-func (s *Server) onTokenAuthorizationCodeGrant(form *TokenQuery) (gin.H, error) {
+type tokenError struct {
+	InvalidGrant bool
+	Err          error
+}
+
+func (s *Server) onTokenAuthorizationCodeGrant(form *TokenQuery) (gin.H, *tokenError) {
 	if !validateRedirectURI(form.RedirectURI) {
-		return nil, fmt.Errorf("Invalid redirect_uri")
+		return nil, &tokenError{
+			InvalidGrant: false,
+			Err:          fmt.Errorf("Invalid redirect_uri"),
+		}
 	}
 	token, err := utils.VerifyToken(form.Code, utils.JWTAuthCodeKey)
 	if err != nil {
-		return nil, err
+		return nil, &tokenError{
+			InvalidGrant: true,
+			Err:          err,
+		}
 	}
 	tp, err := utils.CreateTokenPair()
 	if err != nil {
-		return nil, err
+		return nil, &tokenError{
+			InvalidGrant: false,
+			Err:          err,
+		}
 	}
 	userID, err := primitive.ObjectIDFromHex(token.Claims.Audience)
 	if err != nil {
-		return nil, err
+		return nil, &tokenError{
+			InvalidGrant: false,
+			Err:          err,
+		}
 	}
 
 	err = s.db.Redis.AddTokenPair(userID, tp)
 	if err != nil {
-		return nil, err
+		return nil, &tokenError{
+			InvalidGrant: false,
+			Err:          err,
+		}
 	}
 
 	at := time.Unix(tp.AccessToken.Claims.ExpiresAt, 0)
@@ -44,19 +64,28 @@ func (s *Server) onTokenAuthorizationCodeGrant(form *TokenQuery) (gin.H, error) 
 
 }
 
-func (s *Server) onTokenAccessTokenGrant(form *TokenQuery) (gin.H, error) {
+func (s *Server) onTokenAccessTokenGrant(form *TokenQuery) (gin.H, *tokenError) {
 	rt, err := utils.VerifyToken(form.RefreshToken, utils.JWTRefreshKey)
 	if err != nil {
-		return nil, err
+		return nil, &tokenError{
+			InvalidGrant: true,
+			Err:          err,
+		}
 	}
 	_, err = s.db.Redis.FetchToken(rt.Claims)
 	if err != nil {
-		return nil, err
+		return nil, &tokenError{
+			InvalidGrant: true,
+			Err:          err,
+		}
 	}
 
 	at, err := utils.CreateAccessToken()
 	if err != nil {
-		return nil, err
+		return nil, &tokenError{
+			InvalidGrant: false,
+			Err:          err,
+		}
 	}
 	atexp := time.Unix(at.Claims.ExpiresAt, 0)
 	now := time.Now()
@@ -83,20 +112,30 @@ func (s *Server) onToken(c *gin.Context) {
 	if form.GrantType == "authorization_code" {
 		json, err := s.onTokenAuthorizationCodeGrant(&form)
 		if err != nil {
-			c.JSON(http.StatusBadRequest, gin.H{
-				"error":   "invalid_grant",
-				"message": err.Error(),
-			})
+			if err.InvalidGrant {
+				c.JSON(http.StatusBadRequest, gin.H{
+					"error":   "invalid_grant",
+					"message": err.Err.Error(),
+				})
+			} else {
+				c.JSON(http.StatusInternalServerError, gin.H{})
+			}
+			fmt.Println("Error occured when requesting token", err.Err.Error())
 			return
 		}
 		c.JSON(http.StatusOK, json)
 	} else if form.GrantType == "refresh_token" {
 		json, err := s.onTokenAccessTokenGrant(&form)
 		if err != nil {
-			c.JSON(http.StatusBadRequest, gin.H{
-				"error":   "invalid_grant",
-				"message": err.Error(),
-			})
+			if err.InvalidGrant {
+				c.JSON(http.StatusBadRequest, gin.H{
+					"error":   "invalid_grant",
+					"message": err.Err.Error(),
+				})
+			} else {
+				c.JSON(http.StatusInternalServerError, gin.H{})
+			}
+			fmt.Println("Error occured when requesting token", err.Err.Error())
 			return
 		}
 		c.JSON(http.StatusOK, json)
