993a14383e94bf6cf1cb0031623ca8d4e10bf6a4
gbaranski <gbaranski19@gmail.com>
2020-12-27 22:42:20 +0100

    fix: remove unused creating at at user type

--- auth/types/user.go
+++ auth/types/user.go
@@ -1,10 +1,6 @@
 package types
 
 import (
-	"os"
-	"time"
-
-	"github.com/dgrijalva/jwt-go"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
@@ -15,20 +11,4 @@ type User struct {
 	LastName  string             `json:"lastName" bson:"lastName" form:"lastName" binding:"required"`
 	Email     string             `json:"email" bson:"email" houseflow:"email" form:"email" binding:"required,email"`
 	Password  string             `json:"password" bson:"password" form:"password" binding:"required,min=8,max=20"`
-}
-
-// CreateAccessToken creates JWT access token for user
-func (u *User) CreateAccessToken() (*string, error) {
-	key := os.Getenv("AUTH_JWT_KEY")
-	atClaims := jwt.MapClaims{
-		"authorized": true,
-		"user_id":    u.ID,
-		"exp":        time.Now().Add(time.Minute * 15).Unix(),
-	}
-	at := jwt.NewWithClaims(jwt.SigningMethodHS256, atClaims)
-	token, err := at.SignedString([]byte(key))
-	if err != nil {
-		return nil, err
-	}
-	return &token, nil
 }
