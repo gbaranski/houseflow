96ec249d7d8dd34c43a4f1fa9282302ffcec9e65
gbaranski <gbaranski19@gmail.com>
2020-12-28 17:20:47 +0100

    feat: handling fulfillment and onSync mock

--- actions/src/routes/index.ts
+++ actions/src/routes/index.ts
@@ -1,9 +1,12 @@
 import express from 'express';
+import fulfillment from './fulfillment';
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
   res.send('Hello from Action server');
 });
+
+router.post('/fulfillment', fulfillment);
 
 export default router;
---
+++ actions/src/utils/index.ts
@@ -0,0 +1,12 @@
+import { Headers } from 'actions-on-google';
+
+export const extractJWTToken = (headers: Headers): string => {
+  const header = headers.authorization;
+  if (!header) throw new Error("Headers doesn't include authorization header");
+  if (header instanceof Array)
+    throw new Error('Authorization header is type of array');
+
+  const splitted = header.split(' ');
+  if (splitted.length != 2) throw new Error('Authorization header is invalid');
+  return splitted[1];
+};
