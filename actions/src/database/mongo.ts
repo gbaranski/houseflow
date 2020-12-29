78b6ea5533d0bd6e89be8451337f1d3e125f81d1
gbaranski <gbaranski19@gmail.com>
2020-12-29 14:56:45 +0100

    feat(actions): retreiving user, types for devices

--- actions/src/database/mongo.ts
+++ actions/src/database/mongo.ts
@@ -1,3 +1,7 @@
+import {
+  SmartHomeV1SyncDeviceInfo,
+  SmartHomeV1SyncName,
+} from 'actions-on-google';
 import { Collection, Db, MongoClient, ObjectId } from 'mongodb';
 
 const { MONGO_INITDB_ROOT_USERNAME, MONGO_INITDB_ROOT_PASSWORD } = process.env;
@@ -18,6 +22,7 @@ const client = new MongoClient('mongodb://mongo:27017', {
 
 let db: Db;
 let devicesCollection: Collection;
+let usersCollection: Collection;
 
 export const connectMongo = async (): Promise<void> => {
   console.log('Attempting connection to mongoDB');
@@ -25,15 +30,39 @@ export const connectMongo = async (): Promise<void> => {
   console.log('Successfully connected to mongoDB');
   db = client.db(DB_NAME);
   devicesCollection = db.collection('devices');
+  usersCollection = db.collection('users');
 };
 
-export interface DeviceData {
-  on: boolean;
+interface BaseDeviceData {
   online: boolean;
 }
+
+interface WaterHeaterData extends BaseDeviceData {
+  isRunning: boolean;
+}
+
+interface GarageData extends BaseDeviceData {
+  openPercent: number;
+}
+
+interface LightData extends BaseDeviceData {
+  on: boolean;
+}
+
+type AnyDeviceData = WaterHeaterData | GarageData | LightData;
+
 export interface Device {
+  // defined only when retreiving from DB
   _id?: string;
-  data: DeviceData;
+
+  type: string;
+  traits: string[];
+  name: SmartHomeV1SyncName;
+  willReportState: boolean;
+  deviceInfo: SmartHomeV1SyncDeviceInfo;
+  roomHint: string;
+  data: AnyDeviceData;
+  attributes?: object;
 }
 
 export const addDevice = async (device: Device): Promise<void> => {
@@ -48,4 +77,22 @@ export const findDevices = async (deviceIDs: string[]): Promise<Device[]> => {
     },
   });
   return res.toArray();
+};
+
+export interface User {
+  _id?: string;
+
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+  devices: string[];
+}
+
+export const getUser = async (userID: string): Promise<User> => {
+  const user = await usersCollection.findOne({
+    _id: new ObjectId(userID),
+  });
+  if (!user) throw new Error('Unable to find user with matching ID');
+  return user;
 };
