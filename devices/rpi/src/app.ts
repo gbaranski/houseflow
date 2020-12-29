f8dc2b919ac329eb791316dfc1259086c48fac8b
gbaranski <gbaranski19@gmail.com>
2020-12-29 20:29:01 +0100

    feat(rpi): support for homegraph

--- devices/rpi/src/app.ts
+++ devices/rpi/src/app.ts
@@ -1,26 +1,46 @@
 import { MqttClient } from 'mqtt';
-import { Topic } from '@houseflow/types';
-import { startMixing } from './services/relay';
+import { SmartHomeV1ExecuteResponseCommands } from 'actions-on-google';
 
 const { DEVICE_UID } = process.env;
+if (!DEVICE_UID) throw new Error('DEVICE_UID not defined');
+
+interface DeviceRequest {
+  correlationData: string;
+  params: any;
+}
+
+interface DeviceResponse extends SmartHomeV1ExecuteResponseCommands {
+  correlationData: string;
+}
 
 export const onConnection = (mqtt: MqttClient) => {
   console.log('Initialized connection with MQTT');
 
-  const startMixTopic: Topic = {
-    request: `${DEVICE_UID}/action1/request`,
-    response: `${DEVICE_UID}/action1/response`,
+  const OnOffTopic = {
+    request: `${DEVICE_UID}/commands/OnOff/request`,
+    response: `${DEVICE_UID}/commands/OnOff/response`,
   };
 
-  mqtt.subscribe(startMixTopic.request);
+  mqtt.subscribe(OnOffTopic.request);
 
   mqtt.on('message', (topic, payload, packet) => {
     console.log({ topic, payload, packet });
 
     switch (topic) {
-      case startMixTopic.request:
-        startMixing();
-        sendRequestResponse(startMixTopic.response, payload);
+      case OnOffTopic.request:
+        const request = JSON.parse(payload.toString('utf8')) as DeviceRequest;
+
+        const response: DeviceResponse = {
+          ids: [DEVICE_UID],
+          status: 'SUCCESS',
+          states: {
+            on: request.params.on,
+          },
+          correlationData: request.correlationData,
+        };
+        sendRequestResponse(OnOffTopic.response, response);
+        mqtt.publish(OnOffTopic.response, JSON.stringify(response));
+        console.log(`Changing lights state to ${request.params.on}!`);
         break;
       default:
         console.log('Unrecognized topic');
@@ -28,7 +48,7 @@ export const onConnection = (mqtt: MqttClient) => {
     }
   });
 
-  const sendRequestResponse = (topic: string, payload: Buffer) => {
-    mqtt.publish(topic, payload);
+  const sendRequestResponse = (topic: string, payload: DeviceResponse) => {
+    mqtt.publish(topic, JSON.stringify(payload));
   };
 };
