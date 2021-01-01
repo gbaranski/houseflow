8c398fbb390976eb2989470d46ef32a350138c37
gbaranski <gbaranski19@gmail.com>
2021-01-01 19:59:27 +0100

    feat(rpi): handling reporting states

--- devices/rpi/src/app.ts
+++ devices/rpi/src/app.ts
@@ -1,5 +1,5 @@
-import { MqttClient } from 'mqtt';
 import { SmartHomeV1ExecuteResponseCommands } from 'actions-on-google';
+import { mqttClient } from '.';
 
 const { DEVICE_UID } = process.env;
 if (!DEVICE_UID) throw new Error('DEVICE_UID not defined');
@@ -13,42 +13,57 @@ interface DeviceResponse extends SmartHomeV1ExecuteResponseCommands {
   correlationData: string;
 }
 
-export const onConnection = (mqtt: MqttClient) => {
+let currentState = {
+  online: true,
+  on: true,
+};
+
+// It can be used for example when someone turns on/off light by physical switch
+const publishState = () => {
+  console.log('Publishing state: ', currentState);
+  mqttClient.publish(`${DEVICE_UID}/reportState`, JSON.stringify(currentState));
+};
+
+export const onConnection = () => {
   console.log('Initialized connection with MQTT');
+  publishState();
 
   const OnOffTopic = {
     request: `${DEVICE_UID}/commands/OnOff/request`,
     response: `${DEVICE_UID}/commands/OnOff/response`,
   };
 
-  mqtt.subscribe(OnOffTopic.request);
+  setTimeout(() => {
+    currentState.on = false;
+    publishState();
+  }, 2000);
 
-  mqtt.on('message', (topic, payload, packet) => {
+  mqttClient.subscribe(OnOffTopic.request);
+
+  mqttClient.on('message', (topic, payload, packet) => {
     console.log({ topic, payload, packet });
 
     switch (topic) {
       case OnOffTopic.request:
-        const request = JSON.parse(payload.toString('utf8')) as DeviceRequest;
+        const onOffRequest = JSON.parse(
+          payload.toString('utf8'),
+        ) as DeviceRequest;
+        console.log(`Changing lights state to ${onOffRequest.params.on}!`);
+        currentState.on = onOffRequest.params.on;
 
         const response: DeviceResponse = {
+          correlationData: onOffRequest.correlationData,
           ids: [DEVICE_UID],
           status: 'SUCCESS',
           states: {
-            on: request.params.on,
+            ...currentState,
           },
-          correlationData: request.correlationData,
         };
-        sendRequestResponse(OnOffTopic.response, response);
-        mqtt.publish(OnOffTopic.response, JSON.stringify(response));
-        console.log(`Changing lights state to ${request.params.on}!`);
+        mqttClient.publish(OnOffTopic.response, JSON.stringify(response));
         break;
       default:
         console.log('Unrecognized topic');
         break;
     }
   });
-
-  const sendRequestResponse = (topic: string, payload: DeviceResponse) => {
-    mqtt.publish(topic, JSON.stringify(payload));
-  };
 };
