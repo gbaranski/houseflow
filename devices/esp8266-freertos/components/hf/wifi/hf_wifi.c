3364fe1a44881053931ed416d189a22c2047a0a0
gbaranski <gbaranski19@gmail.com>
2021-01-02 23:11:29 +0100

    style(rtos): format all files

--- devices/esp8266-freertos/components/hf/wifi/hf_wifi.c
+++ devices/esp8266-freertos/components/hf/wifi/hf_wifi.c
@@ -2,6 +2,8 @@
 // Created by gbaranski on 19/12/2020.
 //
 
+#include "hf_wifi.h"
+
 #include <string.h>
 
 #include "esp_event.h"
@@ -10,7 +12,6 @@
 #include "esp_wifi.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/event_groups.h"
-#include "hf_wifi.h"
 #include "nvs.h"
 
 /* FreeRTOS event group to signal when we are connected*/
@@ -28,85 +29,83 @@ static int s_retry_num = 0;
 
 static void event_handler(void *arg, esp_event_base_t event_base,
                           int32_t event_id, void *event_data) {
-    if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START) {
-        esp_wifi_connect();
-    } else if (event_base == WIFI_EVENT &&
-               event_id == WIFI_EVENT_STA_DISCONNECTED) {
-        if (s_retry_num < MAX_RETRY) {
-            esp_wifi_connect();
-            s_retry_num++;
-            ESP_LOGI(WIFI_TAG, "retry to connect to the AP");
-        } else {
-            xEventGroupSetBits(s_wifi_event_group, WIFI_FAIL_BIT);
-        }
-        ESP_LOGI(WIFI_TAG, "connect to the AP fail");
-    } else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) {
-        ip_event_got_ip_t *event = (ip_event_got_ip_t *) event_data;
-        ESP_LOGI(WIFI_TAG, "got ip:%s", ip4addr_ntoa(&event->ip_info.ip));
-        s_retry_num = 0;
-        xEventGroupSetBits(s_wifi_event_group, WIFI_CONNECTED_BIT);
+  if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START) {
+    esp_wifi_connect();
+  } else if (event_base == WIFI_EVENT &&
+             event_id == WIFI_EVENT_STA_DISCONNECTED) {
+    if (s_retry_num < MAX_RETRY) {
+      esp_wifi_connect();
+      s_retry_num++;
+      ESP_LOGI(WIFI_TAG, "retry to connect to the AP");
+    } else {
+      xEventGroupSetBits(s_wifi_event_group, WIFI_FAIL_BIT);
     }
+    ESP_LOGI(WIFI_TAG, "connect to the AP fail");
+  } else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) {
+    ip_event_got_ip_t *event = (ip_event_got_ip_t *)event_data;
+    ESP_LOGI(WIFI_TAG, "got ip:%s", ip4addr_ntoa(&event->ip_info.ip));
+    s_retry_num = 0;
+    xEventGroupSetBits(s_wifi_event_group, WIFI_CONNECTED_BIT);
+  }
 }
 
 void wifi_init_sta() {
-    ESP_LOGI(WIFI_TAG, "Starting WiFI station");
-    s_wifi_event_group = xEventGroupCreate();
+  ESP_LOGI(WIFI_TAG, "Starting WiFI station");
+  s_wifi_event_group = xEventGroupCreate();
 
-    tcpip_adapter_init();
+  tcpip_adapter_init();
 
-    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
-    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+  wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+  ESP_ERROR_CHECK(esp_wifi_init(&cfg));
 
-    ESP_ERROR_CHECK(esp_event_handler_register(WIFI_EVENT, ESP_EVENT_ANY_ID,
-                                               &event_handler, NULL));
-    ESP_ERROR_CHECK(esp_event_handler_register(IP_EVENT, IP_EVENT_STA_GOT_IP,
-                                               &event_handler, NULL));
+  ESP_ERROR_CHECK(esp_event_handler_register(WIFI_EVENT, ESP_EVENT_ANY_ID,
+                                             &event_handler, NULL));
+  ESP_ERROR_CHECK(esp_event_handler_register(IP_EVENT, IP_EVENT_STA_GOT_IP,
+                                             &event_handler, NULL));
 
-    wifi_sta_config_t sta_config = {
-            .ssid = CONFIG_ESP_WIFI_SSID,
-            .password = CONFIG_ESP_WIFI_PASSWORD
-    };
-    wifi_config_t wifi_config = {
-            .sta = sta_config,
-    };
+  wifi_sta_config_t sta_config = {.ssid = CONFIG_ESP_WIFI_SSID,
+                                  .password = CONFIG_ESP_WIFI_PASSWORD};
+  wifi_config_t wifi_config = {
+      .sta = sta_config,
+  };
 
-    /* Setting a password implies station will connect to all security modes
-     * including WEP/WPA. However these modes are deprecated and not advisable to
-     * be used. Incase your Access point doesn't support WPA2, these mode can be
-     * enabled by commenting below line */
+  /* Setting a password implies station will connect to all security modes
+   * including WEP/WPA. However these modes are deprecated and not advisable to
+   * be used. Incase your Access point doesn't support WPA2, these mode can be
+   * enabled by commenting below line */
 
-    if (strlen((char *) wifi_config.sta.password)) {
-        wifi_config.sta.threshold.authmode = WIFI_AUTH_WPA2_PSK;
-    }
+  if (strlen((char *)wifi_config.sta.password)) {
+    wifi_config.sta.threshold.authmode = WIFI_AUTH_WPA2_PSK;
+  }
 
-    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
-    ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_config));
-    ESP_ERROR_CHECK(esp_wifi_start());
+  ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
+  ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_config));
+  ESP_ERROR_CHECK(esp_wifi_start());
 
-    ESP_LOGI(WIFI_TAG, "wifi_init_sta finished.");
+  ESP_LOGI(WIFI_TAG, "wifi_init_sta finished.");
 
-    /* Waiting until either the connection is established (WIFI_CONNECTED_BIT) or
-     * connection failed for the maximum number of re-tries (WIFI_FAIL_BIT). The
-     * bits are set by event_handler() (see above) */
-    EventBits_t bits = xEventGroupWaitBits(s_wifi_event_group,
-                                           WIFI_CONNECTED_BIT | WIFI_FAIL_BIT,
-                                           pdFALSE, pdFALSE, portMAX_DELAY);
+  /* Waiting until either the connection is established (WIFI_CONNECTED_BIT) or
+   * connection failed for the maximum number of re-tries (WIFI_FAIL_BIT). The
+   * bits are set by event_handler() (see above) */
+  EventBits_t bits = xEventGroupWaitBits(s_wifi_event_group,
+                                         WIFI_CONNECTED_BIT | WIFI_FAIL_BIT,
+                                         pdFALSE, pdFALSE, portMAX_DELAY);
 
-    /* xEventGroupWaitBits() returns the bits before the call returned, hence we
-     * can test which event actually happened. */
-    if (bits & WIFI_CONNECTED_BIT) {
-        ESP_LOGI(WIFI_TAG, "connected to ap SSID:%s password:%s",
-                 CONFIG_ESP_WIFI_SSID, CONFIG_ESP_WIFI_PASSWORD);
-    } else if (bits & WIFI_FAIL_BIT) {
-        ESP_LOGI(WIFI_TAG, "Failed to connect to SSID:%s, password:%s",
-                 CONFIG_ESP_WIFI_SSID, CONFIG_ESP_WIFI_PASSWORD);
-    } else {
-        ESP_LOGE(WIFI_TAG, "UNEXPECTED EVENT");
-    }
+  /* xEventGroupWaitBits() returns the bits before the call returned, hence we
+   * can test which event actually happened. */
+  if (bits & WIFI_CONNECTED_BIT) {
+    ESP_LOGI(WIFI_TAG, "connected to ap SSID:%s password:%s",
+             CONFIG_ESP_WIFI_SSID, CONFIG_ESP_WIFI_PASSWORD);
+  } else if (bits & WIFI_FAIL_BIT) {
+    ESP_LOGI(WIFI_TAG, "Failed to connect to SSID:%s, password:%s",
+             CONFIG_ESP_WIFI_SSID, CONFIG_ESP_WIFI_PASSWORD);
+  } else {
+    ESP_LOGE(WIFI_TAG, "UNEXPECTED EVENT");
+  }
 
-    ESP_ERROR_CHECK(esp_event_handler_unregister(IP_EVENT, IP_EVENT_STA_GOT_IP,
-                                                 &event_handler));
-    ESP_ERROR_CHECK(esp_event_handler_unregister(WIFI_EVENT, ESP_EVENT_ANY_ID,
-                                                 &event_handler));
-    vEventGroupDelete(s_wifi_event_group);
+  ESP_ERROR_CHECK(esp_event_handler_unregister(IP_EVENT, IP_EVENT_STA_GOT_IP,
+                                               &event_handler));
+  ESP_ERROR_CHECK(esp_event_handler_unregister(WIFI_EVENT, ESP_EVENT_ANY_ID,
+                                               &event_handler));
+  vEventGroupDelete(s_wifi_event_group);
 }
--- devices/esp8266-freertos/main/main.c
+++ devices/esp8266-freertos/main/main.c
@@ -1,25 +1,25 @@
-#include <esp_types.h>
+#include <esp_event.h>
 #include <esp_log.h>
+#include <esp_netif.h>
+#include <esp_system.h>
+#include <esp_types.h>
 #include <nvs.h>
 #include <nvs_flash.h>
-#include <esp_system.h>
-#include "esp_event.h"
-#include "esp_netif.h"
 
-#include "hf_wifi.h"
 #include "hf_mqtt.h"
+#include "hf_wifi.h"
 
 static const char *TAG = "app";
 
 __unused void app_main() {
-    ESP_LOGI(TAG, "[APP] Startup..");
-    ESP_LOGI(TAG, "[APP] Free memory: %d bytes", esp_get_free_heap_size());
-    ESP_LOGI(TAG, "[APP] IDF version: %s", esp_get_idf_version());
+  ESP_LOGI(TAG, "[APP] Startup..");
+  ESP_LOGI(TAG, "[APP] Free memory: %d bytes", esp_get_free_heap_size());
+  ESP_LOGI(TAG, "[APP] IDF version: %s", esp_get_idf_version());
 
-    ESP_ERROR_CHECK(nvs_flash_init());
-    ESP_ERROR_CHECK(esp_netif_init());
-    ESP_ERROR_CHECK(esp_event_loop_create_default());
+  ESP_ERROR_CHECK(nvs_flash_init());
+  ESP_ERROR_CHECK(esp_netif_init());
+  ESP_ERROR_CHECK(esp_event_loop_create_default());
 
-    wifi_init_sta();
-    mqtt_connect();
+  wifi_init_sta();
+  mqtt_connect();
 }
