fa381fc328a136405bc147365ecc98476739bd1c
gbaranski <gbaranski19@gmail.com>
2021-01-05 21:55:24 +0100

    chore(actions): replace old actions in Node.JS with Go

--- actions/src/types/index.ts
+++
@@ -1,8 +0,0 @@
-export class DeviceOfflineException extends Error {
-  constructor(m: string) {
-    super(m);
-
-    // Set the prototype explicitly.
-    Object.setPrototypeOf(this, DeviceOfflineException.prototype);
-  }
-}
