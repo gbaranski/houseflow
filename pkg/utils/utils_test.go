6ce85e9c91d489a478ad1f4a02394cc2ade408ea
gbaranski <gbaranski19@gmail.com>
2021-01-07 18:37:10 +0100

    test: add testing ParseSignedPayload

--- pkg/utils/utils_test.go
+++ pkg/utils/utils_test.go
@@ -1,6 +1,8 @@
 package utils
 
 import (
+	"encoding/base64"
+	"strings"
 	"testing"
 )
 
@@ -17,6 +19,46 @@ func TestGenerateRandomString(t *testing.T) {
 					t.Fatalf("no enough randomness at length %d", i)
 				}
 			}
+		}
+	}
+}
+
+type Payload struct {
+	sig    string
+	encsig string
+	msg    string
+}
+
+func TestParseSignedPayload(t *testing.T) {
+	// Single constant
+	payloads := []Payload{
+		{
+			sig:    "helloworld",
+			encsig: "aGVsbG93b3JsZA==",
+			msg:    "houseflowisawesome",
+		},
+	}
+	for i := 0; i < 100; i++ {
+		sig := GenerateRandomString(32)
+		encSig := base64.StdEncoding.EncodeToString([]byte(sig))
+		payloads = append(payloads, Payload{
+			sig:    sig,
+			encsig: encSig,
+			msg:    GenerateRandomString(100),
+		})
+	}
+
+	for _, p := range payloads {
+		fp := strings.Join([]string{p.encsig, p.msg}, ".")
+		msg, sig, err := ParseSignedPayload([]byte(fp))
+		if err != nil {
+			t.Fatalf("fail err: %s", err.Error())
+		}
+		if msg != p.msg {
+			t.Fatalf("fail msg doesn't match, received: %s, expected: %s", msg, p.msg)
+		}
+		if string(sig) != p.sig {
+			t.Fatalf("fail sig doesn't match, received: %s, expected: %s", msg, p.msg)
 		}
 	}
 }
