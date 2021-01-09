6ec58b757215ef64905a60d7f93f31bc70944ee3
gbaranski <gbaranski19@gmail.com>
2021-01-09 22:14:21 +0100

    feat(types): add types for device

---
+++ pkg/types/device.go
@@ -0,0 +1,29 @@
+package types
+
+import (
+  "go.mongodb.org/mongo-driver/bson/primitive"
+  "github.com/gbaranski/houseflow/pkg/fulfillment"
+)
+
+// Device is device from database
+type Device struct {
+	fulfillment.Device
+	ID        primitive.ObjectID     `bson:"_id,omitempty" binding:"-"`
+	PublicKey string                 `bson:"publickey"`
+	State     map[string]interface{} `bson:"state"`
+}
+
+// DeviceResponse is type of response coming out of the device
+type DeviceResponse struct {
+	CorrelationData string                 `json:"correlationData"`
+	State           map[string]interface{} `json:"state"`
+	Status          string                 `json:"status"`
+	Error           string                 `json:"error,omitempty"`
+}
+
+// DeviceRequest is type of request incoming to the device
+type DeviceRequest struct {
+	CorrelationData string                 `json:"correlationData"`
+	State           map[string]interface{} `json:"state"`
+	Command         string                 `json:"command"`
+}
