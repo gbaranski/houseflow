bdd9af5d2e19d708b680d5489e845e9749f1b172
gbaranski <gbaranski19@gmail.com>
2021-01-09 11:14:32 +0100

    feat: remove JWT, replace with custom ala JWT implementation

--- go.mod
+++ go.mod
@@ -3,8 +3,9 @@ module github.com/gbaranski/houseflow
 go 1.15
 
 require (
+	bou.ke/monkey v1.0.2
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
-	github.com/dvsekhvalnov/jose2go v0.0.0-20201001154944-b09cfaf05951 // indirect
+	github.com/dvsekhvalnov/jose2go v0.0.0-20201001154944-b09cfaf05951
 	github.com/eclipse/paho.mqtt.golang v1.3.1
 	github.com/gbaranski/houseflow/webhooks v0.0.0-20201217214809-3f36b81d9ed2
 	github.com/gin-gonic/gin v1.6.3
--- pkg/utils/token.go
+++ pkg/utils/token.go
@@ -1,21 +1,17 @@
 package utils
 
 import (
+	"encoding/base64"
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"strings"
 	"time"
 
-	"github.com/dgrijalva/jwt-go"
-	jose "github.com/dvsekhvalnov/jose2go"
-	"github.com/google/uuid"
-)
+	"crypto/hmac"
+	"crypto/sha256"
 
-// TokenDetails combines token string and claims
-type TokenDetails struct {
-	Token  *jwt.Token
-	Claims jwt.StandardClaims
-}
+)
 
 const (
 	// AuthorizationCodeDuration defines expire duration for Authorization Code
@@ -24,73 +20,69 @@ const (
 	AccessTokenDuration = time.Hour
 )
 
-// TokenOptions define options for creating JWT Token
-type TokenOptions struct {
-	// Duration after token will expire
-	Duration time.Duration
-
-	// Secret key used to generate JWT, must be present
-	Key string
-
-	// Defines audience claim, used for adding user_id
-	Audience string
+// Token is some custom implementation ala JWT
+type Token struct {
+	Audience  string `json:"aud,omitempty"`
+  ExpiresAt int64 `json:"exp"`
+  ID        string `json:"id"`
 }
 
-// CreateJWTToken creates JWT Token with options
-func CreateJWTToken(opts TokenOptions) (*TokenDetails, error) {
-	jose.Sign()
-	id, err := uuid.NewRandom()
-	if err != nil {
-		return nil, err
-	}
-	claims := jwt.StandardClaims{
-		Id:       id.String(),
-		Audience: opts.Audience,
-	}
-	if opts.Duration != 0 {
-		claims.ExpiresAt = time.Now().Add(opts.Duration).Unix()
-	}
-	if claims.Valid() != nil {
-		return nil, fmt.Errorf("invalid claims: %s", claims.Valid().Error())
-	}
-	token, err := jwt.NewWithClaims(jwt.SigningMethodHS256, claims).SignedString([]byte(opts.Key))
-	if err != nil {
-		return nil, err
-	}
-	return VerifyToken(token, []byte(opts.Key))
+func (t *Token) Sign(key []byte) (string, error) {
+  payload, err := json.Marshal(t)
+  if err != nil {
+    return "", err
+  }
+  mac := hmac.New(sha256.New, key)
+  _, err = mac.Write(payload)
+  if err != nil {
+    return "", err
+  }
+
+  sig := base64.StdEncoding.EncodeToString(mac.Sum(nil))
+
+  payloadEnc := base64.StdEncoding.EncodeToString(payload)
+  token := strings.Join([]string{sig, payloadEnc}, ".")
+
+  return token, nil
 }
 
-// VerifyToken verifyes jwt token, secretEnv must be some enviroent variable
-func VerifyToken(strtoken string, key []byte) (*TokenDetails, error) {
-	token, err := jwt.ParseWithClaims(strtoken, &jwt.StandardClaims{}, func(token *jwt.Token) (interface{}, error) {
-		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
-			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
-		}
-		return key, nil
-	})
-	if err != nil {
-		return nil, err
-	}
-	if !token.Valid {
-		return nil, fmt.Errorf("token is invalid")
-	}
-
-	if token.Claims.Valid() != nil {
-		return nil, token.Claims.Valid()
-	}
-
-	claims, ok := token.Claims.(*jwt.StandardClaims)
-	if claims.Valid() != nil {
-		return nil, claims.Valid()
-	}
-	if !ok {
-		return nil, fmt.Errorf("claims are invalid")
-	}
-	return &TokenDetails{
-		Token:  token,
-		Claims: *claims,
-	}, nil
+func (t *Token) Equal(t2 Token) bool {
+  return t.Audience == t2.Audience && t.ExpiresAt == t2.ExpiresAt && t.ID == t2.ID
 }
+
+func VerifyToken(strtoken string, key []byte) (*Token, error) {
+  tokenSplitted := strings.Split(strtoken, ".")
+  if len(tokenSplitted) != 2 {
+    return nil, fmt.Errorf("token doesn't contain signature or payload")
+  }
+  sig, err := base64.StdEncoding.DecodeString(tokenSplitted[0])
+  if err != nil {
+    return nil, err
+  }
+  payload, err := base64.StdEncoding.DecodeString(tokenSplitted[1])
+  if err != nil {
+    return nil, err
+  }
+  mac := hmac.New(sha256.New, key)
+  _, err = mac.Write(payload)
+  if err != nil {
+    return nil, err
+  }
+  expectedMAC := mac.Sum(nil)
+  if !hmac.Equal(sig, expectedMAC) {
+    return nil, fmt.Errorf("token is invalid")
+  }
+  var token Token
+  err = json.Unmarshal(payload, &token)
+  if err != nil {
+    return nil, err
+  }
+  if token.ExpiresAt < time.Now().Unix() {
+    return nil, fmt.Errorf("token is expired")
+  }
+  return &token, nil
+}
+
 
 // ExtractHeaderToken extracts Bearer authorization token
 func ExtractHeaderToken(r *http.Request) *string {
