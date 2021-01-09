1b592bd28ccf74135372ac9e66679ef5c1eba9a7
gbaranski <gbaranski19@gmail.com>
2021-01-09 12:51:38 +0100

    refactor: restructure db pkg, fix few flaws

--- pkg/database/mongo.go
+++ pkg/database/mongo.go
@@ -2,11 +2,8 @@ package database
 
 import (
 	"context"
-	"errors"
 	"fmt"
 	"log"
-	"os"
-	"time"
 
 	"github.com/gbaranski/houseflow/pkg/types"
 	"github.com/gbaranski/houseflow/pkg/utils"
@@ -17,126 +14,168 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
 
-const mongoUsernameEnv string = "MONGO_INITDB_ROOT_USERNAME"
-const mongoPasswordEnv string = "MONGO_INITDB_ROOT_PASSWORD"
-
-// MongoOptions defines options for mongoDB
+// MongoOptions defines options for mongoDB, use NewMongoOptions for constructor
 type MongoOptions struct {
 	// Defines if should connect to Mongo
 	//
 	// Default: false
 	Enabled bool
+
+  // Username for mongoDB
+  //
+  // Default: "root"
+  Username string
+
+  // Password for mongoDB
+  //
+  // Default: "example"
+  Password string
+
+  // Name of the database, 
+  //
+  // Default: houseflowDB
+  DatabaseName string
 }
 
-// Mongo contains db and client
+// Parse parses the options and set the defaults
+func (opts *MongoOptions) Parse() {
+  if opts.DatabaseName == "" {
+    opts.DatabaseName = "houseflowDB"
+  }
+  if opts.Username == "" {
+    opts.Username = "root"
+  }
+  if opts.Password == "" {
+    opts.Password = "example"
+  }
+}
+
+// Collections hold avaialble connections for houseflowDB
+type Collections struct {
+  Users *mongo.Collection
+  Devices *mongo.Collection
+
+}
+
+//  Mongo contains db and client
 type Mongo struct {
 	db              *mongo.Database
 	Client          *mongo.Client
-	usersCollection *mongo.Collection
+  Collections     Collections
 }
 
-func getMongoCredentials() (*string, *string, error) {
-	username, present := os.LookupEnv(mongoUsernameEnv)
-	if !present {
-		return nil, nil, fmt.Errorf("%s not set in .env", mongoUsernameEnv)
+func newCollections(ctx context.Context, db *mongo.Database) (Collections, error) {
+  users := db.Collection("users")
+	name, err := createIndexForUnique(ctx, users, "email")
+	if err != nil {
+		return Collections{}, err
 	}
-	password, present := os.LookupEnv(mongoPasswordEnv)
-	if !present {
-		return nil, nil, fmt.Errorf("%s not set in .env", mongoPasswordEnv)
-	}
-	return &username, &password, nil
+  log.Println("Created unique index for usersCollection name:", name)
+
+  devices := db.Collection("devices")
+  return Collections{
+    Users: users,
+    Devices: devices,
+  },nil
 }
 
-func createMongo(ctx context.Context) (*Mongo, error) {
-	username, password, err := getMongoCredentials()
-	if err != nil {
-		return nil, err
-	}
-	client, err := mongo.Connect(ctx, options.Client().ApplyURI(fmt.Sprintf("mongodb://%s:%s@mongo:27017", *username, *password)))
-	if err != nil {
-		return nil, err
-	}
-	db := client.Database("houseflowDB")
 
+// NewMongo creates Mongo struct and returns it, connects to mongoDB with given options
+func NewMongo(ctx context.Context, opts MongoOptions) (Mongo, error) {
+  opts.Parse()
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(fmt.Sprintf("mongodb://%s:%s@mongo:27017", opts.Username, opts.Password)))
+	if err != nil {
+		return Mongo{}, err
+	}
 	// Ping the primary
 	if err := client.Ping(ctx, readpref.Primary()); err != nil {
-		return nil, err
+		return Mongo{}, err
 	}
+
 	log.Println("Successfully connected and pinged.")
 
-	usersCollection := db.Collection("users")
-	name, err := createIndexForUnique(usersCollection, "email")
-	if err != nil {
-		return nil, err
-	}
-	log.Println("Created unique index for usersCollection name:", name)
+	db := client.Database("houseflowDB")
+  collections, err := newCollections(ctx, db)
+  if err != nil {
+    return Mongo{}, err
+  }
 
-	return &Mongo{
+	return Mongo{
 		db:              db,
 		Client:          client,
-		usersCollection: usersCollection,
+    Collections: collections,
 	}, nil
 
 }
 
 // GetUser returns found user from DB
-func (m *Mongo) GetUser(email string) (*types.User, error) {
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
-	result := m.usersCollection.FindOne(ctx, bson.M{"email": email})
+func (m *Mongo) GetUser(ctx context.Context, email string) (types.User, error) {
+	result := m.Collections.Users.FindOne(ctx, bson.M{"email": email})
 	if result.Err() != nil {
-		return nil, result.Err()
+		return types.User{}, result.Err()
 	}
 
-	var user *types.User
+	var user types.User
 	if err := result.Decode(&user); err != nil {
-		return nil, err
+		return types.User{}, err
 	}
 
 	return user, nil
 }
 
 // AddUser adds user to db
-func (m *Mongo) AddUser(user types.User) (*primitive.ObjectID, error) {
+func (m *Mongo) AddUser(ctx context.Context, user types.User) (primitive.ObjectID, error) {
 	password, err := utils.HashPassword(user.Password)
 	if err != nil {
-		return nil, err
+		return primitive.ObjectID{}, err
 	}
 
 	user.Password = string(password)
 	user.Devices = []string{}
 
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
-	res, err := m.usersCollection.InsertOne(ctx, user)
+	res, err := m.Collections.Users.InsertOne(ctx, user)
 	if err != nil {
-		return nil, err
+		return primitive.ObjectID{}, err
 	}
 	id := res.InsertedID.(primitive.ObjectID)
-	return &id, nil
+	return id, nil
 }
 
-func createIndexForUnique(collection *mongo.Collection, key string) (string, error) {
+// createIndexForUnique creates unique index for speicfic key
+func createIndexForUnique(ctx context.Context, collection *mongo.Collection, key string) (string, error) {
 	model := mongo.IndexModel{
 		Keys:    bson.D{{Key: key, Value: 1}},
 		Options: options.Index().SetUnique(true),
 	}
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-
-	defer cancel()
-
 	return collection.Indexes().CreateOne(ctx, model)
 }
 
-// IsDuplicateError checks if mongo write error is about duplicate
-func IsDuplicateError(err error) bool {
-	var e mongo.WriteException
-	if errors.As(err, &e) {
-		for _, we := range e.WriteErrors {
-			if we.Code == 11000 {
-				return true
-			}
-		}
+// UpdateDeviceState updates "state" property on device
+func (m *Mongo) UpdateDeviceState(ctx context.Context, deviceID primitive.ObjectID, state map[string]interface{}) error {
+	result, err := m.Collections.Users.UpdateOne(ctx,
+		bson.M{"_id": deviceID},
+		bson.D{{"$set", bson.D{{"state", state}}}})
+	if err != nil {
+		return err
 	}
-	return false
+	if result.ModifiedCount < 1 {
+		return fmt.Errorf("no document modified")
+	}
+  return nil
 }
+
+// UpdateDeviceOnlineState modifies only the state.online property to "online" arg
+func (m *Mongo) UpdateDeviceOnlineState(ctx context.Context, deviceID primitive.ObjectID, online bool) error {
+	res, err := m.Collections.Devices.UpdateOne(ctx, bson.M{"_id": deviceID}, bson.M{
+		"$set": bson.M{
+			"state.online": online}})
+	if err != nil {
+		return err
+	}
+	if res.ModifiedCount < 1 {
+		return fmt.Errorf("Not matched any devices")
+	}
+
+	return nil
+}
+
