66217abcb1fb594df08e2e0db6591be287411229
gbaranski <gbaranski19@gmail.com>
2021-01-12 16:30:02 +0100

    fix(actions): add bson tags for struct fields

--- pkg/fulfillment/fulfillment.go
+++ pkg/fulfillment/fulfillment.go
@@ -27,70 +27,70 @@ const (
 // DeviceInfo contains fields describing the device for use in one-off logic if needed (e.g. 'broken firmware version X of light Y requires adjusting color', or 'security flaw requires notifying all users of firmware Z').
 type DeviceInfo struct {
 	// Especially useful when the developer is a hub for other devices. Google may provide a standard list of manufacturers here so that e.g. TP-Link and Smartthings both describe 'osram' the same way.
-	Manufacturer string `json:"manufacturer,omitempty"`
+	Manufacturer string `json:"manufacturer,omitempty" bson:"manufacturer,omitempty"`
 
 	// The model or SKU identifier of the particular device.
-	Model string `json:"model,omitempty"`
+	Model string `json:"model,omitempty" bson:"model,omitempty"`
 
 	// Specific version number attached to the hardware if available.
-	HwVersion string `json:"hwVersion,omitempty"`
+	HwVersion string `json:"hwVersion,omitempty" bson:"hwVersion,omitempty"`
 
 	// Specific version number attached to the software/firmware, if available.
-	SwVersion string `json:"swVersion,omitempty"`
+	SwVersion string `json:"swVersion,omitempty" bson:"swVersion,omitempty"`
 }
 
 // OtherDeviceID Alternate device ID.
 type OtherDeviceID struct {
 	// The agent's ID. Generally, this is the project ID in the Actions console.
-	AgentID string `json:"agentId,omitempty"`
+	AgentID string `json:"agentId,omitempty" bson:"agentId,omitempty"`
 
 	// Device ID defined by the agent. The device ID must be unique.
-	DeviceID string `json:"deviceId"`
+	DeviceID string `json:"deviceId" bson:"deviceId"`
 }
 
 // DeviceName names of this device.
 type DeviceName struct {
 	// Primary name of the device, generally provided by the user. This is also the name the Assistant will prefer to describe the device in responses.
-	Name string `json:"name" binding:"required"`
+	Name string `json:"name" binding:"required" bson:"name"`
 
 	// List of names provided by the developer rather than the user, often manufacturer names, SKUs, etc.
-	DefaultNames []string `json:"defaultNames,omitempty"`
+	DefaultNames []string `json:"defaultNames,omitempty" bson:"defaultNames,omitempty"`
 
 	// Additional names provided by the user for the device.
-	Nicknames []string `json:"nicknames,omitempty"`
+	Nicknames []string `json:"nicknames,omitempty" bson:"nicknames,omitempty"`
 }
 
 // Device metadata.
 type Device struct {
 	// The ID of the device in the developer's cloud. This must be unique for the user and for the developer, as in cases of sharing we may use this to dedupe multiple views of the same device. It should be immutable for the device; if it changes, the Assistant will treat it as a new device.
-	ID string `json:"id" binding:"required"`
+	ID string `json:"id" bson:"-" binding:"required"`
 
 	// The hardware type of device.
-	Type string `json:"type" binding:"required"`
+	Type string `json:"type" binding:"required" bson:"type"`
 
 	// List of traits this device has. This defines the commands, attributes, and states that the device supports.
-	Traits []string `json:"traits" binding:"required"`
+	Traits []string `json:"traits" binding:"required" bson:"traits"`
 
 	// Names of this device.
-	Name DeviceName `json:"name" binding:"required"`
+	Name DeviceName `json:"name" binding:"required" bson:"name"`
 
 	// Indicates whether this device will have its states updated by the Real Time Feed. (true to use the Real Time Feed for reporting state, and false to use the polling model.)
-	WillReportState bool `json:"willReportState" binding:"required"`
+	WillReportState bool `json:"willReportState" binding:"required" bson:"willReportState"`
 
 	// Provides the current room of the device in the user's home to simplify setup.
-	RoomHint string `json:"roomHint,omitempty"`
+	RoomHint string `json:"roomHint,omitempty" bson:"roomHint,omitempty"`
 
 	// Contains fields describing the device for use in one-off logic if needed (e.g. 'broken firmware version X of light Y requires adjusting color', or 'security flaw requires notifying all users of firmware Z').
-	DeviceInfo *DeviceInfo `json:"deviceInfo,omitempty"`
+	DeviceInfo *DeviceInfo `json:"deviceInfo,omitempty" bson:"deviceInfo,omitempty"`
 
 	// Aligned with per-trait attributes described in each trait schema reference.
-	Attributes map[string]interface{} `json:"attributes,omitempty"`
+	Attributes map[string]interface{} `json:"attributes,omitempty" bson:"attributes,omitempty"`
 
 	// This is a special object defined by the developer which will be attached to future QUERY and EXECUTE requests. Developers can use this object to store additional information about the device to improve performance or routing within their cloud, such as the global region of the device. Data in this object has a few constraints: No Personally Identifiable Information. Data should change rarely, akin to other attributes -- so this should not contain real-time state. The total object is limited to 512 bytes per device.
-	CustomData map[string]interface{} `json:"customData,omitempty"`
+	CustomData map[string]interface{} `json:"customData,omitempty" bson:"customData,omitempty"`
 
 	// List of alternate IDs used to identify a cloud synced device for local execution.
-	OtherDevicesIDs []OtherDeviceID `json:"otherDeviceIds,omitempty"`
+	OtherDeviceIDs []OtherDeviceID `json:"otherDeviceIds,omitempty" bson:"otherDeviceIds,omitempty"`
 }
 
 // BaseRequestInput ...
