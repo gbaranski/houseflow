dcf212a6f93c7be8d185f9ee7f09900491579a77
gbaranski <gbaranski19@gmail.com>
2021-01-12 22:29:14 +0100

    test(auth): add testing valid password

--- internal/auth/auth_test.go
+++ internal/auth/auth_test.go
@@ -2,6 +2,7 @@ package auth
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"net/http/httptest"
@@ -113,4 +114,86 @@ func TestLoginInvalidPassword(t *testing.T) {
 	if w.Code != http.StatusUnauthorized {
 		t.Fatalf("unexpected /login response %d", w.Code)
 	}
+}
+
+func TestLoginValidPassword(t *testing.T) {
+	lpQuery := url.Values{}
+	encoder.Encode(LoginPageQuery{
+		ClientID:     opts.ClientID,
+		RedirectURI:  fmt.Sprintf("https://oauth-redirect.googleusercontent.com/r/%s", opts.ProjectID),
+		State:        utils.GenerateRandomString(20),
+		ResponseType: "code",
+		UserLocale:   "en_US",
+	}, lpQuery)
+
+	form := LoginCredentials{
+		Email:    realUser.Email,
+		Password: "helloworld", // valid password
+	}
+	data := url.Values{}
+	err := encoder.Encode(form, data)
+	if err != nil {
+		panic(err)
+	}
+
+	w := httptest.NewRecorder()
+	req, err := http.NewRequest(http.MethodPost, fmt.Sprintf("/login?%s", lpQuery.Encode()), strings.NewReader(data.Encode()))
+	if err != nil {
+		panic(err)
+	}
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	req.Header.Add("Content-Length", strconv.Itoa(len(data.Encode())))
+	a.Router.ServeHTTP(w, req)
+
+	if w.Code != http.StatusSeeOther {
+		t.Fatalf("unexpected /login response %d", w.Code)
+	}
+	redirectURL := w.HeaderMap["Location"][0]
+	url, err := url.Parse(redirectURL)
+	if err != nil {
+		t.Fatalf("redirected URL is invalid: %s", redirectURL)
+	}
+	code := url.Query().Get("code")
+
+	if _, err = utils.VerifyToken(code, []byte(opts.AuthorizationCodeKey)); err != nil {
+		t.Fatalf("fail verify authorization code %s", err.Error())
+	}
+	GetAuthorizationCodeGrant(t, code)
+}
+
+func GetAuthorizationCodeGrant(t *testing.T, code string) {
+	form := url.Values{}
+	encoder.Encode(TokenQuery{
+		ClientID:     opts.ClientID,
+		ClientSecret: opts.ClientSecret,
+		GrantType:    "authorization_code",
+		Code:         code,
+		RedirectURI:  fmt.Sprintf("https://oauth-redirect.googleusercontent.com/r/%s", opts.ProjectID),
+	}, form)
+
+	w := httptest.NewRecorder()
+	req, err := http.NewRequest(http.MethodPost, "/token", strings.NewReader(form.Encode()))
+	if err != nil {
+		panic(err)
+	}
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	req.Header.Add("Content-Length", strconv.Itoa(len(form.Encode())))
+	a.Router.ServeHTTP(w, req)
+
+	if w.Code != 200 {
+		t.Fatalf("unexpected response: %d, body: %s", w.Code, w.Body.String())
+	}
+	var res AuthorizationCodeGrantResponse
+	if err = json.Unmarshal(w.Body.Bytes(), &res); err != nil {
+		t.Fatalf("fail decode body response, err: %s, body: %s", err.Error(), w.Body.String())
+	}
+
+	if _, err = utils.VerifyToken(res.AccessToken, []byte(opts.AccessKey)); err != nil {
+		t.Fatalf("fail verify access token %s", err.Error())
+	}
+
+	if _, err = utils.VerifyToken(res.RefreshToken, []byte(opts.RefreshKey)); err != nil {
+		t.Fatalf("fail verify refresh token %s", err.Error())
+	}
+
 }
