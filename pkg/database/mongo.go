a4e9c30a9342eccea6652b2f39938fe59c172906
gbaranski <gbaranski19@gmail.com>
2021-01-12 13:46:15 +0100

    fix(mongo): reformat, fix issue with bson

--- pkg/database/mongo.go
+++ pkg/database/mongo.go
@@ -16,68 +16,66 @@ import (
 
 // MongoOptions defines options for mongoDB, use NewMongoOptions for constructor
 type MongoOptions struct {
-  // Username for mongoDB
-  //
-  // Default: "root"
-  Username string
+	// Username for mongoDB
+	//
+	// Default: "root"
+	Username string
 
-  // Password for mongoDB
-  //
-  // Default: "example"
-  Password string
+	// Password for mongoDB
+	//
+	// Default: "example"
+	Password string
 
-  // Name of the database, 
-  //
-  // Default: houseflowDB
-  DatabaseName string
+	// Name of the database,
+	//
+	// Default: houseflowDB
+	DatabaseName string
 }
 
 // Parse parses the options and set the defaults
 func (opts *MongoOptions) Parse() {
-  if opts.DatabaseName == "" {
-    opts.DatabaseName = "houseflowDB"
-  }
-  if opts.Username == "" {
-    opts.Username = "root"
-  }
-  if opts.Password == "" {
-    opts.Password = "example"
-  }
+	if opts.DatabaseName == "" {
+		opts.DatabaseName = "houseflowDB"
+	}
+	if opts.Username == "" {
+		opts.Username = "root"
+	}
+	if opts.Password == "" {
+		opts.Password = "example"
+	}
 }
 
 // Collections hold avaialble connections for houseflowDB
 type Collections struct {
-  Users *mongo.Collection
-  Devices *mongo.Collection
-
+	Users   *mongo.Collection
+	Devices *mongo.Collection
 }
 
 //  Mongo contains db and client
 type Mongo struct {
-	db              *mongo.Database
-	Client          *mongo.Client
-  Collections     Collections
+	db          *mongo.Database
+	Client      *mongo.Client
+	Collections Collections
 }
 
 func newCollections(ctx context.Context, db *mongo.Database) (Collections, error) {
-  users := db.Collection("users")
+	users := db.Collection("users")
 	name, err := createIndexForUnique(ctx, users, "email")
 	if err != nil {
 		return Collections{}, err
 	}
-  log.Println("Created unique index for usersCollection name:", name)
+	log.Println("Created unique index for usersCollection name:", name)
 
-  devices := db.Collection("devices")
-  return Collections{
-    Users: users,
-    Devices: devices,
-  },nil
+	devices := db.Collection("devices")
+	return Collections{
+		Users:   users,
+		Devices: devices,
+	}, nil
 }
-
 
 // NewMongo creates Mongo struct and returns it, connects to mongoDB with given options
 func NewMongo(ctx context.Context, opts MongoOptions) (Mongo, error) {
-  opts.Parse()
+	opts.Parse()
 	client, err := mongo.Connect(ctx, options.Client().ApplyURI(fmt.Sprintf("mongodb://%s:%s@mongo:27017", opts.Username, opts.Password)))
 	if err != nil {
 		return Mongo{}, err
@@ -90,15 +88,15 @@ func NewMongo(ctx context.Context, opts MongoOptions) (Mongo, error) {
 	log.Println("Successfully connected and pinged.")
 
 	db := client.Database("houseflowDB")
-  collections, err := newCollections(ctx, db)
-  if err != nil {
-    return Mongo{}, err
-  }
+	collections, err := newCollections(ctx, db)
+	if err != nil {
+		return Mongo{}, err
+	}
 
 	return Mongo{
-		db:              db,
-		Client:          client,
-    Collections: collections,
+		db:          db,
+		Client:      client,
+		Collections: collections,
 	}, nil
 
 }
@@ -164,14 +162,18 @@ func createIndexForUnique(ctx context.Context, collection *mongo.Collection, key
 func (m *Mongo) UpdateDeviceState(ctx context.Context, deviceID primitive.ObjectID, state map[string]interface{}) error {
 	result, err := m.Collections.Users.UpdateOne(ctx,
 		bson.M{"_id": deviceID},
-		bson.D{{"$set", bson.D{{"state", state}}}})
+		bson.M{
+			"$set": bson.M{
+				"state": state,
+			},
+		})
 	if err != nil {
 		return err
 	}
 	if result.ModifiedCount < 1 {
 		return fmt.Errorf("no document modified")
 	}
-  return nil
+	return nil
 }
 
 // UpdateDeviceOnlineState modifies only the state.online property to "online" arg
@@ -191,7 +193,11 @@ func (m *Mongo) UpdateDeviceOnlineState(ctx context.Context, deviceID primitive.
 
 // GetUserDevices retreives user devices
 func (m *Mongo) GetDevicesByIDs(ctx context.Context, deviceIDs []primitive.ObjectID) ([]types.Device, error) {
-	cur, err := m.Collections.Devices.Find(ctx, bson.M{"_id": bson.M{"$in": deviceIDs}})
+	cur, err := m.Collections.Devices.Find(ctx,
+		bson.M{"_id": bson.M{
+			"$in": deviceIDs,
+		},
+		})
 	if err != nil {
 		return nil, err
 	}
@@ -201,4 +207,3 @@ func (m *Mongo) GetDevicesByIDs(ctx context.Context, deviceIDs []primitive.Objec
 	}
 	return devices, nil
 }
-
