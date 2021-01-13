7e03d367a980c0e4763c6e36b93bfbc20fcca1c4
gbaranski <gbaranski19@gmail.com>
2021-01-13 21:32:01 +0100

    chore: remove webapp, won't be used anymore

--- app_web/src/access.ts
+++
@@ -1,9 +0,0 @@
-// src/access.ts
-import { Client } from '@houseflow/types';
-
-export default function access(initialState: { firebaseUser?: Client.FirebaseUser }) {
-  const { firebaseUser } = initialState || {};
-  return {
-    canAdmin: firebaseUser ? firebaseUser.role === 'admin' : false,
-  };
-}
--- app_web/src/utils/constants.ts
+++
@@ -1,5 +0,0 @@
-export const CARD_MIN_HEIGHT = 180;
-export const CARD_MIN_WIDTH = 300;
-
-export const GITHUB_URL = 'https://github.com/gbaranski/houseflow';
-export const CONTACT_MAIL = 'gbaranski19@gmail.com';
