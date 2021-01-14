523ec8db5e4beea4096f039fd5fc98a55f8c992e
gbaranski <gbaranski19@gmail.com>
2021-01-14 23:17:12 +0100

    fix: TokenQuery schema tags

--- internal/auth/types.go
+++ internal/auth/types.go
@@ -26,12 +26,12 @@ func DecodeLoginPageQuery(u url.Values) LoginPageQuery {
 
 // TokenQuery sent by google
 type TokenQuery struct {
-	ClientID     string `form:"client_id" binding:"required"`
-	ClientSecret string `form:"client_secret" binding:"required"`
-	GrantType    string `form:"grant_type" binding:"required"`
-	Code         string `form:"code" binding:"required_if=GrantType authorization_code"`
-	RedirectURI  string `form:"redirect_uri" binding:"required_if=GrantType authorization_code"`
-	RefreshToken string `form:"refresh_token" binding:"required_if=GrantType refresh_token"`
+	ClientID     string `schema:"client_id" binding:"required"`
+	ClientSecret string `schema:"client_secret" binding:"required"`
+	GrantType    string `schema:"grant_type" binding:"required"`
+	Code         string `schema:"code" binding:"required_if=GrantType authorization_code"`
+	RedirectURI  string `schema:"redirect_uri" binding:"required_if=GrantType authorization_code"`
+	RefreshToken string `schema:"refresh_token" binding:"required_if=GrantType refresh_token"`
 }
 
 // LoginCredentials is body of the request sent to /login
