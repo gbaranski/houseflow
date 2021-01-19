6c3a21c8f6fa79b3936a8dfa204047ff644fa0dd
gbaranski <gbaranski19@gmail.com>
2021-01-19 22:34:41 +0100

    fix(fulfillment): update execute fulfillment

--- internal/fulfillment/execute.go
+++ internal/fulfillment/execute.go
@@ -2,9 +2,7 @@ package fulfillment
 
 import (
 	"context"
-	"fmt"
 	"net/http"
-	"time"
 
 	"github.com/gbaranski/houseflow/pkg/fulfillment"
 	"github.com/gbaranski/houseflow/pkg/mqtt"
@@ -13,83 +11,80 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+func (f *Fulfillment) executeCommand(
+	ctx context.Context,
+	user types.User,
+	targetDevice fulfillment.QueryRequestPayloadDevice,
+	execution fulfillment.ExecuteRequestExecution,
+) fulfillment.ExecuteResponseCommands {
+
+	perms, err := f.db.GetUserDevicePermissions(ctx, user.ID, targetDevice.ID)
+	if err != nil {
+		return fulfillment.ExecuteResponseCommands{
+			IDs:       []string{targetDevice.ID},
+			Status:    "ERROR",
+			ErrorCode: "hardError",
+		}
+	}
+	if !perms.Execute {
+		return fulfillment.ExecuteResponseCommands{
+			IDs:       []string{targetDevice.ID},
+			Status:    fulfillment.StatusError,
+			ErrorCode: "authFailure",
+		}
+	}
+	device, err := f.db.GetDeviceByID(ctx, targetDevice.ID)
+	if err != nil {
+		return fulfillment.ExecuteResponseCommands{
+			IDs:       []string{targetDevice.ID},
+			Status:    "ERROR",
+			ErrorCode: "hardError",
+		}
+	}
+
+	req := types.DeviceRequest{
+		CorrelationData: utils.GenerateRandomString(16),
+		State:           execution.Params,
+		Command:         execution.Command,
+	}
+	response, err := f.dm.SendRequestWithResponse(ctx, device, req)
+	if err != nil {
+		if err == mqtt.ErrDeviceTimeout {
+			return fulfillment.ExecuteResponseCommands{
+				IDs:       []string{targetDevice.ID},
+				Status:    fulfillment.StatusOffline,
+				ErrorCode: "offline",
+			}
+		}
+		if err == mqtt.ErrInvalidSignature {
+			return fulfillment.ExecuteResponseCommands{
+				IDs:       []string{targetDevice.ID},
+				Status:    fulfillment.StatusError,
+				ErrorCode: "transientError",
+			}
+		}
+		return fulfillment.ExecuteResponseCommands{
+			IDs:       []string{targetDevice.ID},
+			Status:    fulfillment.StatusError,
+			ErrorCode: "hardError",
+		}
+	}
+
+	return fulfillment.ExecuteResponseCommands{
+		IDs:    []string{targetDevice.ID},
+		Status: "SUCCESS",
+		States: response.State,
+	}
+}
+
 // OnExecute https://developers.google.com/assistant/smarthome/reference/intent/execute
-func (f *Fulfillment) onExecute(c *gin.Context, r fulfillment.ExecuteRequest, user types.User, userDevices []types.Device) {
+func (f *Fulfillment) onExecute(c *gin.Context, r fulfillment.ExecuteRequest, user types.User) {
 	var responseCommands []fulfillment.ExecuteResponseCommands
 	for _, command := range r.Inputs[0].Payload.Commands {
 		for _, execution := range command.Execution {
 			for _, device := range command.Devices {
-				// Search userDevices for the devices which Execute request wants to match
-				var correspondingDBDevice *types.Device
-				for _, userDevice := range userDevices {
-					if userDevice.ID.Hex() == device.ID {
-						correspondingDBDevice = &userDevice
-						break
-					}
-				}
-
-				if correspondingDBDevice == nil {
-					responseCommands = append(
-						responseCommands,
-						fulfillment.ExecuteResponseCommands{
-							IDs:       []string{device.ID},
-							Status:    fulfillment.StatusError,
-							ErrorCode: "relinkRequired",
-						})
-					continue
-				}
-
-				request := types.DeviceRequest{
-					CorrelationData: utils.GenerateRandomString(16),
-					State:           execution.Params,
-					Command:         execution.Command,
-				}
-				ctx, cancel := context.WithTimeout(context.Background(), time.Second*3)
-				defer cancel()
-				deviceResponse, err := f.dm.SendRequestWithResponse(ctx, *correspondingDBDevice, request)
-				if err != nil {
-					fmt.Println("Error occured when executing on device: ", err.Error())
-					if err == mqtt.ErrDeviceTimeout {
-						responseCommands = append(responseCommands, fulfillment.ExecuteResponseCommands{
-							IDs:       []string{device.ID},
-							Status:    fulfillment.StatusOffline,
-							ErrorCode: "offline",
-							States: map[string]interface{}{
-								"online": false,
-							},
-						})
-						// should also change state in db
-					} else if err == mqtt.ErrInvalidSignature {
-						responseCommands = append(responseCommands, fulfillment.ExecuteResponseCommands{
-							IDs:       []string{device.ID},
-							Status:    fulfillment.StatusError,
-							ErrorCode: "transientError",
-							States:    correspondingDBDevice.State,
-						})
-					} else {
-						fmt.Println("Unknown error, ", err.Error())
-						responseCommands = append(responseCommands, fulfillment.ExecuteResponseCommands{
-							IDs:       []string{device.ID},
-							Status:    fulfillment.StatusError,
-							ErrorCode: "hardError",
-							States:    correspondingDBDevice.State,
-						})
-					}
-					continue
-				}
-				responseCommands = append(responseCommands, fulfillment.ExecuteResponseCommands{
-					IDs:       []string{device.ID},
-					Status:    deviceResponse.Status,
-					States:    deviceResponse.State,
-					ErrorCode: deviceResponse.Error,
-				})
-				ctx, cancel = context.WithTimeout(context.Background(), time.Second*3)
-				defer cancel()
-				err = f.db.UpdateDeviceState(ctx, correspondingDBDevice.ID, deviceResponse.State)
-				if err != nil {
-					fmt.Println("failed updating state, ", err.Error())
-				}
-
+				exec := f.executeCommand(c.Request.Context(), user, device, execution)
+				responseCommands = append(responseCommands, exec)
 			}
 		}
 	}
