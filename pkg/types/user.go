dcc0bd95d7c162e8c585d34f61276e7004d9b206
gbaranski <gbaranski19@gmail.com>
2021-01-20 19:33:20 +0100

    fix(types): fix user type

--- pkg/types/user.go
+++ pkg/types/user.go
@@ -1,15 +1,11 @@
 package types
 
-import (
-	"go.mongodb.org/mongo-driver/bson/primitive"
-)
-
 // User struct, used to either creating user, or this one in DB
 type User struct {
-	ID        primitive.ObjectID `bson:"_id,omitempty" json:"id,omitempty" binding:"-"`
-	FirstName string             `json:"firstName" bson:"firstName" form:"firstName" binding:"required"`
-	LastName  string             `json:"lastName" bson:"lastName" form:"lastName" binding:"required"`
-	Email     string             `json:"email" bson:"email" houseflow:"email" form:"email" binding:"required,email"`
-	Password  string             `json:"password" bson:"password" form:"password" binding:"required,min=8,max=20"`
-	Devices   []string           `bson:"devices"`
+	ID           string `json:"id" form:"-"`
+	FirstName    string `json:"firstName" form:"firstName" binding:"required"`
+	LastName     string `json:"lastName" form:"lastName" binding:"required"`
+	Email        string `json:"email" houseflow:"email" form:"email" binding:"required,email"`
+	Password     string `json:"password" form:"password" binding:"required,min=8,max=20"`
+	PasswordHash string `json:"-" form:"-"`
 }
