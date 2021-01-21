9ffa5734e304ac7518e4721f81d4cc01bd8f0eb3
gbaranski <gbaranski19@gmail.com>
2021-01-21 14:55:41 +0100

    fix(postgres): waiting for postgres ready

--- pkg/database/postgres/postgres.go
+++ pkg/database/postgres/postgres.go
@@ -2,10 +2,12 @@ package postgres
 
 import (
 	"context"
+	"fmt"
+	"net"
+	"time"
 
 	log "github.com/sirupsen/logrus"
 
-	"github.com/jackc/pgconn"
 	"github.com/jackc/pgx/v4"
 	"github.com/jackc/pgx/v4/log/logrusadapter"
 	"github.com/jackc/pgx/v4/pgxpool"
@@ -13,11 +15,12 @@ import (
 
 // Options defines options for postgres, fields supports https://github.com/caarlos0/env tags
 type Options struct {
+	Username string `env:"POSTGRES_USER" envDefault:"postgres"`
 	// Password for Postgres
 	Password string `env:"POSTGRES_PASSWORD,required"`
 
 	// Name of the database,
-	DatabaseName string `env:"DATABASE_NAME" envDefault:"houseflow"`
+	DatabaseName string `env:"POSTGRES_DB" envDefault:"houseflow"`
 }
 
 // Postgres ...
@@ -25,24 +28,44 @@ type Postgres struct {
 	conn *pgxpool.Pool
 }
 
+func waitPostgresReady(timeout time.Duration) error {
+	ticker := time.NewTicker(time.Millisecond * 500)
+waitLoop:
+	for {
+		select {
+		case <-ticker.C:
+			conn, err := net.Dial("tcp", "postgres:5432")
+			if err != nil {
+				fmt.Println("received err", err.Error())
+				continue waitLoop
+			}
+			conn.Close()
+			return nil
+		case <-time.After(timeout):
+			return fmt.Errorf("timeout")
+		}
+	}
+}
+
 // New connect to pgxpool and returns Postgres stuct
 func New(ctx context.Context, opts Options) (Postgres, error) {
-	conn, err := pgxpool.ConnectConfig(ctx, &pgxpool.Config{
-		ConnConfig: &pgx.ConnConfig{
-			Config: pgconn.Config{
-				Host:     "postgres",
-				Port:     5432,
-				Database: opts.DatabaseName,
-				Password: opts.Password,
-			},
-			Logger:   logrusadapter.NewLogger(log.StandardLogger()),
-			LogLevel: pgx.LogLevelInfo,
-		},
-	})
+	waitPostgresReady(time.Second * 5)
+	config, err := pgxpool.ParseConfig("postgresql://postgres:5432")
+	if err != nil {
+		return Postgres{}, err
+	}
+	config.ConnConfig.Database = opts.DatabaseName
+	config.ConnConfig.Password = opts.Password
+	config.ConnConfig.User = opts.Username
+	config.ConnConfig.Logger = logrusadapter.NewLogger(log.StandardLogger())
+	config.ConnConfig.LogLevel = pgx.LogLevelInfo
+
+	conn, err := pgxpool.ConnectConfig(ctx, config)
 	if err != nil {
 		return Postgres{}, err
 	}
 
+	fmt.Println("Connected to PostgreSQL")
 	return Postgres{
 		conn: conn,
 	}, nil
