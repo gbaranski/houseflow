4051746a734cfcb60262391574def1e8a8b6e286
gbaranski <root@gbaranski.com>
2021-02-03 15:53:49 +0100

    feat: add libsodium

--- cmd/broker/README.md
+++ cmd/broker/README.md
@@ -1,4 +1,4 @@
 Compile using
 ```
-gcc -I/Users/gbaranski/mosquitto-2.0.6/include -fPIC -shared mosquitto_houseflow.c -undefined dynamic_lookup -o mosquitto_houseflow.so -lpq 
+gcc -I/Users/gbaranski/mosquitto-2.0.6/include -fPIC -shared mosquitto_houseflow.c -undefined dynamic_lookup -o mosquitto_houseflow.so -lpq -lsodium
 ```
--- cmd/broker/mosquitto_houseflow.c
+++ cmd/broker/mosquitto_houseflow.c
@@ -6,30 +6,31 @@
 #include <mosquitto_plugin.h>
 #include <mosquitto.h>
 #include <mqtt_protocol.h>
+#include <sodium.h>
+
+
+#define ED25519_PKEY_BASE64_BYTES 44
+#define ED25519_PKEY_BYTES 32
 
 static mosquitto_plugin_id_t *mosq_pid = NULL;
 static PGconn *pgconn = NULL;
 
-static int basic_auth_callback(int event, void *event_data, void *userdata)
+static int auth_cb(int event, void *event_data, void *userdata)
 {
   printf("basic_auth_callback()\n");
 	struct mosquitto_evt_basic_auth *ed = event_data;
-	const char *ip_address = mosquitto_client_address(ed->client);
+  if(strlen(ed->username) != ED25519_PKEY_BASE64_BYTES) {
+    printf("invalid username len: %lu\n", strlen(ed->username));
+    return MOSQ_ERR_AUTH;
+  }
 
-	if(!strcmp(ip_address, "127.0.0.1")){
-		/* Only allow connections from localhost */
-		return MOSQ_ERR_SUCCESS;
-	}else{
-		return MOSQ_ERR_AUTH;
-	}
+	return MOSQ_ERR_SUCCESS;
 }
 
 int mosquitto_plugin_version(int supported_version_count, const int *supported_versions)
 {
   printf("mosquitto_plugin_version()\n");
-	int i;
-
-	for(i=0; i<supported_version_count; i++){
+	for(int i = 0; i < supported_version_count; i++){
 		if(supported_versions[i] == 5){
 			return 5;
 		}
@@ -42,15 +43,20 @@ int mosquitto_plugin_init(mosquitto_plugin_id_t *identifier, void **user_data, s
   printf("mosquitto_plugin_init()\n");
 	mosq_pid = identifier;
 
+  if(sodium_init() == -1) {
+    return 1;
+  }
+  printf("Sodium initialized\n");
+
   pgconn = PQconnectdb("user=postgres dbname=gbaranski");
-  if (PQstatus(pgconn) == CONNECTION_BAD) {
+  if (PQstatus(pgconn) != CONNECTION_OK) {
     printf("Connection to database failed: %s\n", PQerrorMessage(pgconn));
     PQfinish(pgconn);
     return 1;
   }
-  printf("Successfully connected to PostgreSQL status: %x\n", PQstatus(pgconn));
+  printf("Connected to PostgreSQL\n");
 
-	return mosquitto_callback_register(mosq_pid, MOSQ_EVT_BASIC_AUTH, basic_auth_callback, NULL, NULL);
+	return mosquitto_callback_register(mosq_pid, MOSQ_EVT_BASIC_AUTH, auth_cb, NULL, NULL);
 }
 
 int mosquitto_plugin_cleanup(void *user_data, struct mosquitto_opt *opts, int opt_count)
@@ -58,5 +64,5 @@ int mosquitto_plugin_cleanup(void *user_data, struct mosquitto_opt *opts, int op
   printf("mosquitto_plugin_cleanup()\n");
   printf("Disconnecting from PostgreSQL\n");
   PQfinish(pgconn);
-	return mosquitto_callback_unregister(mosq_pid, MOSQ_EVT_BASIC_AUTH, basic_auth_callback, NULL);
+	return mosquitto_callback_unregister(mosq_pid, MOSQ_EVT_BASIC_AUTH, auth_cb, NULL);
 }
