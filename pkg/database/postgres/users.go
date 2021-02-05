d71c60f935be74e60498a48e265c5d82fc2b0026
gbaranski <root@gbaranski.com>
2021-02-05 22:02:02 +0100

    fix(postgres0: on delete actions, parsed sql

--- pkg/database/postgres/users.go
+++ pkg/database/postgres/users.go
@@ -26,8 +26,8 @@ CREATE TABLE IF NOT EXISTS users (
 // UserDevicesSchema is SQL schema for users_devices, it defines whether user has r/w/x permission
 const UserDevicesSchema = `
 CREATE TABLE IF NOT EXISTS user_devices (
-    user_id     UUID    REFERENCES users   (id),
-    device_id   UUID    REFERENCES devices (id),
+    user_id     UUID    REFERENCES users   (id) ON DELETE CASCADE,
+    device_id   UUID    REFERENCES devices (id) ON DELETE CASCADE,
     read        BOOL    NOT NULL, -- determines if user can do query intent
     write       BOOL    NOT NULL, -- unused at the moment, probably cna be used to tell if user can invite someone
     execute     BOOL    NOT NULL, -- determines if user can do execute intent
@@ -37,10 +37,10 @@ CREATE TABLE IF NOT EXISTS user_devices (
 `
 
 func (p Postgres) getUser(ctx context.Context, field string, value string) (*types.User, error) {
-	sql := fmt.Sprintf("SELECT first_name, last_name, email, password_hash FROM users WHERE %s=$1", field)
+	sql := fmt.Sprintf("SELECT id, first_name, last_name, email, password_hash FROM users WHERE %s=$1", field)
 	row := p.conn.QueryRow(ctx, sql, value)
 	user := types.User{}
-	err := row.Scan(&user.FirstName, &user.LastName, &user.Email, &user.PasswordHash)
+	err := row.Scan(&user.ID, &user.FirstName, &user.LastName, &user.Email, &user.PasswordHash)
 	if err != nil {
 		if err == pgx.ErrNoRows {
 			return nil, nil
@@ -62,12 +62,7 @@ func (p Postgres) GetUserByEmail(ctx context.Context, email string) (*types.User
 
 // GetUserDevices retreives user devices, only those which have READ=true
 func (p Postgres) GetUserDevices(ctx context.Context, userID string) ([]types.Device, error) {
-	const sql = `
-		SELECT device_id 
-		FROM user_devices
-		WHERE user_id=$1
-		AND read
-	`
+	const sql = "SELECT device_id FROM user_devices WHERE user_id=$1 AND read"
 	cur, err := p.conn.Query(ctx, sql, userID)
 	if err != nil {
 		return nil, fmt.Errorf("fail query %s", err.Error())
@@ -88,12 +83,7 @@ func (p Postgres) GetUserDevices(ctx context.Context, userID string) ([]types.De
 
 // GetUserDevicePermissions retreives permissions to specific device from specific user
 func (p Postgres) GetUserDevicePermissions(ctx context.Context, userID string, deviceID string) (perms types.DevicePermissions, err error) {
-	const sql = `
-	SELECT read, write, execute
-	FROM user_devices
-	WHERE user_id=$1
-	AND device_id=$2
-	`
+	const sql = "SELECT read, write, execute FROM user_devices WHERE user_id=$1 AND device_id=$2"
 	row := p.conn.QueryRow(ctx, sql, userID, deviceID)
 	err = row.Scan(&perms.Read, &perms.Write, &perms.Execute)
 	if err == pgx.ErrNoRows {
@@ -113,10 +103,7 @@ func (p Postgres) GetUserDevicePermissions(ctx context.Context, userID string, d
 
 // AddUser inserts new user to database
 func (p Postgres) AddUser(ctx context.Context, user types.User) (id string, err error) {
-	const sql = `
-	INSERT INTO users
-	VALUES ($1, $2, $3, $4, $5)
-	`
+	const sql = "INSERT INTO users VALUES ($1, $2, $3, $4, $5)"
 	userID, err := uuid.NewRandom()
 	if err != nil {
 		return "", fmt.Errorf("fail gen uuid %s", err.Error())
