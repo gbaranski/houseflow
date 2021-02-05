636ee20a719642399961ae1f08dd50f8c4ee29ea
gbaranski <root@gbaranski.com>
2021-02-05 22:01:41 +0100

    fix(postgres): fix deviceTraits, fetching device

--- pkg/database/postgres/devices.go
+++ pkg/database/postgres/devices.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"strings"
 
+	"github.com/gbaranski/houseflow/pkg/fulfillment"
 	"github.com/gbaranski/houseflow/pkg/types"
 	"github.com/google/uuid"
 )
@@ -16,6 +17,7 @@ CREATE TABLE IF NOT EXISTS devices (
     publickey           CHAR(44)    NOT NULL, -- Base64 encoded ed25519 public key, size = 4 * ceil(PKEY_BYTES / 3) = 44
     type                TEXT        NOT NULL, -- Type of the device, must be one of those https://developers.google.com/assistant/smarthome/guides
     will_report_state   BOOL        NOT NULL, -- True if device will report it state, false if use polling
+    name   				TEXT        NOT NULL, -- Name of the device
     room_hint           TEXT        NOT NULL, -- e.g Bedroom
     manufacturer        TEXT        NOT NULL, -- e.g gbaranski's garage
     model               TEXT        NOT NULL, -- e.g nightlamp
@@ -28,21 +30,38 @@ CREATE TABLE IF NOT EXISTS devices (
 
 // DeviceTraitsSchema is SQL schema for device_traits
 const DeviceTraitsSchema = `
-CREATE TABLE IF NOT EXISTS devices (
-    id          UUID PRIMARY KEY,
-    device_id   UUID REFERENCES devices (id),
+CREATE TABLE IF NOT EXISTS device_traits (
+    id          UUID,
+    device_id   UUID REFERENCES devices (id) ON DELETE CASCADE,
 	name        TEXT NOT NULL, -- Name of the trait, must be one of those https://developers.google.com/assistant/smarthome/traits
 
 	PRIMARY KEY (id)
 )
 `
 
+// GetDeviceTraits retrieves all traits for specific device
+func (p Postgres) GetDeviceTraits(ctx context.Context, id string) ([]string, error) {
+	const sql = "SELECT name FROM device_traits WHERE device_id=$1"
+	rows, err := p.conn.Query(ctx, sql, id)
+	if err != nil {
+		return nil, err
+	}
+	traits := make([]string, 0)
+
+	for rows.Next() {
+		var trait string
+
+		if err := rows.Scan(&trait); err != nil {
+			return nil, err
+		}
+		traits = append(traits, trait)
+	}
+	return traits, nil
+}
+
 // GetDeviceByID retreives device with given ID
 func (p Postgres) GetDeviceByID(ctx context.Context, id string) (device *types.Device, err error) {
-	const sql = `
-		SELECT publickey,type,will_report_state,room_hint,manufacturer,model,hw_version,sw_version 
-		FROM devices 
-		WHERE id=$1`
+	const sql = `SELECT publickey,type,will_report_state,room_hint,manufacturer,model,hw_version,sw_version FROM devices WHERE id=$1`
 	row := p.conn.QueryRow(ctx, sql, id)
 	err = row.Scan(
 		&device.PublicKey,
@@ -54,27 +73,48 @@ func (p Postgres) GetDeviceByID(ctx context.Context, id string) (device *types.D
 		&device.DeviceInfo.HwVersion,
 		&device.DeviceInfo.SwVersion,
 	)
+
+	device.Traits, err = p.GetDeviceTraits(ctx, id)
+	if err != nil {
+		return nil, fmt.Errorf("fail retrive traits %s", err.Error())
+	}
 	return device, err
 }
 
 // GetDevicesByIDs retreives device with given ID slice
 func (p Postgres) GetDevicesByIDs(ctx context.Context, ids []string) (devices []types.Device, err error) {
-	const sql = `
-		SELECT publickey,type,will_report_state,room_hint,manufacturer,model,hw_version,sw_version 
-		FROM devices 
-		WHERE id IN ($1)
-		`
+	const sql = `SELECT id,publickey,type,will_report_state,name,room_hint,manufacturer,model,hw_version,sw_version FROM devices WHERE id IN (%s)`
 
-	rows, err := p.conn.Query(ctx, sql, strings.Join(ids, ","))
+	if len(ids) < 1 {
+		return make([]types.Device, 0), nil
+	}
+
+	for i, id := range ids {
+		// verify ids to prevent SQL injection
+		if _, err := uuid.Parse(id); err != nil {
+			return nil, fmt.Errorf("invalid uuid %s", err.Error())
+		}
+		// add surrounding ''
+		ids[i] = fmt.Sprintf("'%s'", id)
+	}
+
+	rows, err := p.conn.Query(ctx, fmt.Sprintf(sql, strings.Join(ids, ",")))
 	if err != nil {
 		return nil, err
 	}
 	for rows.Next() {
-		var device types.Device
+		device := types.Device{
+			Device: fulfillment.Device{
+				// To prevent nil pointer deref when scanning
+				DeviceInfo: &fulfillment.DeviceInfo{},
+			},
+		}
 		err = rows.Scan(
+			&device.ID,
 			&device.PublicKey,
 			&device.Type,
 			&device.WillReportState,
+			&device.Name.Name,
 			&device.RoomHint,
 			&device.DeviceInfo.Manufacturer,
 			&device.DeviceInfo.Model,
@@ -83,6 +123,10 @@ func (p Postgres) GetDevicesByIDs(ctx context.Context, ids []string) (devices []
 		)
 		if err != nil {
 			return nil, err
+		}
+		device.Traits, err = p.GetDeviceTraits(ctx, device.ID)
+		if err != nil {
+			return nil, fmt.Errorf("fail retrive traits %s", err.Error())
 		}
 		devices = append(devices, device)
 	}
@@ -94,7 +138,7 @@ func (p Postgres) GetDevicesByIDs(ctx context.Context, ids []string) (devices []
 func (p Postgres) AddDevice(ctx context.Context, device types.Device) (id string, err error) {
 	const sql = `
 	INSERT INTO devices
-	VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)
+	VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)
 	`
 	deviceID, err := uuid.NewRandom()
 	if err != nil {
@@ -105,6 +149,7 @@ func (p Postgres) AddDevice(ctx context.Context, device types.Device) (id string
 		device.PublicKey,
 		device.Type,
 		device.WillReportState,
+		device.Name.Name,
 		device.RoomHint,
 		device.DeviceInfo.Manufacturer,
 		device.DeviceInfo.Model,
