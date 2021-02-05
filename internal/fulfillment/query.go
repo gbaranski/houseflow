401bb48ad0137ec0d5502b17b38a41610888b6f3
gbaranski <root@gbaranski.com>
2021-02-05 23:52:15 +0100

    feat(fulfillment): querying state and returning it

--- internal/fulfillment/query.go
+++ internal/fulfillment/query.go
@@ -7,24 +7,43 @@ import (
 	"github.com/gbaranski/houseflow/pkg/devmgmt"
 	"github.com/gbaranski/houseflow/pkg/fulfillment"
 	"github.com/gbaranski/houseflow/pkg/types"
+	"github.com/sirupsen/logrus"
 )
 
 func (f *Fulfillment) queryState(ctx context.Context, user types.User, deviceID string) (devices map[string]interface{}) {
+	device, err := f.db.GetDeviceByID(ctx, deviceID)
+	if err != nil {
+		logrus.Errorf("fail retrieve device %s", err.Error())
+		return map[string]interface{}{
+			"status":           fulfillment.StatusError,
+			"errorCode":        "hardError",
+			"errorDescription": err.Error(),
+		}
+	}
+	if device == nil {
+		return map[string]interface{}{
+			"status":    fulfillment.StatusError,
+			"errorCode": "deviceNotFound",
+		}
+	}
+
 	perm, err := f.db.GetUserDevicePermissions(ctx, user.ID, deviceID)
 	if err != nil {
+		logrus.Errorf("fail retrieve device permission%s", err.Error())
 		return map[string]interface{}{
 			"status":    fulfillment.StatusError,
 			"errorCode": "hardError",
 		}
 	}
 	if !perm.Read {
+		logrus.Errorf("user doesn't have read permission to device")
 		return map[string]interface{}{
-			"status":      fulfillment.StatusError,
-			"errorCode":   "authFailure",
-			"debugString": "missing execute permission",
+			"status":    fulfillment.StatusError,
+			"errorCode": "authFailure",
 		}
 	}
-	res, err := f.devmgmt.FetchDeviceState(ctx, deviceID)
+
+	res, err := f.devmgmt.FetchDeviceState(ctx, *device)
 	if err != nil {
 		if err == devmgmt.ErrDeviceTimeout {
 			return map[string]interface{}{
