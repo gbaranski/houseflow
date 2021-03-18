507752eb3665423ca7c4c0b28803059991974e44
gbaranski <root@gbaranski.com>
2021-03-18 12:59:37 +0100

    init new project with Cargo
    add database communication crate

---
+++ db/src/lib.rs
@@ -0,0 +1,43 @@
+pub mod device;
+
+pub enum Error {
+    Error(String),
+    PgError(tokio_postgres::Error)
+}
+
+impl From<tokio_postgres::Error> for Error {
+    fn from(err: tokio_postgres::Error) -> Error {
+        err.into()
+    }
+}
+
+pub struct DatabaseOptions {
+    pub user: String,
+    pub password: String,
+    pub host: String,
+    pub db_name: String,
+}
+
+pub struct Database {
+    client: tokio_postgres::Client,
+    options: DatabaseOptions
+}
+
+impl Database {
+    pub async fn connect(options: DatabaseOptions) -> Result<Database, Error> {
+        let cstr = format!("user={} password={} host={} db={}", 
+                           options.user, 
+                           options.password, 
+                           options.host, 
+                           options.db_name
+                           );
+
+        let (client, conn) = 
+            tokio_postgres::connect(cstr.as_ref(), tokio_postgres::NoTls).await?;
+            
+        Ok(Database{
+            client,
+            options,
+        })
+    }
+}
