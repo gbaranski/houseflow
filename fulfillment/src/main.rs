c17267346249874f2e02890f324ac8fe5dae0a0d
gbaranski <root@gbaranski.com>
2021-03-28 18:01:25 +0200

    feat(fulfillment): add working with lighthouse

--- fulfillment/src/main.rs
+++ fulfillment/src/main.rs
@@ -1,32 +1,36 @@
-use houseflow_db::{Database, models::User};
-use actix_web::{web, post, App, HttpResponse, HttpRequest, HttpServer, Responder};
-use std::convert::TryInto;
-use houseflow_token::{Token, SizedFrame};
-use crate::error::Error;
+use houseflow_db::Database;
+use houseflow_lighthouse::LighthouseAPI;
+use houseflow_token::Token;
+use actix_web::{web, post, App, HttpResponse, HttpRequest, HttpServer};
+use error::{Error, AuthError};
 
 mod error;
 mod intent {
     mod intent;
+    mod error;
     pub mod sync;
     pub mod execute;
     pub mod query;
 
     pub use intent::Request;
+    pub use error::IntentError;
 }
 
 
 /// This struct represents shared state across routes
 #[derive(Clone)]
-struct AppState {
+pub struct AppState<'a> {
     db: Database,
+    memcache: memcache::Client,
+    lighthouse: LighthouseAPI<'a>,
 }
 
 
 #[post("/webhook")]
-async fn webhook(
+async fn webhook<'a>(
     req: HttpRequest,
     intent_request: web::Json<intent::Request>,
-    state: web::Data<AppState>
+    state: web::Data<AppState<'a>>
 ) -> Result<&'static str, Error> {
     let access_token_base64 = match req.headers().get("Authorization") {
         Some(value) => {
@@ -35,26 +39,22 @@ async fn webhook(
             // Token starts at byte 7
             Ok(&value.as_bytes()[7..])
         },
-        None => Err(Error::MissingToken),
+        None => Err(AuthError::MissingToken),
     }?;
-    let access_token_bytes: [u8; Token::SIZE] = base64::decode(access_token_base64)
-        .map_err(|err| Error::InvalidToken(format!("fail decode base64: `{}`", err)))?
-        .try_into()
-        .map_err(|_err| Error::InvalidToken("failed conversion into fixed size array".to_string()))?;
 
-    let access_token = Token::from_bytes(access_token_bytes);
+    let access_token = Token::from_base64(access_token_base64)?;
     access_token.verify(std::env::var("ACCESS_KEY").unwrap().as_bytes())?;
 
-    let user = state.db.user_by_id(access_token.payload.audience)
+    let user = state.db.get_user_by_id(access_token.payload.audience)
         .await?
-        .ok_or(Error::UserNotFound)?;
+        .ok_or(AuthError::UserNotFound)?;
 
 
-    intent_request.inputs
-        .iter()
-        .map(|input| match input.intent {
-            // "action.devices.SYNC" => intent::sync::handle()
-        });
+    // intent_request.inputs
+    //     .iter()
+    //     .map(|input| match input.intent {
+    //         // "action.devices.SYNC" => intent::sync::handle()
+    //     });
     log::info!("Request ID: {}", intent_request.request_id);
     HttpResponse::InternalServerError();
     Ok("sadhahs")
@@ -64,14 +64,21 @@ async fn webhook(
 
 #[actix_web::main]
 async fn main() -> Result<(), Error> {
-    log::info!("Starting houseflow-fulfillment");
     env_logger::init();
+    log::info!("Starting houseflow-fulfillment");
 
     let db = Database::connect()?;
     db.init().await?;
+
+    let memcache = memcache::connect("memcache://memcache:11211?timeout=10&tcp_nodelay=true")?;
     
     let app_state = AppState {
         db,
+        memcache,
+        lighthouse: LighthouseAPI {
+            memcache: &memcache,
+            db: &db,
+        },
     };
 
     log::info!("Starting HttpServer");
@@ -83,7 +90,8 @@ async fn main() -> Result<(), Error> {
     })
     .bind("0.0.0.0:80")?
     .run()
-    .await?;
+    .await
+    .unwrap();
 
     Ok(())
 }
