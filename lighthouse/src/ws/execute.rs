b0233f67a9bf314cb059aacebde76d5275350253
gbaranski <root@gbaranski.com>
2021-03-31 10:21:09 +0200

    feat(lighthouse): working Http -> WS response

---
+++ lighthouse/src/ws/execute.rs
@@ -0,0 +1,63 @@
+use std::time::Duration;
+use actix::prelude::*;
+use tokio::sync::oneshot;
+use uuid::Uuid;
+use actix::dev::*;
+use super::session::WebsocketSession;
+
+const EXECUTE_TIMEOUT: Duration = Duration::from_secs(5);
+
+#[derive(Debug)]
+pub enum ExecuteError {
+    Timeout,
+}
+
+
+#[derive(Debug, Clone)]
+pub struct Response(pub String);
+use std::future::Future;
+use std::pin::Pin;
+use std::boxed::Box;
+use std::task::Poll;
+
+
+#[derive(MessageResponse)]
+pub struct ResponseFuture(pub Pin<Box<dyn Future<Output = Response> + Send>>);
+
+// impl Future for ResponseFuture {
+//     type Output = Response;
+
+//     fn poll(&mut self, wake: fn()) -> Poll<Self::Output> {
+//         self.0.
+//     }
+// }
+
+#[derive(Message, Debug)]
+#[rtype(result = "ResponseFuture")]
+pub struct Request(pub String);
+
+
+impl Handler<Request> for WebsocketSession {
+
+    type Result = ResponseFuture;
+
+    fn handle(
+        &mut self, 
+        _req: Request, 
+        ctx: &mut Self::Context
+    ) -> Self::Result {
+        ctx.text("Send me response");
+        let (tx, rx) = oneshot::channel();
+
+        let request_id = Uuid::new_v4();
+        self.response_channels.push((request_id, Some(tx)));
+
+        let boxed = Box::pin(async move {
+            let resp = rx.await.unwrap();
+            println!("Received response: {:?}", resp);
+            resp
+        });
+
+        ResponseFuture(Box::pin(boxed))
+    }
+}
