0e61dbc5a07b3ba7169cd4e74f5d8f0dd9a2702c
gbaranski <root@gbaranski.com>
2021-04-07 23:14:08 +0200

    feat(lighthouse): add Ping packet

--- lighthouse/packets/ping.go
+++ lighthouse/packets/ping.go
@@ -1,40 +1,31 @@
 package packets
 
 import (
-	"fmt"
 	"io"
 
 	"github.com/gbaranski/houseflow/lighthouse/utils"
 )
 
-// PingPayload is payload for PING Control packet
+// PingPayload is payload for PING packet
 type PingPayload struct {
 	ID uint16
 }
 
-// ReadPingPayload reads ping payload from io.Reader
-func ReadPingPayload(r io.Reader) (PingPayload, error) {
-	length, err := utils.Read16BitInteger(r)
-	if err != nil {
-		return PingPayload{}, fmt.Errorf("fail read payload len")
-	}
-	if length != 2 {
-		return PingPayload{}, fmt.Errorf("invlaid length: %d", length)
-	}
+// ReadPingPayload reads pong payload from io.Reader
+func ReadPingPayload(r io.Reader) (p PingPayload, err error) {
 	id, err := utils.Read16BitInteger(r)
 	if err != nil {
-		return PingPayload{}, err
+    return p, err
 	}
+
 	return PingPayload{
 		ID: id,
 	}, nil
 }
 
-// Bytes convert PingPayload to bytes
-func (p PingPayload) Bytes() (b []byte) {
-	b = make([]byte, 2)
-	b[0] = byte(p.ID >> 8)
-	b[1] = byte(p.ID)
+// WriteTo writes PingPayload to io.Writer
+func (p PingPayload) WriteTo(w io.Writer) (n int64, err error) {
+  k, err := utils.Write16BitInteger(w, p.ID)
 
-	return b
+  return int64(k), err
 }
