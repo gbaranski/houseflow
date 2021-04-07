cc391f2045cbaa1042521d36f963008160feeae1
gbaranski <root@gbaranski.com>
2021-04-07 23:10:51 +0200

    test(lighthouse): add tests for store

--- lighthouse/server/store_test.go
+++ lighthouse/server/store_test.go
@@ -1,87 +1,55 @@
 package server
 
-import "testing"
+import (
+  "testing"
+	"github.com/google/uuid"
+)
 
-func TestClientStore(t *testing.T) {
-	cs := NewClientStore()
-	c1 := Client{
-		ID:        "SomeClientID",
-		IPAddress: nil,
-	}
-	c2 := Client{
-		ID:        "SomeDifferentClientID",
-		IPAddress: nil,
-	}
+func TestStore(t *testing.T) {
+  store := NewSessionStore()
+  session := Session {
+    ClientID: uuid.Must(uuid.NewRandom()),
+    conn: nil,
+  }
 
-	if err := cs.Add(c1); err != nil {
-		t.Fatalf("fail add c1 %s", err.Error())
-	}
-	if err := cs.Add(c2); err != nil {
-		t.Fatalf("fail add c2 %s", err.Error())
-	}
-
-	c, ok := cs.Get(c1.ID)
-	if !ok {
-		t.Fatalf("fail retreive c1")
-	}
-	if c.ID != c1.ID {
-		t.Fatalf("invalid c1 clientID")
-	}
-
-	c, ok = cs.Get(c2.ID)
-	if !ok {
-		t.Fatalf("fail retreive c2")
-	}
-	if c.ID != c2.ID {
-		t.Fatalf("invalid c2 clientID")
-	}
+  if err := store.Add(&session); err != nil {
+    t.Fatalf("fail adding to store: %s", err.Error())
+  }
+  if !store.Exists(session.ClientID) {
+    t.Fatalf("session does not exists in store after adding")
+  }
+  retrievedSession := store.Get(session.ClientID)
+  if retrievedSession == nil {
+    t.Fatalf("retrieving session failed")
+  }
+  if retrievedSession != &session {
+    t.Fatalf("retrieved session pointer mismatch, expect: %s, got: %s", &session, retrievedSession)
+  }
+  store.Delete(session.ClientID)
+  if store.Exists(session.ClientID) {
+    t.Fatalf("session exists in store after deleting")
+  }
 }
 
-func TestClientStoreNotPresentUser(t *testing.T) {
-	cs := NewClientStore()
-	c1 := Client{
-		ID:        "SomeClientID",
-		IPAddress: nil,
-	}
 
-	c2 := Client{
-		ID:        "SomeDifferentClientID",
-		IPAddress: nil,
-	}
+func BenchmarkAddGetStore(b *testing.B) {
+  store := NewSessionStore()
+  session := Session {
+    ClientID: uuid.Must(uuid.NewRandom()),
+    conn: nil,
+  }
+  for i := 0; i < 1000; i++ {
+    session := Session {
+      ClientID: uuid.Must(uuid.NewRandom()),
+      conn: nil,
+    }
+    store.Add(&session)
+  }
+  b.ResetTimer()
 
-	if err := cs.Add(c1); err != nil {
-		t.Fatalf("fail add c1 %s", err.Error())
-	}
-
-	c, ok := cs.Get(c1.ID)
-	if !ok {
-		t.Fatalf("fail retreive c1")
-	}
-	if c.ID != c1.ID {
-		t.Fatalf("invalid c1 clientID")
-	}
-
-	c, ok = cs.Get(c2.ID)
-	if ok {
-		t.Fatalf("expected to fail on c2")
-	}
-	if c.ID == c2.ID {
-		t.Fatalf("invalid c2 clientID")
-	}
-}
-
-func TestClientStoreDuplicate(t *testing.T) {
-	cs := NewClientStore()
-	c1 := Client{
-		ID:        "SomeClientID",
-		IPAddress: nil,
-	}
-
-	if err := cs.Add(c1); err != nil {
-		t.Fatalf("fail add c1 %s", err.Error())
-	}
-
-	if err := cs.Add(c1); err == nil {
-		t.Fatalf("expected error, received nil while adding dup")
-	}
+  for i := 0; i < b.N; i++ {
+    store.Add(&session)
+    store.Get(session.ClientID)
+    store.Delete(session.ClientID)
+  }
 }
