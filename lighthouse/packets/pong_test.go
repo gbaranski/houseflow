8b3406bc1f3e30c4e233846b9c76db9a256f3da0
gbaranski <root@gbaranski.com>
2021-04-07 23:14:48 +0200

    test(lighthouse): add tests for Pong packet

--- lighthouse/packets/pong_test.go
+++ lighthouse/packets/pong_test.go
@@ -1,26 +1,28 @@
 package packets
 
 import (
-	"bytes"
-	"math"
-	"math/rand"
-	"testing"
+  "testing"
+  "bytes"
+  "math"
+  "math/rand"
 )
 
 func TestPongPayload(t *testing.T) {
-	payload := PongPayload{
-		ID: uint16(rand.Intn(math.MaxUint16)),
-	}
-	b := make([]byte, 0)
-	b = append(b, 0) // MSB
-	b = append(b, 2) // LSB
-	b = append(b, payload.Bytes()...)
+  payload := PongPayload{
+    ID: uint16(rand.Intn(math.MaxUint16)),
+  }
+  buf := bytes.NewBuffer([]byte{})
+  _, err := payload.WriteTo(buf)
+  if err != nil {
+    t.Fatalf(err.Error())
+  }
 
-	readenPayload, err := ReadPongPayload(bytes.NewReader(b))
-	if err != nil {
-		t.Fatalf(err.Error())
-	}
-	if readenPayload.ID != payload.ID {
-		t.Fatalf("Unexpected ID: %d, expected: %d", readenPayload.ID, payload.ID)
-	}
+  newPayload, err := ReadPongPayload(buf)
+  if err != nil {
+    t.Fatalf("fail reading execute payload: %s", err.Error())
+  }
+
+  if payload.ID != newPayload.ID {
+    t.Fatalf("`payload.ID` does not match! Expected: `%d`, found: `%d`", payload.ID, newPayload.ID)
+  }
 }
