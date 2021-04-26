70f4c35770abc3023e1e2b32422c5fd0a4968e5d
gbaranski <root@gbaranski.com>
2021-04-26 19:29:58 +0200

    feat: add new frames, Execute & ExecuteResponse
    test: add tests for Execute & ExecuteResponse

--- lighthouse/proto/src/codec.rs
+++ lighthouse/proto/src/codec.rs
@@ -1,13 +1,22 @@
 use crate::{frame::Frame, Opcode};
+use crate::{ExecuteCommand, ExecuteResponseCode, ExecuteResponseError};
 use bytes::{Buf, BufMut, BytesMut};
-use std::convert::TryInto;
+use serde::{Deserialize, Serialize};
+use std::{
+    collections::HashMap,
+    convert::{TryFrom, TryInto},
+};
 use strum::IntoEnumIterator;
 use tokio_util::codec::{Decoder, Encoder};
 
+/// Max size of JSON-encoded field in frame
+const MAX_JSON_LEN: usize = 1024;
+
 #[derive(Debug)]
 pub enum Error {
-    InvalidOpcode(u8),
-    InvalidResponseCode(u8),
+    InvalidField(&'static str, Box<dyn std::fmt::Debug>),
+    FieldTooBig(usize),
+    FieldNotNullTerminated,
     IOError(std::io::Error),
 }
 
@@ -15,8 +24,18 @@ impl std::fmt::Display for Error {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> Result<(), std::fmt::Error> {
         use Error::*;
         let msg = match self {
-            InvalidOpcode(opcode) => format!("Invalid opcode: `{}`", opcode),
-            InvalidResponseCode(response_code) => format!("Invalid response code: `{}`", response_code),
+            InvalidField(field, received) => {
+                format!("Invalid `{}`: `{:?}`", field, received)
+            }
+            FieldTooBig(size) => {
+                format!(
+                    "Received too big field, aborting decoding it, size: `{}`",
+                    size
+                )
+            }
+            FieldNotNullTerminated => {
+                format!("Field was not NULL terminated")
+            }
             IOError(err) => format!("IOError: {}", err),
         };
         write!(f, "{}", msg)
@@ -52,11 +71,13 @@ impl Decoder for FrameCodec {
 
         let opcode = src.get_u8();
 
-        let opcode = Opcode::iter()
-            .find(|v| *v as u8 == opcode)
-            .ok_or(Error::InvalidOpcode(opcode))?;
+        let opcode =
+            Opcode::try_from(opcode).or(Err(Error::InvalidField("opcode", Box::new(opcode))))?;
 
         let frame = match opcode {
+            Opcode::NoOperation => {
+                return Err(Error::InvalidField("opcode", Box::new(opcode)));
+            }
             Opcode::Connect => {
                 let mut client_id = [0; 16];
                 src.copy_to_slice(&mut client_id[..]);
@@ -69,7 +90,58 @@ impl Decoder for FrameCodec {
                 let response_code = src.get_u8();
 
                 Frame::ConnACK {
-                response_code: response_code.try_into().map_err(|_| Error::InvalidOpcode(response_code))?,
+                    response_code: response_code.try_into().map_err(|_| {
+                        Error::InvalidField("ConnACK.ResponseCode", Box::new(response_code))
+                    })?,
+                }
+            }
+            Opcode::Execute => {
+                let id = src.get_u32();
+                let command = src.get_u16();
+                let command = ExecuteCommand::try_from(command)
+                    .map_err(|_| Error::InvalidField("Execute.Command", Box::new(command)))?;
+                let params_len = src
+                    .iter()
+                    .position(|v| *v == b'\0')
+                    .ok_or(Error::FieldNotNullTerminated)?;
+                if params_len > MAX_JSON_LEN {
+                    return Err(Error::FieldTooBig(params_len));
+                }
+                let params_bytes = src.copy_to_bytes(params_len);
+                let params = serde_json::from_slice(&params_bytes)
+                    .map_err(|err| Error::InvalidField("Execute.Params", Box::new(err)))?;
+
+                Frame::Execute {
+                    id,
+                    command,
+                    params,
+                }
+            }
+            Opcode::ExecuteResponse => {
+                let id = src.get_u32();
+                let response_code = src.get_u8();
+                let response_code = ExecuteResponseCode::try_from(response_code).map_err(|_| {
+                    Error::InvalidField("ExecuteResponse.ResponseCode", Box::new(response_code))
+                })?;
+
+                let error = src.get_u16();
+                let error = ExecuteResponseError::try_from(error)
+                    .map_err(|_| Error::InvalidField("ExecuteResponse.Error", Box::new(error)))?;
+                let state_len = src
+                    .iter()
+                    .position(|v| *v == b'\0')
+                    .ok_or(Error::FieldNotNullTerminated)?;
+                if state_len > MAX_JSON_LEN {
+                    return Err(Error::FieldTooBig(state_len));
+                }
+                let state_bytes = src.copy_to_bytes(state_len);
+                let state = serde_json::from_slice(&state_bytes).unwrap();
+
+                Frame::ExecuteResponse {
+                    id,
+                    response_code,
+                    state,
+                    error,
                 }
             }
         };
@@ -83,6 +155,10 @@ impl Encoder<Frame> for FrameCodec {
 
     fn encode(&mut self, item: Frame, dst: &mut BytesMut) -> Result<(), Self::Error> {
         match item {
+            Frame::NoOperation => {
+                let opcode = Frame::NoOperation.opcode();
+                return Err(Error::InvalidField("opcode", Box::new(opcode)));
+            }
             Frame::Connect { client_id } => {
                 let client_id: [u8; 16] = client_id.into();
                 dst.put_u8(Opcode::Connect as u8);
@@ -91,6 +167,32 @@ impl Encoder<Frame> for FrameCodec {
             Frame::ConnACK { response_code } => {
                 dst.put_u8(Opcode::ConnACK as u8);
                 dst.put_u8(response_code as u8);
+            }
+            Frame::Execute {
+                id,
+                command,
+                params,
+            } => {
+                dst.put_u8(Opcode::Execute as u8);
+                dst.put_u32(id);
+                dst.put_u16(command as u16);
+                let params = serde_json::to_vec(&params).unwrap();
+                dst.put_slice(&params);
+                dst.put_u8(b'\0');
+            }
+            Frame::ExecuteResponse {
+                id,
+                response_code,
+                error,
+                state,
+            } => {
+                dst.put_u8(Opcode::ExecuteResponse as u8);
+                dst.put_u32(id);
+                dst.put_u8(response_code as u8);
+                dst.put_u16(error as u16);
+                let state = serde_json::to_vec(&state).unwrap();
+                dst.put_slice(&state);
+                dst.put_u8(b'\0');
             }
         }
 
@@ -106,7 +208,7 @@ mod tests {
     fn test_frame_codec(frame: Frame) {
         let mut codec = FrameCodec {};
         let mut bytes = BytesMut::new();
-        codec.encode(frame, &mut bytes).unwrap();
+        codec.encode(frame.clone(), &mut bytes).unwrap();
 
         let decoded_frame = codec.decode(&mut bytes).unwrap().unwrap();
 
@@ -125,6 +227,41 @@ mod tests {
     fn test_connack_codec() {
         let frame = Frame::ConnACK {
             response_code: random(),
+        };
+        test_frame_codec(frame)
+    }
+
+    #[test]
+    fn test_execute_codec() {
+        let params = r#"
+            {
+                "on": true,
+                "online": true,
+                "openPercent": 20
+            }
+            "#;
+        let frame = Frame::Execute {
+            id: random(),
+            command: random(),
+            params: serde_json::from_str(params).unwrap(),
+        };
+        test_frame_codec(frame)
+    }
+
+    #[test]
+    fn test_execute_response_codec() {
+        let state = r#"
+            {
+                "on": true,
+                "online": true,
+                "openPercent": 20
+            }
+            "#;
+        let frame = Frame::ExecuteResponse {
+            id: random(),
+            response_code: random(),
+            error: random(),
+            state: serde_json::from_str(state).unwrap(),
         };
         test_frame_codec(frame)
     }
