756595ef29b54705a6b019f31cd7d52fd252ef3f
gbaranski <root@gbaranski.com>
2021-05-06 00:58:44 +0200

    feat: add API crate for lighthouse

---
+++ lighthouse/api/src/lib.rs
@@ -0,0 +1,49 @@
+use lighthouse_proto::{Frame, frame};
+use thiserror::Error;
+use actix::prelude::Message;
+
+#[derive(Debug, Clone)]
+pub enum Request {
+    Execute(frame::execute::Frame),
+}
+
+
+#[derive(Debug, Error)]
+pub enum RequestError {
+    #[error("Client is not connected")]
+    ClientNotConnected,
+
+    #[error("Timeout when sending request")]
+    Timeout,
+}
+
+#[derive(Debug, Clone)]
+pub enum Response {
+    Execute(frame::execute_response::Frame)
+}
+
+impl Message for Request {
+    type Result = ();
+}
+
+impl Message for Response {
+    type Result = ();
+}
+
+impl Into<Frame> for Request {
+    fn into(self) -> Frame {
+        match self {
+            Self::Execute(frame) => Frame::Execute(frame),
+        }
+    }
+}
+
+impl Into<Frame> for Response {
+    fn into(self) -> Frame {
+        match self {
+            Self::Execute(frame) => Frame::ExecuteResponse(frame),
+        }
+    }
+}
+
+
