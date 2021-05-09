daae792dcd06e775ad4db29ea94bdc1db966491d
gbaranski <root@gbaranski.com>
2021-05-09 18:18:10 +0200

    fix: move state_check.rs to state_check

---
+++ lighthouse/proto/src/frame/state_check/frame.rs
@@ -0,0 +1,28 @@
+use crate::{DecodeError, Decoder, Encoder, Framed, Frame};
+use bytes::{Buf, BufMut};
+use lighthouse_macros::decoder;
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Clone, Eq, PartialEq, Serialize, Deserialize)]
+pub struct StateCheckFrame {}
+
+impl Decoder for StateCheckFrame {
+    const MIN_SIZE: usize = 0;
+
+    #[decoder]
+    fn decode(buf: &mut impl Buf) -> Result<Self, DecodeError> {
+        Ok(Self {})
+    }
+}
+
+impl Encoder for StateCheckFrame {
+    fn encode(&self, _buf: &mut impl BufMut) {}
+}
+
+impl<'de> Framed<'de> for StateCheckFrame {}
+
+impl Into<Frame> for StateCheckFrame {
+    fn into(self) -> Frame {
+        Frame::StateCheck(self)
+    }
+}
