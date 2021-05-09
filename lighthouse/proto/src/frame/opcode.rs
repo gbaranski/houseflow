dbb736d31ba3c8a7b28c5148213f682ecf379b79
gbaranski <root@gbaranski.com>
2021-05-09 18:18:24 +0200

    feat: add opcodes

---
+++ lighthouse/proto/src/frame/opcode.rs
@@ -0,0 +1,27 @@
+use std::convert::TryFrom;
+use strum::IntoEnumIterator;
+use strum_macros::EnumIter;
+
+#[derive(Debug, Clone, Copy, EnumIter)]
+#[repr(u8)]
+pub(crate) enum Opcode {
+    NoOperation,
+    State,
+    StateCheck,
+    Command,
+    CommandResponse,
+}
+
+impl Into<u8> for Opcode {
+    fn into(self) -> u8 {
+        self as u8
+    }
+}
+
+impl TryFrom<u8> for Opcode {
+    type Error = ();
+
+    fn try_from(v: u8) -> Result<Self, Self::Error> {
+        Opcode::iter().find(|e| *e as u8 == v).ok_or(())
+    }
+}
