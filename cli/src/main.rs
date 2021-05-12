e5e0e8e3da0e74c5929f9324848d4c5a6d645323
gbaranski <root@gbaranski.com>
2021-05-12 20:39:15 +0200

    feat: generating random device ID's

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+type DeviceID = String;
+
+pub struct Device {
+    pub id: DeviceID,
+}
+fn generate_random_device_id() -> DeviceID {
+    let bytes: [u8; 16] = rand::random();
+    hex::encode(bytes)
+}
+/// Creates a infinite Iterator of fake devices
+fn get_devices() -> impl Iterator<Item = Device> {
+    std::iter::repeat_with(|| Device {
+        id: generate_random_device_id(),
+    })
 }
