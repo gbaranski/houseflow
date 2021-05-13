17297c1fbc7850ca1d942c57ae60e95edf69728b
gbaranski <root@gbaranski.com>
2021-05-13 09:47:02 +0200

    feat: scroll_view with dialogs

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,12 +1,18 @@
-type DeviceID = String;
+/// Used to identify the device
+struct DeviceID {
+    inner: [u8; 16],
+}
 
-pub struct Device {
+struct Device {
     pub id: DeviceID,
 }
 
-fn generate_random_device_id() -> DeviceID {
-    let bytes: [u8; 16] = rand::random();
-    hex::encode(bytes)
+impl std::fmt::Display for DeviceID {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", hex::encode(self.inner))
+    }
+}
+
 use rand::distributions;
 
 impl distributions::Distribution<DeviceID> for distributions::Standard {
@@ -33,7 +39,7 @@ fn get_devices_select_view(
     let devices_cursive_iter = devices
         .iter()
         .enumerate()
-        .map(|(index, device)| (&device.id, index));
+        .map(|(index, device)| (device.id.to_string(), index));
 
     let mut view = SelectView::new();
     view.add_all(devices_cursive_iter);
@@ -45,12 +51,18 @@ fn get_devices_select_view(
 }
 
 use cursive::views::{Dialog, TextView};
+
 fn device_select_callback(siv: &mut Cursive, device: &Device) {
-    let text_view = TextView::new("Select action");
-    let dialog_title = format!("Selected: {}", device.id);
+    let text_view = TextView::new("Select what to do with the device");
+    let dialog_title = format!("Selected device: {}", device.id);
     let dialog = Dialog::around(text_view)
         .title(dialog_title)
-        .button("Cancel", |s| s.quit());
+        .button("Send command", |s| {
+            s.pop_layer().unwrap();
+        })
+        .button("Cancel", |s| {
+            s.pop_layer().unwrap();
+        });
 
     siv.add_layer(dialog);
 }
@@ -59,8 +71,8 @@ fn device_select_callback(siv: &mut Cursive, device: &Device) {
 async fn main() -> Result<(), anyhow::Error> {
     let mut siv = cursive::default();
     let devices = get_devices().take(3).collect();
-    let devices_selectview = get_devices_select_view(devices, device_select_callback);
-    siv.add_layer(devices_selectview);
+    let devices_select_view = get_devices_select_view(devices, device_select_callback);
+    siv.add_layer(devices_select_view);
     siv.run();
 
     Ok(())
