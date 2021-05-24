a9835bd38a68fd8a6aca812bcd011fb5b72a521b
gbaranski <root@gbaranski.com>
2021-05-24 22:22:39 +0200

    feat(auth/types): handling properly Errors in response

--- auth/types/src/lib.rs
+++ auth/types/src/lib.rs
@@ -1,5 +1,5 @@
-use std::time::Duration;
 use houseflow_token::Token;
+use std::time::Duration;
 
 #[cfg(feature = "serde")]
 use serde::{Deserialize, Serialize};
@@ -13,14 +13,13 @@ pub enum GrantType {
 
 #[derive(Debug, Clone)]
 #[cfg_attr(feature = "serde", derive(Deserialize, Serialize))]
-pub struct AccessTokenRequest {
+pub struct AccessTokenRequestBody {
     /// The grant_type parameter must be set to `GrantType::RefreshToken`.
     pub grant_type: GrantType,
 
     /// The refresh token previously issued to the client.
     pub refresh_token: Token,
 }
-
 
 #[derive(Debug, Clone)]
 #[cfg_attr(feature = "serde", derive(Deserialize, Serialize))]
@@ -30,13 +29,63 @@ pub enum TokenType {
 
 #[derive(Debug, Clone)]
 #[cfg_attr(feature = "serde", derive(Deserialize, Serialize))]
-pub struct AccessTokenResponse {
+#[cfg_attr(feature = "serde", serde(untagged))]
+pub enum AccessTokenResponse {
+    Ok(AccessTokenRequestBody),
+    Err(AccessTokenRequestError),
+}
+
+#[derive(Debug, Clone)]
+#[cfg_attr(feature = "serde", derive(Deserialize, Serialize))]
+pub struct AccessTokenResponseBody {
     /// The access token string as issued by the authorization server.
     pub access_token: Token,
 
     /// The type of token this is, typically just the string “Bearer”.
     pub token_type: TokenType,
-    
+
     /// If the access token expires, the server should reply with the duration of time the access token is granted for.
     pub expires_in: Option<Duration>,
+}
+
+#[derive(Debug, Clone, thiserror::Error)]
+#[cfg_attr(feature = "serde", derive(Deserialize, Serialize))]
+#[error("kind: `{error}`, description: `{}`")]
+pub struct AccessTokenRequestError {
+    error: AccessTokenRequestErrorKind,
+    error_description: String,
+}
+
+#[derive(Debug, Clone, thiserror::Error)]
+#[cfg_attr(feature = "serde", derive(Deserialize, Serialize))]
+#[cfg_attr(feature = "serde", serde(rename = "snake_case"))]
+pub enum AccessTokenRequestErrorKind {
+    /// The request is missing a parameter so the server can’t proceed with the request.
+    /// This may also be returned if the request includes an unsupported parameter or repeats a parameter.
+    #[error("Request is missing a parameter")]
+    InvalidRequest,
+
+    /// Client authentication failed, such as if the request contains an invalid client ID or secret.
+    /// Send an HTTP 401 response in this case.
+    #[error("Invalid Client ID or Secret")]
+    InvalidClient,
+
+    /// The authorization code (or user’s password for the password grant type) is invalid or expired.
+    /// This is also the error you would return if the redirect URL given in the authorization grant does not match the URL provided in this access token request.
+    #[error("Authorization Code(or user's password) is invalid, or redirect URL does not match")]
+    InvalidGrant,
+
+    /// For access token requests that include a scope (password or client_credentials grants), this error indicates an invalid scope value in the request.
+    #[error("Scope of requested access token is invalid")]
+    InvalidScope,
+
+    /// This client is not authorized to use the requested grant type.
+    /// For example, if you restrict which applications can use the Implicit grant, you would return this error for the other apps.
+    #[error("Client is not authorized to use the requested grant type")]
+    UnauthorizedClient,
+
+    /// If a grant type is requested that the authorization server doesn’t recognize, use this code.
+    /// Note that unknown grant types also use this specific error code rather than using the invalid_request above.
+    #[error("Grant type is unsupported")]
+    UnsupportedGrantType,
 }
