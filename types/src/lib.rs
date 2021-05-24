016685a5ed3a80fb1fb02929671f615b4a802016
gbaranski <root@gbaranski.com>
2021-05-24 20:12:16 +0200

    fix: reexport CredentialError from types

--- types/src/lib.rs
+++ types/src/lib.rs
@@ -3,6 +3,7 @@ mod device;
 mod user;
 
 
+pub use common::{Credential, CredentialError};
 pub use device::{Device, DeviceTrait, DeviceType, DeviceID, DevicePassword};
 pub use user::{User, UserID, UserAgent};
 
