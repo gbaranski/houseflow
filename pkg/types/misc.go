be863cf851419b86c693d3fb21f1a6a749944345
gbaranski <root@gbaranski.com>
2021-05-24 12:25:43 +0200

    remove all old Go code, 🦀

--- pkg/types/misc.go
+++
@@ -1,8 +0,0 @@
-package types
-
-// ResponseError is used to return it for example from some HTTP route
-type ResponseError struct {
-	Name        string `json:"error"`
-	Description string `json:"error_description,omitempty"`
-	StatusCode  int    `json:"-"`
-}
