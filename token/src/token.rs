73c963b4656aa74e4e36617b23a3b9fe7e9cdf0b
gbaranski <root@gbaranski.com>
2021-06-03 12:28:44 +0200

    refactor(token): remove all base64 methods, convert to string conversions

--- token/src/token.rs
+++ token/src/token.rs
@@ -30,20 +30,6 @@ impl Token {
         Self::decode(&mut buf)
     }
 
-    pub fn into_base64(self) -> String {
-        use bytes::BytesMut;
-        let mut buf = BytesMut::with_capacity(Self::SIZE);
-        self.encode(&mut buf);
-        base64::encode(buf)
-    }
-
-    pub fn base64(&self) -> String {
-        use bytes::BytesMut;
-        let mut buf = BytesMut::with_capacity(Self::SIZE);
-        self.encode(&mut buf);
-        base64::encode(buf)
-    }
-
     pub fn verify(&self, key: impl AsRef<[u8]>, user_agent: &UserAgent) -> Result<(), VerifyError> {
         self.payload.verify(user_agent)?;
         self.signature.verify(&self.payload, key)?;
@@ -53,6 +39,26 @@ impl Token {
     #[inline]
     pub fn has_expired(&self) -> bool {
         self.payload.expires_at.has_expired()
+    }
+}
+
+impl std::string::ToString for Token {
+    fn to_string(&self) -> String {
+        use bytes::BytesMut;
+
+        let mut buf = BytesMut::with_capacity(Self::SIZE);
+        self.encode(&mut buf);
+        base64::encode(buf)
+    }
+}
+
+impl std::str::FromStr for Token {
+    type Err = DecodeError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        use bytes::BytesMut;
+        let mut s = BytesMut::from(s);
+        Self::decode(&mut s)
     }
 }
 
@@ -136,85 +142,6 @@ impl Serialize for Token {
     where
         S: Serializer,
     {
-        let base64 = self.base64();
-        serializer.serialize_str(&base64)
+        serializer.serialize_str(&self.to_string())
     }
 }
-
-// use crate::{Error, HmacSha256, Payload, Signature, SizedFrame};
-// use bytes::{Buf, BufMut, BytesMut};
-// use hmac::{Mac, NewMac};
-// use houseflow_types::UserAgent;
-// use std::time::SystemTime;
-//
-// #[derive(Debug, Clone, PartialEq, Eq)]
-// pub struct Token {
-//     pub signature: Signature,
-//     pub payload: Payload,
-// }
-//
-// impl SizedFrame for Token {
-//     const SIZE: usize = Signature::SIZE + Payload::SIZE;
-// }
-//
-// impl Token {
-//     #[cfg(feature = "serde")]
-//     const BASE64_SIZE: usize = (Self::SIZE / 3) * 4;
-//
-//     pub fn from_base64(base64: impl AsRef<[u8]>) -> Result<Self, Error> {
-//         let mut bytes: &[u8] = &base64::decode(base64)?;
-//         Self::from_buf(&mut bytes)
-//     }
-//
-//     pub fn to_base64(&self) -> String {
-//         let mut bytes = BytesMut::with_capacity(Self::SIZE);
-//         self.signature.to_buf(&mut bytes);
-//         self.payload.to_buf(&mut bytes);
-//         base64::encode(bytes)
-//     }
-//
-//     pub fn from_buf(buf: &mut impl Buf) -> Result<Self, Error> {
-//         let signature = Signature::from_buf(buf)?;
-//         let payload = Payload::from_buf(buf)?;
-//         Ok(Self { payload, signature })
-//     }
-//
-//     pub fn to_buf(&self, buf: &mut impl BufMut) {
-//         self.signature.to_buf(buf);
-//         self.payload.to_buf(buf);
-//     }
-//
-//     pub fn has_expired(&self) -> bool {
-//         self.payload.expires_at.elapsed().is_ok()
-//     }
-//
-//     pub fn verify(&self, key: &[u8], user_agent: &UserAgent) -> Result<(), Error> {
-//         if self.payload.user_agent != *user_agent {
-//             return Err(Error::InvalidUserAgent {
-//                 expected: self.payload.user_agent,
-//                 received: *user_agent,
-//             });
-//         }
-//         if self.has_expired() {
-//             return Err(Error::Expired {
-//                 expired_by: self
-//                     .payload
-//                     .expires_at
-//                     .duration_since(SystemTime::UNIX_EPOCH)
-//                     .unwrap()
-//                     .as_secs(),
-//             });
-//         }
-//         let mut mac = HmacSha256::new_varkey(key)
-//             .expect(format!("Invalid HMAC Key size of {}", key.len()).as_str());
-//
-//         let mut payload_buf = BytesMut::with_capacity(Payload::SIZE);
-//         self.payload.to_buf(&mut payload_buf);
-//         mac.update(&payload_buf);
-//         mac.verify(self.signature.as_ref())
-//             .map_err(|_err| Error::InvalidSignature)?;
-//
-//         Ok(())
-//     }
-// }
-//
