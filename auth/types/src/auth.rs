8a282558fde0c066c33b2e2f02645ce38af77ae4
gbaranski <root@gbaranski.com>
2021-06-04 11:37:47 +0200

    feat(auth/types): improve errors for auth

--- auth/types/src/auth.rs
+++ auth/types/src/auth.rs
@@ -1,20 +1,26 @@
 use houseflow_token::Token;
+use houseflow_types::UserAgent;
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Clone, Deserialize, Serialize)]
 pub struct LoginRequest {
     pub email: String,
     pub password: String,
+    pub user_agent: UserAgent,
 }
 
 pub type LoginResponse = Result<LoginResponseBody, LoginError>;
 
 #[derive(Debug, Clone, Deserialize, Serialize, thiserror::Error)]
 pub enum LoginError {
-    #[error("invalid credentials")]
-    InvalidCredentials,
+    #[error("invalid password")]
+    InvalidPassword,
 
-    #[error("internal error: ``")]
+    #[error("user not found")]
+    UserNotFound,
+
+
+    #[error("internal error: `{0}`")]
     // Replace it with better type if needed
     InternalError(String)
 }
@@ -38,10 +44,17 @@ impl actix_web::ResponseError for LoginError {
         use actix_web::http::StatusCode;
 
         match self {
-            LoginError::InvalidCredentials => StatusCode::BAD_REQUEST,
+            LoginError::InvalidPassword => StatusCode::BAD_REQUEST,
+            LoginError::UserNotFound => StatusCode::NOT_FOUND,
             LoginError::InternalError(_) => StatusCode::INTERNAL_SERVER_ERROR,
         }
     }
+
+    fn error_response(&self) -> actix_web::HttpResponse {
+        let json = actix_web::web::Json(self);
+        actix_web::HttpResponse::build(self.status_code()).json(json)
+    }
+
 }
 
 #[derive(Debug, Clone, Deserialize, Serialize)]
