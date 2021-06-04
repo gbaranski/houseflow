8b8e523c7b344c2327f162f45b22a008f7cd2004
gbaranski <root@gbaranski.com>
2021-06-04 13:03:18 +0200

    fix(db): set email to be unique field

--- db/migrations/V1__initial.sql
+++ db/migrations/V1__initial.sql
@@ -1,9 +1,9 @@
 CREATE TABLE users (
-    id            CHAR(32)    NOT NULL,
-    first_name    TEXT        NOT NULL,
-    last_name     TEXT        NOT NULL,
-    email         TEXT        NOT NULL,
-    password_hash TEXT        NOT NULL,
+    id            CHAR(32)        NOT NULL,
+    first_name    TEXT            NOT NULL,
+    last_name     TEXT            NOT NULL,
+    email         TEXT     UNIQUE NOT NULL,
+    password_hash TEXT            NOT NULL,
 
     PRIMARY KEY(
       id
