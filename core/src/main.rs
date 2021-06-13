0d946942a381bbd3105243bc2e3048cff0be5165
gbaranski <root@gbaranski.com>
2021-06-13 17:03:42 +0200

    feat(core): fetch access token function

--- core/src/main.rs
+++ core/src/main.rs
@@ -7,15 +7,16 @@ mod fulfillment;
 mod keystore;
 mod run;
 
-pub use keystore::{Keystore, KeystoreFile};
 pub use auth::AuthCommand;
-pub use fulfillment::FulfillmentCommand;
-pub use run::RunCommand;
 pub use config::ConfigCommand;
+pub use fulfillment::FulfillmentCommand;
+pub use keystore::{Keystore, KeystoreFile};
+pub use run::RunCommand;
 
 use cli::{CliConfig, Subcommand};
 use config::{ClientConfig, Config, ServerConfig};
 use strum_macros::{EnumIter, EnumString};
+use token::Token;
 
 #[derive(Clone, Debug, EnumString, strum_macros::Display, EnumIter)]
 pub enum Target {
@@ -41,6 +42,38 @@ pub struct ClientCommandState {
     pub keystore: Keystore,
     pub auth: auth_api::Auth,
     pub fulfillment: fulfillment_api::Fulfillment,
+}
+
+impl ClientCommandState {
+    pub async fn access_token(&self) -> anyhow::Result<Token> {
+        let keystore_file = self.keystore.read().await?;
+        if keystore_file.refresh_token.has_expired() == true {
+            log::debug!("cached refresh token is expired");
+            return Err(anyhow::Error::msg(
+                "refresh token expired, you need to log in again using `houseflow auth login`",
+            ));
+        }
+
+        if keystore_file.access_token.has_expired() == false {
+            log::debug!("cached access token is not expired");
+            Ok(keystore_file.access_token)
+        } else {
+            log::debug!("cached access token is expired, fetching new one");
+            let fetched_access_token = self
+                .auth
+                .fetch_access_token(&keystore_file.refresh_token)
+                .await?
+                .into_result()?
+                .access_token;
+            let keystore_file = KeystoreFile {
+                refresh_token: keystore_file.refresh_token,
+                access_token: fetched_access_token.clone(),
+            };
+            self.keystore.save(&keystore_file).await?;
+
+            Ok(fetched_access_token)
+        }
+    }
 }
 
 #[async_trait(?Send)]
