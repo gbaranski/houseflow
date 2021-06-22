2cd48be7b879a218ff7ace64606e828a4b175fa7
gbaranski <root@gbaranski.com>
2021-06-22 15:18:47 +0200

    fix(core): reorder, add #[serde(default)] at scfg

--- core/src/config/server.rs
+++ core/src/config/server.rs
@@ -4,24 +4,30 @@ use types::ServerSecrets;
 
 #[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct ServerConfig {
-    /// Secret data
-    pub secrets: ServerSecrets,
-
+    /// Host, e.g 127.0.0.1
     #[serde(default = "defaults::host")]
     pub host: String,
 
+    /// Port, e.g 6001
     #[serde(default = "defaults::port")]
     pub port: u16,
 
+    /// Secret data
+    pub secrets: ServerSecrets,
+
     /// Configuration of the auth service
+    #[serde(default)]
     pub auth: auth::server::Config,
 
     /// Configuration of the fulfillment service
+    #[serde(default)]
     pub fulfillment: fulfillment::server::Config,
 
     /// Configuration of the lighthouse service
+    #[serde(default)]
     pub lighthouse: lighthouse::server::Config,
 
     /// Configuration of the PostgreSQL Database
+    #[serde(default)]
     pub postgres: db::postgres::Config,
 }
