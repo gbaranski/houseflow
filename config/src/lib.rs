b7489820d79138d962ab2bf9fad396414eccbd13
gbaranski <root@gbaranski.com>
2021-07-01 23:18:23 +0200

    fix(config): compile-time error with no features

--- config/src/lib.rs
+++ config/src/lib.rs
@@ -47,6 +47,7 @@ pub async fn read_file<T: serde::de::DeserializeOwned>(
     Ok(config)
 }
 
+#[cfg(any(test, feature = "device", feature = "client", feature = "server", feature = "postgres"))]
 pub(crate) mod resolve_socket_address {
     use serde::{
         de::{self, Visitor},
