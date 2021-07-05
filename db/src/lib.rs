a98efa1c9cdd2bf7d7d8d4692681152ed15fa7b0
gbaranski <root@gbaranski.com>
2021-07-05 19:04:12 +0200

    fix(db): make db ops sync

--- db/src/lib.rs
+++ db/src/lib.rs
@@ -1,12 +1,23 @@
-use async_trait::async_trait;
-
-
-pub trait DatabaseInternalError: std::fmt::Debug + std::error::Error + Send + Sync {}
+#[cfg(feature = "sqlite")]
+pub mod sqlite;
 
 #[derive(Debug, thiserror::Error)]
+#[non_exhaustive]
 pub enum Error {
-    #[error("internal error: `{0}`")]
-    InternalError(Box<dyn DatabaseInternalError>),
+    #[cfg(feature = "sqlite")]
+    #[error("sqlite error: {0}")]
+    Sqlite(#[from] rusqlite::Error),
+
+    #[cfg(feature = "refinery")]
+    #[error("sqlite error: {0}")]
+    Refinery(#[from] refinery::Error),
+
+    #[cfg(feature = "refinery")]
+    #[error("sqlite error: {0}")]
+    PoolError(#[from] r2d2::Error),
+
+    #[error("json error: {0}")]
+    JsonError(#[from] serde_json::Error),
 
     #[error("Query did not modify anything")]
     NotModified,
@@ -15,51 +26,32 @@ pub enum Error {
     AlreadyExists,
 }
 
-impl<T: DatabaseInternalError + 'static> From<T> for Error {
-    fn from(v: T) -> Self {
-        Self::InternalError(Box::new(v))
-    }
-}
+use houseflow_types::{Device, DeviceID, Room, RoomID, Structure, StructureID, User, UserID, UserStructure};
 
-use houseflow_types::{Device, DeviceID, Room, RoomID, Structure, User, UserID, UserStructure};
-
-#[async_trait]
 pub trait Database: Send + Sync {
-    async fn add_structure(&self, structure: &Structure) -> Result<(), Error>;
+    fn add_structure(&self, structure: &Structure) -> Result<(), Error>;
+    fn add_room(&self, room: &Room) -> Result<(), Error>;
+    fn add_device(&self, device: &Device) -> Result<(), Error>;
+    fn add_user(&self, user: &User) -> Result<(), Error>;
+    fn add_admin(&self, user_id: &UserID) -> Result<(), Error>;
+    fn add_user_structure(&self, user_structure: &UserStructure) -> Result<(), Error>;
 
-    async fn add_room(&self, room: &Room) -> Result<(), Error>;
 
-    async fn add_device(&self, device: &Device) -> Result<(), Error>;
+    fn get_structure(&self, structure_id: &StructureID) -> Result<Option<Structure>, Error>;
+    fn get_room(&self, room_id: &RoomID) -> Result<Option<Room>, Error>;
+    fn get_device(&self, device_id: &DeviceID) -> Result<Option<Device>, Error>;
+    fn get_user(&self, user_id: &UserID) -> Result<Option<User>, Error>;
+    fn get_user_by_email(&self, email: &str) -> Result<Option<User>, Error>;
+    fn get_user_devices(&self, user_id: &UserID) -> Result<Vec<Device>, Error>;
 
-    async fn add_user_structure(&self, user_structure: &UserStructure) -> Result<(), Error>;
-
-    async fn add_user(&self, user: &User) -> Result<(), Error>;
-
-    async fn get_room(&self, room_id: &RoomID) -> Result<Option<Room>, Error>;
-
-    async fn get_device(&self, device_id: &DeviceID) -> Result<Option<Device>, Error>;
-
-    async fn get_user_devices(&self, user_id: &UserID) -> Result<Vec<Device>, Error>;
-
-    async fn get_user(&self, user_id: &UserID) -> Result<Option<User>, Error>;
-
-    async fn get_user_by_email(&self, email: &str) -> Result<Option<User>, Error>;
-
-    async fn check_user_device_access(
+    fn check_user_device_access(
         &self,
         user_id: &UserID,
         device_id: &DeviceID,
     ) -> Result<bool, Error>;
 
-    async fn check_user_device_manager_access(
-        &self,
-        user_id: &UserID,
-        device_id: &DeviceID,
-    ) -> Result<bool, Error>;
+    fn check_user_admin(&self, user_id: &UserID) -> Result<bool, Error>;
 
-    async fn check_user_admin(&self, user_id: &UserID) -> Result<bool, Error>;
-
-    async fn delete_user(&self, user_id: &UserID) -> Result<(), Error>;
 }
 
 impl From<Error> for houseflow_types::InternalServerError {
