c60a977d65670a9b6f6f77022d490a3108504458
gbaranski <root@gbaranski.com>
2021-07-23 20:42:41 +0200

    remove devices/esp-rtos

--- devices/esp-rtos/components/hf/component.mk
+++
@@ -1,3 +0,0 @@
-COMPONENT_SRCDIRS := crypto device mqtt wifi
-COMPONENT_EMBED_TXTFILES := letsencrypt.pem
-LIB_NAME := hf_core
--- devices/esp-rtos/components/hf/include/hf_types.h
+++
@@ -1,9 +0,0 @@
-#ifndef HF_TYPES_H
-#define HF_TYPES_H
-#include <driver/gpio.h>
-#include <stdbool.h>
-
-#include "sdkconfig.h"
-
-
-#endif
