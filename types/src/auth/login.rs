fd450216e607b9058ae21ab5423f6ce5a9e84c80
gbaranski <root@gbaranski.com>
2021-08-03 00:31:29 +0200

    fix: simplify auth/login response error

--- types/src/auth/login.rs
+++ types/src/auth/login.rs
@@ -1,3 +1,4 @@
+use houseflow_macros::server_error;
 use serde::{Deserialize, Serialize};
 use validator::Validate;
 
@@ -19,40 +20,31 @@ pub struct ResponseBody {
     pub access_token: String,
 }
 
-#[derive(Debug, Clone, Deserialize, Serialize, PartialEq, thiserror::Error)]
-#[serde(
-    tag = "error",
-    content = "error_description",
-    rename_all = "snake_case"
-)]
+#[server_error]
 pub enum ResponseError {
-    #[error("internal error: {0}")]
-    InternalError(#[from] crate::InternalServerError),
-
-    #[error("validation error: {0}")]
-    ValidationError(#[from] crate::ValidationError),
-
     #[error("invalid password")]
+    #[response(status_code = 400)]
     InvalidPassword,
 
     #[error("user not found")]
+    #[response(status_code = 404)]
     UserNotFound,
 }
 
-#[cfg(feature = "actix")]
-impl actix_web::ResponseError for ResponseError {
-    fn status_code(&self) -> actix_web::http::StatusCode {
-        use actix_web::http::StatusCode;
-
-        match self {
-            Self::InternalError(_) => StatusCode::INTERNAL_SERVER_ERROR,
-            Self::ValidationError(_) => StatusCode::BAD_REQUEST,
-            Self::InvalidPassword => StatusCode::UNAUTHORIZED,
-            Self::UserNotFound => StatusCode::UNAUTHORIZED,
-        }
-    }
-
-    fn error_response(&self) -> actix_web::HttpResponse {
-        crate::json_error_response(self.status_code(), self)
-    }
-}
+// #[cfg(feature = "actix")]
+// impl actix_web::ResponseError for ResponseError {
+//     fn status_code(&self) -> actix_web::http::StatusCode {
+//         use actix_web::http::StatusCode;
+// 
+//         match self {
+//             Self::InternalError(_) => StatusCode::INTERNAL_SERVER_ERROR,
+//             Self::ValidationError(_) => StatusCode::BAD_REQUEST,
+//             Self::InvalidPassword => StatusCode::UNAUTHORIZED,
+//             Self::UserNotFound => StatusCode::UNAUTHORIZED,
+//         }
+//     }
+// 
+//     fn error_response(&self) -> actix_web::HttpResponse {
+//         crate::json_error_response(self.status_code(), self)
+//     }
+// }
