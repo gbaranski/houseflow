b414db8d09c68a0c7fe45cf8f3889fc6177e55fb
gbaranski <root@gbaranski.com>
2021-08-07 12:27:48 +0200

    test(server): testing logout

--- server/src/auth/logout.rs
+++ server/src/auth/logout.rs
@@ -1,18 +1,42 @@
 use crate::{extractors::RefreshToken, State};
 use axum::{extract, response};
-use houseflow_types::{
-    auth::logout::{Request, Response},
-    errors::ServerError,
-};
+use houseflow_types::{auth::logout::Response, errors::ServerError};
 use tracing::Level;
 
-#[tracing::instrument(name = "Logout", skip(state, _request, refresh_token), err)]
+#[tracing::instrument(name = "Logout", skip(state, refresh_token), err)]
 pub async fn handle(
     extract::Extension(state): extract::Extension<State>,
-    extract::Json(_request): extract::Json<Request>,
     RefreshToken(refresh_token): RefreshToken,
 ) -> Result<response::Json<Response>, ServerError> {
     state.token_store.remove(&refresh_token.tid).await.unwrap();
     tracing::event!(Level::INFO, user_id = %refresh_token.sub, "Logged out");
     Ok(response::Json(Response {}))
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::test_utils::*;
+
+    #[tokio::test]
+    async fn valid() {
+        let state = get_state();
+        let user = get_user();
+        let refresh_token = houseflow_types::token::RefreshToken::new(
+            state.config.secrets.refresh_key.as_bytes(),
+            houseflow_types::token::RefreshTokenPayload {
+                tid: rand::random(),
+                sub: user.id.clone(),
+                exp: None,
+            },
+        );
+        state.database.add_user(&user).unwrap();
+        state.token_store.add(&refresh_token.tid, refresh_token.exp.as_ref()).await.unwrap();
+        let _ = super::handle(
+            state.clone(),
+            crate::extractors::RefreshToken(refresh_token.clone()),
+        )
+        .await
+        .unwrap();
+        assert!(!state.token_store.exists(&refresh_token.tid).await.unwrap());
+    }
 }
