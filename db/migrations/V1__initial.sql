69c48fc917f09186f33a6c7a2224ff3d32cb33bc
gbaranski <root@gbaranski.com>
2021-08-09 03:12:20 +0200

    fix(db): remove device realted stuff from sqlite

--- db/migrations/V1__initial.sql
+++ db/migrations/V1__initial.sql
@@ -1,6 +1,3 @@
--- insert into users values('faf04931e9f14f0da3a59d3e51375e6e', 'gbaranski', 'root@gbaranski.com', 'phash');
--- insert into admins values('faf04931e9f14f0da3a59d3e51375e6e');
-
 CREATE TABLE users (
   id            CHAR(32) NOT NULL,
   username      VARCHAR  NOT NULL,
@@ -17,56 +14,3 @@ CREATE TABLE admins (
 
   PRIMARY KEY( user_id )
 );
-
-CREATE TABLE structures (
-  id          CHAR(32)   NOT NULL UNIQUE,
-  name        VARCHAR    NOT NULL,
-
-  CHECK( length(id) == 32 )
-
-  PRIMARY KEY( id )
-);
-
-CREATE TABLE user_structures (
-  structure_id  CHAR(32) NOT NULL REFERENCES structures(id) ON DELETE CASCADE,
-  user_id       CHAR(32) NOT NULL REFERENCES users     (id) ON DELETE CASCADE,
-  is_manager    BOOLEAN  NOT NULL,
-
-  CHECK( is_manager in (0, 1) )
-
-  PRIMARY KEY( structure_id, user_id )
-);
-
-
-CREATE TABLE rooms (
-  id           CHAR(32) NOT NULL,
-  structure_id CHAR(32) NOT NULL REFERENCES structures(id) ON DELETE CASCADE,
-  name         VARCHAR  NOT NULL,
-
-  PRIMARY KEY( id )
-);
-
-CREATE TABLE devices (
-  id              CHAR(32)    NOT NULL,
-  room_id         CHAR(32)    NOT NULL REFERENCES rooms(id) ON DELETE CASCADE,
-  password_hash   VARCHAR     NOT NULL, -- password hash used to verify
-  type            VARCHAR     NOT NULL, -- type of the device
-  name            VARCHAR     NOT NULL, -- name of the device
-  will_push_state BOOLEAN     NOT NULL, -- true if device will push state on it's own
-  model           VARCHAR     NOT NULL, -- model name of the device
-  hw_version      VARCHAR     NOT NULL, -- hardware version, must follow semver
-  sw_version      VARCHAR     NOT NULL, -- software version, must follow semver
-  attributes      VARCHAR     NOT NULL, -- device attributes in JSON format
-
-  CHECK( will_push_state in (0, 1) )
-
-  PRIMARY KEY( id )
-);
-
-CREATE TABLE device_traits (
-  device_id  CHAR(32) NOT NULL REFERENCES devices(id) ON DELETE CASCADE,
-  trait_name VARCHAR  NOT NULL,
-  
-  PRIMARY KEY(device_id, trait_name)
-);
-
