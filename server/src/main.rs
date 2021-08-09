930fb5692c693c5792d7b5f3e72ba2d9a844b164
gbaranski <root@gbaranski.com>
2021-08-09 16:53:16 +0200

    feat(server): nicer 'config not found' event

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,4 +1,4 @@
-use houseflow_config::{defaults, server::Config, Config as _};
+use houseflow_config::{defaults, Error as ConfigError, server::Config, Config as _};
 use houseflow_db::sqlite::Database as SqliteDatabase;
 use houseflow_server::{Sessions, SledTokenStore};
 use std::net::ToSocketAddrs;
@@ -13,7 +13,20 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .map(std::path::PathBuf::from)
         .unwrap_or_else(|_| Config::default_path());
 
-    let config = Config::read(config_path).expect("cannot load server config");
+    tracing::debug!("Config path: {}", config_path.to_str().unwrap());
+
+    let config = match Config::read(&config_path) {
+        Ok(config) => config,
+        Err(ConfigError::IOError(err)) => match err.kind() {
+            std::io::ErrorKind::NotFound => {
+                tracing::error!("Config file could not be found at {}", config_path.to_str().unwrap());
+                return Ok(())
+            },
+            _ => panic!("Read config IO Error: {}", err),
+        },
+        Err(err) => panic!("Config error: {}", err),
+    };
+    tracing::debug!("Config: {:#?}", config);
     let token_store =
         SledTokenStore::new(defaults::token_store_path()).expect("cannot open token store");
     let database = SqliteDatabase::new(defaults::database_path()).expect("cannot open database");
@@ -26,7 +39,11 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         sessions: Arc::new(Mutex::new(sessions)),
     };
 
-    let address = format!("{}:{}", state.config.hostname, defaults::server_port());
+    let address = format!(
+        "{}:{}",
+        state.config.network.hostname,
+        defaults::server_port()
+    );
     let address = address
         .to_socket_addrs()
         .expect("invalid address")
