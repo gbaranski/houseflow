89dfd782b03752c9898ddbd2baf66e5ba80f77cd
gbaranski <root@gbaranski.com>
2021-08-09 21:09:13 +0200

    feat(server): add websocket heartbeat

--- server/src/lighthouse/session.rs
+++ server/src/lighthouse/session.rs
@@ -1,9 +1,16 @@
-use axum::extract::ws::Message;
+use axum::extract::ws::Message as WebSocketMessage;
 use houseflow_types::{
     errors::InternalError,
     lighthouse::proto::{execute, execute_response, query, state, Frame},
 };
+use std::{
+    sync::{Arc, Mutex},
+    time::{Duration, Instant},
+};
 use tokio::sync::{broadcast, mpsc};
+
+const HEARTBEAT_INTERVAL: Duration = Duration::from_secs(5);
+const PING_TIMEOUT: Duration = Duration::from_secs(5);
 
 #[derive(Debug, thiserror::Error)]
 pub enum SessionError {
@@ -18,6 +25,9 @@ pub enum SessionError {
 
     #[error("send message over channel failed")]
     SendOverChannelError(String),
+
+    #[error("heartbeat failed")]
+    HeartbeatFailed,
 }
 
 impl<T> From<tokio::sync::broadcast::error::SendError<T>> for SessionError {
@@ -33,38 +43,46 @@ impl<T> From<tokio::sync::mpsc::error::SendError<T>> for SessionError {
 }
 
 #[derive(Debug, Clone)]
-enum Request {
+enum ServerMessage {
     Execute(execute::Frame),
     Query(query::Frame),
+    Ping(Vec<u8>),
+    Pong(Vec<u8>),
 }
 
 #[derive(Debug, Clone)]
-enum Response {
-    Execute(execute_response::Frame),
-    Query(state::Frame),
+enum DeviceMessage {
+    ExecuteResponse(execute_response::Frame),
+    State(state::Frame),
+    // Ping(Vec<u8>),
+    // Pong(Vec<u8>),
 }
 
 #[derive(Debug, Clone)]
 pub struct Session {
-    requests: mpsc::UnboundedSender<Request>,
-    responses: broadcast::Sender<Response>,
+    server_messages: mpsc::UnboundedSender<ServerMessage>,
+    device_messages: broadcast::Sender<DeviceMessage>,
+    last_heartbeat: Arc<Mutex<Instant>>,
 }
 
 // Non-clonable session internals
 #[derive(Debug)]
 pub struct SessionInternals {
-    requests: (
-        mpsc::UnboundedSender<Request>,
-        mpsc::UnboundedReceiver<Request>,
+    server_messages: (
+        mpsc::UnboundedSender<ServerMessage>,
+        mpsc::UnboundedReceiver<ServerMessage>,
     ),
-    responses: (broadcast::Sender<Response>, broadcast::Receiver<Response>),
+    device_messages: (
+        broadcast::Sender<DeviceMessage>,
+        broadcast::Receiver<DeviceMessage>,
+    ),
 }
 
 impl Default for SessionInternals {
     fn default() -> Self {
         Self {
-            requests: mpsc::unbounded_channel(),
-            responses: broadcast::channel(512),
+            server_messages: mpsc::unbounded_channel(),
+            device_messages: broadcast::channel(512),
         }
     }
 }
@@ -80,15 +98,16 @@ use futures::{SinkExt, StreamExt};
 impl Session {
     pub fn new(internals: &SessionInternals) -> Self {
         Self {
-            requests: internals.requests.0.clone(),
-            responses: internals.responses.0.clone(),
+            server_messages: internals.server_messages.0.clone(),
+            device_messages: internals.device_messages.0.clone(),
+            last_heartbeat: Arc::new(Mutex::new(Instant::now())),
         }
     }
 
     pub async fn run(
         &self,
-        stream: impl futures::Stream<Item = Result<Message, axum::Error>>
-            + futures::Sink<Message, Error = axum::Error>
+        stream: impl futures::Stream<Item = Result<WebSocketMessage, axum::Error>>
+            + futures::Sink<WebSocketMessage, Error = axum::Error>
             + Unpin,
         internals: SessionInternals,
     ) -> Result<(), tower::BoxError> {
@@ -96,6 +115,7 @@ impl Session {
         tokio::select! {
             _ = self.stream_read(rx) => {},
             _ = self.stream_write(tx, internals) => {}
+            _ = self.heartbeat() => {}
         };
 
         Ok(())
@@ -103,7 +123,7 @@ impl Session {
 
     async fn stream_read<S>(&self, mut stream: S) -> Result<(), SessionError>
     where
-        S: futures::Stream<Item = Result<Message, axum::Error>> + Unpin,
+        S: futures::Stream<Item = Result<WebSocketMessage, axum::Error>> + Unpin,
     {
         while let Some(message) = stream
             .next()
@@ -112,14 +132,15 @@ impl Session {
             .map_err(SessionError::WebsocketError)?
         {
             match message {
-                Message::Text(text) => {
+                WebSocketMessage::Text(text) => {
                     let frame = serde_json::from_str::<Frame>(&text)?;
                     match frame {
                         Frame::State(state) => {
-                            self.responses.send(Response::Query(state))?;
+                            self.device_messages.send(DeviceMessage::State(state))?;
                         }
                         Frame::ExecuteResponse(execute_response) => {
-                            self.responses.send(Response::Execute(execute_response))?;
+                            self.device_messages
+                                .send(DeviceMessage::ExecuteResponse(execute_response))?;
                         }
                         Frame::Query(_) | Frame::Execute(_) => {
                             return Err(SessionError::UnexpectedFrame {
@@ -129,10 +150,16 @@ impl Session {
                         _ => unimplemented!(),
                     };
                 }
-                Message::Binary(_) => todo!(),
-                Message::Ping(_) => todo!(),
-                Message::Pong(_) => todo!(),
-                Message::Close(_) => todo!(),
+                WebSocketMessage::Binary(_) => todo!(),
+                WebSocketMessage::Ping(bytes) => {
+                    // self.server_messages.send(ServerMessage::Pong(bytes))?;
+                }
+                WebSocketMessage::Pong(_bytes) => {
+                    *self.last_heartbeat.lock().unwrap() = Instant::now();
+                }
+                WebSocketMessage::Close(frame) => {
+                    tracing::info!("Close info: {:?}", frame);
+                }
             }
         }
 
@@ -145,47 +172,54 @@ impl Session {
         mut internals: SessionInternals,
     ) -> Result<(), SessionError>
     where
-        S: futures::Sink<Message, Error = axum::Error> + Unpin,
+        S: futures::Sink<WebSocketMessage, Error = axum::Error> + Unpin,
     {
-        async fn send_json<S, T>(stream: &mut S, val: &T) -> Result<(), SessionError>
-        where
-            S: futures::Sink<Message, Error = axum::Error> + Unpin,
-            T: serde::Serialize,
-        {
-            let json = serde_json::to_string(val)?;
-            stream
-                .send(Message::Text(json))
-                .await
-                .map_err(SessionError::WebsocketError)
-        }
-
-        while let Some(request) = internals.requests.1.recv().await {
-            match request {
-                Request::Execute(execute) => {
-                    send_json(&mut stream, &Frame::Execute(execute)).await?
+        while let Some(request) = internals.server_messages.1.recv().await {
+            let message = match request {
+                ServerMessage::Execute(frame) => {
+                    WebSocketMessage::Text(serde_json::to_string(&Frame::Execute(frame))?)
                 }
-                Request::Query(query) => send_json(&mut stream, &Frame::Query(query)).await?,
-            }
+                ServerMessage::Query(frame) => {
+                    WebSocketMessage::Text(serde_json::to_string(&Frame::Query(frame))?)
+                }
+                ServerMessage::Ping(bytes) => WebSocketMessage::Ping(bytes),
+                ServerMessage::Pong(bytes) => WebSocketMessage::Pong(bytes),
+            };
+            stream
+                .send(message)
+                .await
+                .map_err(SessionError::WebsocketError)?;
         }
         Ok(())
+    }
+
+    async fn heartbeat(&self) -> Result<(), SessionError> {
+        let mut interval = tokio::time::interval(HEARTBEAT_INTERVAL);
+        loop {
+            interval.tick().await;
+            if Instant::now().duration_since(*self.last_heartbeat.lock().unwrap()) > PING_TIMEOUT {
+                return Err(SessionError::HeartbeatFailed);
+            }
+            self.server_messages.send(ServerMessage::Ping(Vec::new()))?;
+        }
     }
 
     pub async fn execute(
         &self,
         frame: execute::Frame,
     ) -> Result<execute_response::Frame, InternalError> {
-        let mut response_subscriber = self.responses.subscribe();
+        let mut subscriber = self.device_messages.subscribe();
         let frame_id = frame.id;
-        self.requests
-            .send(Request::Execute(frame))
+        self.server_messages
+            .send(ServerMessage::Execute(frame))
             .map_err(|err| InternalError::Other(err.to_string()))?;
 
         loop {
-            let response = response_subscriber
+            let response = subscriber
                 .recv()
                 .await
                 .map_err(|err| InternalError::Other(err.to_string()))?;
-            if let Response::Execute(execute_response) = response {
+            if let DeviceMessage::ExecuteResponse(execute_response) = response {
                 if execute_response.id == frame_id {
                     break Ok::<_, InternalError>(execute_response);
                 }
@@ -194,18 +228,18 @@ impl Session {
     }
 
     pub async fn query(&self, frame: query::Frame) -> Result<state::Frame, InternalError> {
-        let mut response_subscriber = self.responses.subscribe();
-        self.requests
-            .send(Request::Query(frame))
+        let mut subscriber = self.device_messages.subscribe();
+        self.server_messages
+            .send(ServerMessage::Query(frame))
             .map_err(|err| InternalError::Other(err.to_string()))?;
 
         loop {
-            if let Response::Query(query_respose) = response_subscriber
+            if let DeviceMessage::State(state) = subscriber
                 .recv()
                 .await
                 .map_err(|err| InternalError::Other(err.to_string()))?
             {
-                return Ok(query_respose);
+                return Ok(state);
             }
         }
     }
