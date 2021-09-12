b24dac2a1505ae7e181910cca7957478fafe6e77
gbaranski <root@gbaranski.com>
2021-09-12 16:27:22 +0200

    fix(types): flatten command on Execute frame

--- types/src/lighthouse/execute.rs
+++ types/src/lighthouse/execute.rs
@@ -6,5 +6,6 @@ use serde::Serialize;
 #[derive(Debug, Clone, Eq, PartialEq, Serialize, Deserialize)]
 pub struct Frame {
     pub id: FrameID,
+    #[serde(flatten)]
     pub command: device::Command,
 }
