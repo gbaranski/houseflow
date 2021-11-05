b87b9b68d2ca429630e33e5cf10f36aa4da441f9
gbaranski <root@gbaranski.com>
2021-11-05 20:38:27 +0100

    fix(hub): start discovery before listening

--- hub/src/main.rs
+++ hub/src/main.rs
@@ -4,9 +4,10 @@ use futures::StreamExt;
 #[tokio::main]
 async fn main() -> Result<(), anyhow::Error> {
     let (_, session) = MijiaSession::new().await?;
+    session.bt_session.start_discovery().await?;
     let mut stream = session.event_stream().await?;
-    while let Some(device) = stream.next().await {
-        println!("{:?}", device);
+    while let Some(event) = stream.next().await {
+        println!("{:?}", event);
     }
 
     Ok(())
