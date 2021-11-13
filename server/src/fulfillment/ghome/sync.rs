5a3d2273ba64c2919b582ce9a29e465a92eb834a
Andrew Walbran <qwandor@google.com>
2021-11-13 18:52:12 +0000

    Return one Google Home device per Homie device.

--- server/src/fulfillment/ghome/sync.rs
+++ server/src/fulfillment/ghome/sync.rs
@@ -1,7 +1,11 @@
+use std::collections::HashMap;
+
 use crate::State;
 use google_smart_home::device::Trait as GHomeDeviceTrait;
 use google_smart_home::device::Type as GHomeDeviceType;
 use google_smart_home::sync::response;
+use google_smart_home::sync::response::PayloadDevice;
+use homie_controller::Device;
 use houseflow_types::device::Trait as DeviceTrait;
 use houseflow_types::device::Type as DeviceType;
 use houseflow_types::errors::InternalError;
@@ -10,55 +14,58 @@ use houseflow_types::user;
 
 #[tracing::instrument(name = "Sync", skip(state), err)]
 pub async fn handle(state: State, user_id: user::ID) -> Result<response::Payload, ServerError> {
-    let user_devices = state.config.get_user_devices(&user_id);
+    let user_devices = if let Some(homie_controller) = state.homie_controllers.get(&user_id) {
+        homie_devices_to_google_home(&homie_controller.devices())
+    } else {
+        let user_devices = state.config.get_user_devices(&user_id);
 
-    let user_devices = user_devices
-        .into_iter()
-        .map(|device_id| state.config.get_device(&device_id).unwrap())
-        .map(|device| {
-            let room = state
-                .config
-                .get_room(&device.room_id)
-                .ok_or_else(|| InternalError::Other("couldn't find matching room".to_string()))?;
-            let payload = response::PayloadDevice {
-                id: device.id.to_string(),
-                device_type: match device.device_type {
-                    DeviceType::Garage => GHomeDeviceType::Garage,
-                    DeviceType::Gate => GHomeDeviceType::Gate,
-                    DeviceType::Light => GHomeDeviceType::Light,
-                    _ => todo!(),
-                },
-                traits: device
-                    .traits
-                    .iter()
-                    .map(|t| match t {
-                        DeviceTrait::OnOff => GHomeDeviceTrait::OnOff,
-                        DeviceTrait::OpenClose => GHomeDeviceTrait::OpenClose,
+        user_devices
+            .into_iter()
+            .map(|device_id| state.config.get_device(&device_id).unwrap())
+            .map(|device| {
+                let room = state.config.get_room(&device.room_id).ok_or_else(|| {
+                    InternalError::Other("couldn't find matching room".to_string())
+                })?;
+                let payload = response::PayloadDevice {
+                    id: device.id.to_string(),
+                    device_type: match device.device_type {
+                        DeviceType::Garage => GHomeDeviceType::Garage,
+                        DeviceType::Gate => GHomeDeviceType::Gate,
+                        DeviceType::Light => GHomeDeviceType::Light,
                         _ => todo!(),
-                    })
-                    .collect(),
-                name: response::PayloadDeviceName {
-                    default_names: None,
-                    name: device.name,
-                    nicknames: None,
-                },
-                will_report_state: device.will_push_state,
-                notification_supported_by_agent: false, // not sure about that
-                room_hint: Some(room.name),
-                device_info: Some(response::PayloadDeviceInfo {
-                    manufacturer: Some("houseflow".to_string()),
-                    model: None,
-                    hw_version: Some(device.hw_version.to_string()),
-                    sw_version: Some(device.sw_version.to_string()),
-                }),
-                attributes: device.attributes,
-                custom_data: None,
-                other_device_ids: None,
-            };
+                    },
+                    traits: device
+                        .traits
+                        .iter()
+                        .map(|t| match t {
+                            DeviceTrait::OnOff => GHomeDeviceTrait::OnOff,
+                            DeviceTrait::OpenClose => GHomeDeviceTrait::OpenClose,
+                            _ => todo!(),
+                        })
+                        .collect(),
+                    name: response::PayloadDeviceName {
+                        default_names: None,
+                        name: device.name,
+                        nicknames: None,
+                    },
+                    will_report_state: device.will_push_state,
+                    notification_supported_by_agent: false, // not sure about that
+                    room_hint: Some(room.name),
+                    device_info: Some(response::PayloadDeviceInfo {
+                        manufacturer: Some("houseflow".to_string()),
+                        model: None,
+                        hw_version: Some(device.hw_version.to_string()),
+                        sw_version: Some(device.sw_version.to_string()),
+                    }),
+                    attributes: device.attributes,
+                    custom_data: None,
+                    other_device_ids: None,
+                };
 
-            Ok::<_, ServerError>(payload)
-        })
-        .collect::<Result<Vec<_>, _>>()?;
+                Ok::<_, ServerError>(payload)
+            })
+            .collect::<Result<Vec<_>, _>>()?
+    };
 
     tracing::info!("Synced {} devices", user_devices.len());
 
@@ -68,4 +75,27 @@ pub async fn handle(state: State, user_id: user::ID) -> Result<response::Payload
         debug_string: None,
         devices: user_devices,
     })
+}
+
+fn homie_devices_to_google_home(devices: &HashMap<String, Device>) -> Vec<PayloadDevice> {
+    devices
+        .values()
+        .map(|device| response::PayloadDevice {
+            id: device.id.clone(),
+            device_type: GHomeDeviceType::Light,
+            traits: vec![GHomeDeviceTrait::OnOff],
+            name: response::PayloadDeviceName {
+                default_names: None,
+                name: device.name.clone().unwrap_or_else(|| device.id.clone()),
+                nicknames: None,
+            },
+            device_info: None,
+            will_report_state: false,
+            notification_supported_by_agent: false,
+            room_hint: None,
+            attributes: Default::default(),
+            custom_data: None,
+            other_device_ids: None,
+        })
+        .collect()
 }
