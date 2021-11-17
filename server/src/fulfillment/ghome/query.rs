a57f7a6786ea28278470c9ffc781d1bf7cea2787
Andrew Walbran <qwandor@google.com>
2021-11-17 22:55:29 +0000

    Factor out function to get device from config and session.

--- server/src/fulfillment/ghome/query.rs
+++ server/src/fulfillment/ghome/query.rs
@@ -13,66 +13,68 @@ pub async fn handle(
     user_id: user::ID,
     payload: &request::Payload,
 ) -> Result<response::Payload, InternalError> {
-    let sessions = &state.sessions;
-    let config = &state.config;
     let user_id = &user_id;
 
-    let responses = payload.devices.iter().map(|device| async move {
-        async {
-            let device_id = device::ID::from_str(&device.id).expect("invalid device ID");
-            if config.get_permission(&device_id, user_id).is_none() {
-                return Ok::<_, InternalError>(response::PayloadDevice {
-                    status: response::PayloadDeviceStatus::Error,
-                    state: Default::default(),
-                    error_code: Some(String::from("authFailure")),
-                });
-            }
-            let session = match sessions.get(&device_id) {
-                Some(session) => session.clone(),
-                None => {
-                    return Ok(response::PayloadDevice {
-                        state: Default::default(),
-                        status: response::PayloadDeviceStatus::Offline,
-                        error_code: Some(String::from("offline")),
-                    })
-                }
-            };
-
-            let request = lighthouse::query::Frame {};
-            let response = match tokio::time::timeout(
-                crate::fulfillment::EXECUTE_TIMEOUT,
-                session.query(request),
-            )
-            .await
-            {
-                Ok(val) => val?,
-                Err(_) => {
-                    return Ok(response::PayloadDevice {
-                        status: response::PayloadDeviceStatus::Offline,
-                        state: Default::default(),
-                        error_code: Some(String::from("offline")),
-                    })
-                }
-            };
-
-            tracing::info!(state = %serde_json::to_string(&response.state).unwrap(),
-                "Queried device state");
-
-            Ok(response::PayloadDevice {
-                status: response::PayloadDeviceStatus::Success,
-                error_code: None,
-                state: response.state,
-            })
-        }
-        .await
-        .map(|response| (device.id.to_owned(), response))
+    let responses = payload.devices.iter().map(|device| async {
+        let response = get_lighthouse_device(&state, user_id, device).await?;
+        Ok::<_, InternalError>((device.id.to_owned(), response))
     });
+    let devices = futures::future::try_join_all(responses)
+        .await?
+        .into_iter()
+        .collect();
     Ok(response::Payload {
         error_code: None,
         debug_string: None,
-        devices: futures::future::try_join_all(responses)
-            .await?
-            .into_iter()
-            .collect(),
+        devices,
+    })
+}
+
+async fn get_lighthouse_device(
+    state: &State,
+    user_id: &user::ID,
+    device: &request::PayloadDevice,
+) -> Result<response::PayloadDevice, InternalError> {
+    let device_id = device::ID::from_str(&device.id).expect("invalid device ID");
+
+    if state.config.get_permission(&device_id, user_id).is_none() {
+        return Ok(response::PayloadDevice {
+            status: response::PayloadDeviceStatus::Error,
+            state: Default::default(),
+            error_code: Some(String::from("authFailure")),
+        });
+    }
+    let session = match state.sessions.get(&device_id) {
+        Some(session) => session.clone(),
+        None => {
+            return Ok(response::PayloadDevice {
+                state: Default::default(),
+                status: response::PayloadDeviceStatus::Offline,
+                error_code: Some(String::from("offline")),
+            })
+        }
+    };
+
+    let request = lighthouse::query::Frame {};
+    let response =
+        match tokio::time::timeout(crate::fulfillment::EXECUTE_TIMEOUT, session.query(request))
+            .await
+        {
+            Ok(val) => val?,
+            Err(_) => {
+                return Ok(response::PayloadDevice {
+                    status: response::PayloadDeviceStatus::Offline,
+                    state: Default::default(),
+                    error_code: Some(String::from("offline")),
+                })
+            }
+        };
+
+    tracing::info!(state = %serde_json::to_string(&response.state).unwrap(), "Queried device state");
+
+    Ok(response::PayloadDevice {
+        status: response::PayloadDeviceStatus::Success,
+        error_code: None,
+        state: response.state,
     })
 }
