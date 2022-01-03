88211b7956dc1c7c0f70d842be967b4a3429305b
gbaranski <me@gbaranski.com>
2022-01-03 14:12:05 +0100

    fix(config/hub): remove unrelated fields

--- config/src/hub/mod.rs
+++ config/src/hub/mod.rs
@@ -1,38 +1,16 @@
-use crate::defaults;
 use houseflow_types::accessory;
-use houseflow_types::hub;
 use serde::Deserialize;
 use serde::Serialize;
-use url::Url;
 
 #[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
 #[serde(rename_all = "kebab-case")]
 pub struct Config {
-    pub credentials: Credentials,
-    #[serde(default)]
-    pub server: Server,
     #[serde(default)]
     pub accessories: Vec<Accessory>,
     #[serde(default)]
     pub providers: Providers,
     #[serde(default)]
     pub controllers: Controllers,
-}
-
-#[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
-#[serde(rename_all = "kebab-case")]
-pub struct Credentials {
-    /// ID of the hub
-    pub id: hub::ID,
-    /// Password of the hub in plain-text
-    pub password: hub::Password,
-}
-
-#[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
-#[serde(rename_all = "kebab-case")]
-pub struct Server {
-    #[serde(default = "defaults::server_websocket_url")]
-    pub url: Url,
 }
 
 #[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
@@ -120,24 +98,7 @@ impl crate::Config for Config {
     const DEFAULT_FILE: &'static str = "hub.toml";
 
     fn preprocess(&mut self) -> Result<(), String> {
-        if self.server.url.port().is_none() {
-            let scheme = self.server.url.scheme();
-            let port = match scheme {
-                "ws" => defaults::server_port(),
-                "wss" => defaults::server_port_tls(),
-                _ => return Err(format!("unexpected scheme: {}", scheme)),
-            };
-            self.server.url.set_port(Some(port)).unwrap();
-        }
         Ok(())
-    }
-}
-
-impl Default for Server {
-    fn default() -> Self {
-        Self {
-            url: defaults::server_websocket_url(),
-        }
     }
 }
 
@@ -146,19 +107,10 @@ mod tests {
     use super::*;
     use crate::Config as _;
     use houseflow_types::accessory;
-    use houseflow_types::hub;
-    use url::Url;
 
     #[test]
     fn test_example() {
         let expected = Config {
-            credentials: Credentials {
-                id: hub::ID::parse_str("345469C1-6C6F-461A-AB60-E21578D5A608").unwrap(),
-                password: hub::Password::from("some-password"),
-            },
-            server: Server {
-                url: Url::parse("wss://example.com:1234/hello/world").unwrap(),
-            },
             accessories: vec![Accessory {
                 id: accessory::ID::parse_str("37c6a8bd-264c-4653-a641-c9b574207be5").unwrap(),
                 name: String::from("Thermometer"),
@@ -179,10 +131,6 @@ mod tests {
             },
         };
 
-        std::env::set_var(
-            "SERVER_PORT",
-            expected.server.url.port().unwrap().to_string(),
-        );
         println!(
             "--------------------\n\n Serialized: \n{}\n\n--------------------",
             toml::to_string(&expected).unwrap()
