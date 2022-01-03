0492b011b6f7e6cc0ecdac38d086669733bc0a9e
gbaranski <me@gbaranski.com>
2022-01-03 14:05:58 +0100

    fix(config/accessory): rename `server` field to `hub`

--- config/src/accessory/mod.rs
+++ config/src/accessory/mod.rs
@@ -7,50 +7,50 @@ use url::Url;
 #[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
 #[serde(rename_all = "kebab-case")]
 pub struct Config {
-    pub credentials: Credentials,
     #[serde(default)]
-    pub server: Server,
+    pub hub: Hub,
+    pub credentials: Credentials,
+}
+
+#[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
+#[serde(rename_all = "kebab-case")]
+pub struct Hub {
+    #[serde(default = "defaults::hub_websocket_url")]
+    pub url: Url,
 }
 
 #[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
 #[serde(rename_all = "kebab-case")]
 pub struct Credentials {
-    /// ID of the hub
+    /// ID of the accessory
     pub id: accessory::ID,
-    /// Password of the hub in plain-text
+    /// Password of the accessory in plain-text
     pub password: accessory::Password,
-}
-
-#[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
-#[serde(rename_all = "kebab-case")]
-pub struct Server {
-    #[serde(default = "defaults::server_websocket_url")]
-    pub url: Url,
 }
 
 impl crate::Config for Config {
     const DEFAULT_TOML: &'static str = include_str!("default.toml");
 
-    const DEFAULT_FILE: &'static str = "hub.toml";
+    const DEFAULT_FILE: &'static str = "accessory.toml";
 
     fn preprocess(&mut self) -> Result<(), String> {
-        if self.server.url.port().is_none() {
-            let scheme = self.server.url.scheme();
+        if self.hub.url.port().is_none() {
+            let scheme = self.hub.url.scheme();
             let port = match scheme {
                 "ws" => defaults::server_port(),
                 "wss" => defaults::server_port_tls(),
                 _ => return Err(format!("unexpected scheme: {}", scheme)),
             };
-            self.server.url.set_port(Some(port)).unwrap();
+            self.hub.url.set_port(Some(port)).unwrap();
         }
         Ok(())
     }
 }
 
-impl Default for Server {
+impl Default for Hub {
     fn default() -> Self {
         Self {
-            url: defaults::server_websocket_url(),
+            url: defaults::hub_websocket_url(),
         }
     }
 }
@@ -69,14 +69,14 @@ mod tests {
                 id: accessory::ID::parse_str("345469C1-6C6F-461A-AB60-E21578D5A608").unwrap(),
                 password: accessory::Password::from("some-password"),
             },
-            server: Server {
+            hub: Hub {
                 url: Url::parse("wss://example.com:1234/hello/world").unwrap(),
             },
         };
 
         std::env::set_var(
-            "SERVER_PORT",
-            expected.server.url.port().unwrap().to_string(),
+            "HUB_PORT",
+            expected.hub.url.port().unwrap().to_string(),
         );
         println!(
             "--------------------\n\n Serialized: \n{}\n\n--------------------",
